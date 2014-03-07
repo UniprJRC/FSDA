54995ea5f2ff082bf9dd6d59f9ec6874af157382
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2014-03-07 10:47:44 +0000

    added the possibility of calling this function without arguments and improved documentation
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@55 336e6153-a435-9040-aa94-c6aba0fb517b

--- docsearchFS.m
+++ docsearchFS.m
@@ -1,19 +1,74 @@
-function docsearchFS(namehtmlhelpfile)
-% Find version of MATLAB which is installed
+function docsearchFS(varargin)
+%docsearchFS opens inside Help browser html documentation file.
+%
+%<a href="matlab: docsearchFS('docsearchFS')">Link to the help function</a>
+%
+%   docsearchFS opens the Help browser and displays the documentation home
+%   page of FS. 
+%
+%   docsearchFS('namehtmlhelpfile') searches FSDA documentation for pages
+%   with words that match the specified expression (version of matlab
+%   <=2012a) or opens FSDA documentation associated to namehtmlhelpfile.
+%
+%
+% Copyright 2008-2014.
+% Written by FSDA team
+%
+%
+%<a href="matlab: docsearchFS('docsearchFS')">Link to the help page for this function</a>
+% Last modified 08-Dec-2013
+
+% Examples:
+
+%
+%{
+    % Open main documentation page of FSDA
+    docsearchFS
+%}
+%
+%
+%{
+    % Open html documentation page of FSDA function named LXS
+    docsearchFS('lxs')
+%}
+
+%% Beginning of code
+if nargin > 1
+    namehtmlhelpfile = deblank(sprintf('%s ', varargin{:}));
+elseif nargin == 1
+    namehtmlhelpfile = varargin{1};
+else
+    namehtmlhelpfile = '';
+end
+
+% Find version of MATLAB which is installed 
 a=ver('matlab');
 
 if str2double(a.Version)>7.14
-    % If installed version of MATLAB is newer than 2012a command
+    % If installed version of MATLAB is newer than 2012a 
     % function helpbrowser is called
     
     % Find path of docsearchFS.m
     mname=mfilename('fullpath');
-    % Find full path of associated html file
-    rname = ['' mname(1:end-11) 'helpfiles\FSDA\' namehtmlhelpfile '.html' ''];
-    % Open html help file
-    web(rname,'-helpbrowser')
+    
+    if isempty(namehtmlhelpfile)
+        % if namehtmlhelpfile is not specified main page of FSDA
+        % documentation is opened
+        rname = ['' mname(1:end-11) 'helpfiles\FSDA\fsda_product_page.html' ''];
+        web(rname,'-helpbrowser')
+    else
+        
+        % rnames contains full path of associated html file
+        rname = ['' mname(1:end-11) 'helpfiles\FSDA\' namehtmlhelpfile '.html' ''];
+        % Open html help file
+        web(rname,'-helpbrowser')
+    end
 else
-    % If installed version of MATLAB is 2012a or older function docsearch
+    % If installed version of MATLAB is 2012a or older, matlab function docsearch
     % is called
-    docsearch(namehtmlhelpfile)
+    if isempty(namehtmlhelpfile)
+        docsearch('FSDA')
+    else
+        docsearch(namehtmlhelpfile)
+    end
 end
