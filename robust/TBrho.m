1b2ae66347579d9e70583a7cc41ac8436c0bba72
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2014-03-07 09:18:37 +0000

    MInor modifications typos corrected
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@53 336e6153-a435-9040-aa94-c6aba0fb517b

--- robust/TBrho.m
+++ robust/TBrho.m
@@ -40,8 +40,10 @@ x=-6:0.01:6;
 rhoTB=TBrho(x,2);
 plot(x,rhoTB)
 xlabel('x','Interpreter','Latex')
-ylabel('$\rho (x)$','Interpreter','Latex')
-
+ylabel('$\rho (x,2)$','Interpreter','Latex')
+text(x(1)-0.8,rhoTB(1),'c^2/6')
+text(x(end)+0.2,rhoTB(end),'c^2/6')
+title('$\rho (x,c)$','Interpreter','Latex')
 %}
 
 %% Beginning of code
