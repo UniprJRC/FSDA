c85f0d183b8287a1c8c884a66a140c34fd80f13e
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2014-04-12 11:20:47 +0000

    one typo corrected
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@71 336e6153-a435-9040-aa94-c6aba0fb517b

--- regression/LXS.m
+++ regression/LXS.m
@@ -1,7 +1,7 @@
 function [out , varargout] = LXS(y,X,varargin)
 %LXS computes the Least Median of Squares (LMS) or Least Trimmed Squares (LTS) estimators
 %
-%<a href="matlab: docsearch('LXS')">Link to the help function</a>
+%<a href="matlab: docsearch('lxs')">Link to the help function</a>
 %
 %  Required input arguments:
 %
