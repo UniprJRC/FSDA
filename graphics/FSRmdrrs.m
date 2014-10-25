8880e3bf6471a9603e038fb587c3c838fc520f8e
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2014-10-25 10:19:43 +0000

    Just one additional example added
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@219 336e6153-a435-9040-aa94-c6aba0fb517b

--- graphics/FSRmdrrs.m
+++ graphics/FSRmdrrs.m
@@ -313,6 +313,23 @@ function [mdrrs,BBrs]=FSRmdrrs(y,X,varargin)
     % computer)
     figure
     [mdrrs,BBrs]=FSRmdrrs(y,X,'nsimul',100,'plots',1,'bsbsteps',[10 300 600]);
+    % sum(~isnan(BBrs(:,1,1))) 
+    % 
+    % ans =
+    % 
+    %     10
+    % 
+    % sum(~isnan(BBrs(:,2,1)))
+    % 
+    % ans =
+    % 
+    %    300
+    % 
+    % sum(~isnan(BBrs(:,3,1)))
+    % 
+    % ans =
+    % 
+    %    600
 %}
 
 %{
