9986d12c0150291340a9f2a839788ed8ab9fcde0
emmanuele <emmanuele@336e6153-a435-9040-aa94-c6aba0fb517b>
2014-12-03 15:42:55 +0000

    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@240 336e6153-a435-9040-aa94-c6aba0fb517b

--- _automation_tools/create_tap.sh
+++ _automation_tools/create_tap.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-tapfile="pippo.tap"
+tapfile=${WORKSPACE}/pippo.tap
 
 rm -f $tapfile
 awk -f ${WORKSPACE}/_automation_tools/create_tap.awk execution_log.txt
 
 test_count=`wc -l $tapfile | awk '{ RS=" "; print $1; }'`
 
-echo "1..$test_count" >tempfile.tap
+echo "1..$test_count" >${WORKSPACE}/tempfile.tap
 cat $tapfile >>tempfile.tap
 rm $tapfile
-mv tempfile.tap $tapfile
-rm tempfile.tap
+mv ${WORKSPACE}/tempfile.tap $tapfile
+rm ${WORKSPACE}/tempfile.tap
