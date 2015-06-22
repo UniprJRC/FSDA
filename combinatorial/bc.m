3ed56bc041cd5fa613280b2f6e055b90a4c263a3
domenico <domenico@336e6153-a435-9040-aa94-c6aba0fb517b>
2015-06-22 16:26:05 +0000

    some changes in view of publishFS
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@558 336e6153-a435-9040-aa94-c6aba0fb517b

--- combinatorial/bc.m
+++ combinatorial/bc.m
@@ -10,37 +10,31 @@ function c = bc(n,k)
 %
 % Output:
 %
-%   bc(n,k) where n and k are non-negative integers returns n!/k!(n-k)!,
-%   i.e. the coefficient of the x^k term in the polynomial expansion of the
-%   binomial power (1 + x)^n.
-%   This is also the so called choose function of n and k (nchoosek in
-%   MATLAB), i.e. the number of k-element subsets (the k-combinations)
-%   of a set of n objects.
-%   When a coefficient is large, results may be inexact. The result is only
-%   accurate to 15 digits for double-precision inputs in 32bits computers.
+%  bc(n,k) = where n and k are non-negative integers returns n!/k!(n-k)!,
+%            i.e. the coefficient of the x^k term in the polynomial
+%            expansion of the binomial power (1 + x)^n. This is also the so
+%            called choose function of n and k (nchoosek in MATLAB), i.e.
+%            the number of k-element subsets (the k-combinations) of a set
+%            of n objects. When a coefficient is large, results may be
+%            inexact. The result is only accurate to 15 digits for
+%            double-precision inputs in 32bits computers.
 %
-% Ensure computations in doubles.
-%
-% See also the MATLAB function nchoosek
-%
-%<a href="matlab: docsearchFS('bc')">Link to the help page for this function</a>
+% See also: nchoosek
 %
 % Copyright 2008-2015.
-% Matlab function bc has been adapted to this toolbox by: 
-% FSDA team 
+% Matlab function bc has been adapted to this toolbox by FSDA team
 %
+%<a href="matlab: docsearchFS('bc')">Link to the help page for this function</a>
 %
 % Last modified 06-Feb-2015
 %
 % Examples:
-%
-%
 %{
-bc(6,2)
-% returns 15
+    %Number of pairs chosen among 6 elements (returns 15)
+    bc(6,2)
 %}
 
-
+% Ensure computations in doubles.
 n = double(n);
 k = double(k);
 
