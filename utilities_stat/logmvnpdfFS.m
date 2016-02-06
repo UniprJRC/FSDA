792fd7828b5130cd80ccbace2ab1d676d091aa1d
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2016-02-06 09:06:29 +0000

    Option msg has been added as final input and in addition help has been rewritten to comply with the parser
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@777 336e6153-a435-9040-aa94-c6aba0fb517b

--- utilities_stat/logmvnpdfFS.m
+++ utilities_stat/logmvnpdfFS.m
@@ -1,47 +1,95 @@
-function y = logmvnpdfFS(X, Mu, Sigma, X0, eyed, n, d)
-%logmvnpdfFS log of Multivariate normal probability density function (pdf)
+function y = logmvnpdfFS(X, Mu, Sigma, X0, eyed, n, d, msg)
+%logmvnpdfFS produces log of Multivariate normal probability density function (pdf)
 %
 %<a href="matlab: docsearchFS('logmvnpdffs')">Link to the help function</a>
 %
-% This function is a much faster version than (log of) Matlab function mvnpdf
+% This function is a much faster version than (log of) Matlab function
+% mvnpdf.  If this function is called without optional arguments than it
+% uses matlab function bsxfun to compute
+% the deviations form the means and no mex function.
+% If this function is called with the four optional input
+% arguments X0, eyed, n and d a mex function based on C code is directly used. 
+% Additional details follow: in order to compute the kernel of the quadratic form
+% at the exponent logmvnpdfFS creates an identity of size length(Mu) and
+% similarly needs to compute length(Mu). These two quantites can be
+% precalculated and supplied as input parameters. If logmvnpdfFS has to be
+% called thousands of times (as it happens for example in each iteration
+% of all procedures of cluster analysis based mixtures of multivariate
+% gaussian distributions). The same argument above applies to scalars n
+% and d which are directly passed to the compiled mex function
 %
-%   y = logmvnpdfFS(X,Mu,Sigma) returns the log-density of the multivariate normal
-%   distribution with mean Mu and covariance Sigma, evaluated at each row
-%   of X. This sintax uses matlab function bsxfun to compute
-%   the deviations form the means and no mex function
+%  Required input arguments:
 %
-%   y = logmvnpdfFS(X,Mu,Sigma,X0,eyed,n,d) must be used if logmvnpdfFS is
-%   repeatedly called. This sintax uses to compute the deviations from the
-%   means a mex function obtained using C code DfM.c
-%   In this case X0 is another matrix of the same size
-%   of X, eyed is an identity matrix of size length(Mu), n is a scalar
-%   associated to the number of rows of X and d is a scalar associated with
-%   the number of columns of X
 %
-%   The details are as follows:
-%   In order to compute the kernel of the quadratic form
-%   at the exponent logmvnpdfFS creates an identity of size length(Mu) and
-%   similarly needs to compute length(Mu). These two quantites can be
-%   precalculated and supplied as input parameters. If logmvnpdfFS has to be
-%   called thousands of times (as it happens for example in each iteration
-%   of all procedures of cluster analysis based mixtures of multivariate
-%   gaussian distributions). The same argument above applies to scalars n
-%   and d which are directly passed to the compiled mex function
+% X :           Input data. Scalar, Vector or matrix. 
+%               n x d data matrix; n observations and d variables. Rows of
+%               Y represent observations, and columns represent variables or coordinates.
+%               The (log of the) probability density of the multivariate
+%               normal distribution will be evaluated at each row of the
+%               n-by-d matrix Y
+%               Data Types - single|double
+% Mu:           mean mu of the multivariate normal distribution. 1-by-d vector. 
+%               Data Types - single|double
+% Sigma  :      covariance matrix of the multivariate normal distribution.
+%               d-by-d matrix.
+%               Data Types - single|double
 %
-%   In the examples below we compare the speed of the different solutions
+%  Optional input arguments:
 %
-%   <a href="matlab: docsearchFS('logmvnpdffs')">Link to the help function</a>
+%   X0  :       matrix of the same size of X which passes to C function a container. 
+%               Note that options X0, eyed, n, and d must be supplied
+%               together.
+%                 Example - 'X0',X
+%                 Data Types - single|double
+%  eyed :       identity matrix of size length(Mu) wchich passes to C function a container.
+%               Note that options X0, eyed, n, and d must be supplied
+%               together.
+%                 Example - 'eyed',eye(v)
+%                 Data Types - single|double
+%     n :       scalar which passes to C function size(X,1).
+%               Note that options X0, eyed, n, and d must be supplied
+%               together.
+%                 Example - 'eyed',eye(v)
+%                 Data Types - single|double
+%     d :       scalar which passes to C function length(Mu).
+%               Note that options X0, eyed, n, and d must be supplied
+%               together.
+%                 Example - 'eyed',eye(v)
+%                 Data Types - single|double
+%        msg  : Level of output to display. Scalar.
+%               Scalar which controls whether to display or not messages
+%               on the screen. If msg==1 (default) messages are displayed
+%               on the screen when cholesky of Sigma is impossibile 
+%               else no message is displayed on the screen. When Clolesky
+%               of Sigma is impossible -Inf output is returned.
+%                 Example - 'msg',1
+%                 Data Types - single | double
+%
+% Output:
+%
+%   y    :   log-density of the multivariate normal. Vector. Vector with length
+%               equal to n which returns the log-density of the multivariate normal
+%               distribution with mean Mu and covariance Sigma, evaluated at each row
+%               of X.  
+%
+% See also mvnpdf
+%
+% References:
+%
+%
 %
 % Copyright 2008-2015.
 % Written by FSDA team
 %
-%
+%   <a href="matlab: docsearchFS('logmvnpdffs')">Link to the help function</a>
 % Last modified 06-Feb-2015
 
 %   Examples:
-%
+
 %{
-     % In this example we check the agreement of the results with MATLAB function mvnpdf
+     % Comparisonw with mvnpdf.
+     % In this example we check the agreement of the results with MATLAB
+     % function mvnpdf.
      n=20000;
      X=randn(n,2);
      mu = [1 -1]; Sigma = [.9 .4; .4 .3];
@@ -51,9 +99,31 @@ function y = logmvnpdfFS(X, Mu, Sigma, X0, eyed, n, d)
      % 2.842e-14
 %}
 
+%{ 
+    Remark: Options X0, eyed, n, and d must be used together.
+%}
+
+%{ 
+%}
+
+%{ 
+%}
+
+%{ 
+%}
+
 %{
-% TIME COMPARISON USING TIC TOC
-% logmvnpdfFS with mex function and logmvnpdfFS without mex function
+    % Example of the use of option msg.
+    msg=0;
+    y = logmvnpdfFS(X,Mu,Sigma,X0,eyed,n,d,msg)
+    %  enables to control the display of the error message on the cov matrix
+
+%}
+
+%{
+    %% TIME COMPARISON USING TIC TOC.
+    % In the examples below we compare the speed of the different solutions
+    % logmvnpdfFS with mex function and logmvnpdfFS without mex function
 
     % In this code computation time is based on tic toc combined with a series
     % of replications
@@ -159,13 +229,13 @@ function y = logmvnpdfFS(X, Mu, Sigma, X0, eyed, n, d)
 %}
 
 %{
-    % TIME COMPARISON USING TIMEIT FUNCTION
-    % Remark: this function is present from  MATLAB 2013b
+    %% TIME COMPARISON USING TIMEIT FUNCTION.
+    % Remark: timeit function is present from  MATLAB 2013b
 
     if verLessThan('matlab', '8.2.0')
         warning('FSDA:logmvnpdfFS:MatlabTooOld','This example needs routine timeit which has been introduced in Matlab 2013b')
         warning('FSDA:logmvnpdfFS:MatlabTooOld','You have a version of Matlab which is < 2013b and you cannot run this example')
-    else   
+    else
         % nn = sample size
         % vv = number of variables
         nn=[100 200 500 1000 5000 10000 50000 100000];
@@ -192,11 +262,11 @@ function y = logmvnpdfFS(X, Mu, Sigma, X0, eyed, n, d)
                 tMat = timeit(yMat);
 
 
-                % logmvnpdfFS using mex file for mean deviations. 
+                % logmvnpdfFS using mex file for mean deviations.
                 yFSwithMex = @() logmvnpdfFS(X, Mu, Sigma,X0,eyed,n,v);
                 tFSwithMex = timeit(yFSwithMex);
 
-                % logmvnpdfFS and no mex file for mean deviations. 
+                % logmvnpdfFS and no mex file for mean deviations.
                 yFSnoMex = @() logmvnpdfFS(X, Mu, Sigma);
                 tFSnoMex = timeit(yFSnoMex);
 
@@ -213,7 +283,7 @@ function y = logmvnpdfFS(X, Mu, Sigma, X0, eyed, n, d)
         end
 
 
-        %% Plotting part
+        % Plotting part
         a=ver;
         a=a.Release;
         aa=1;
@@ -245,6 +315,7 @@ function y = logmvnpdfFS(X, Mu, Sigma, X0, eyed, n, d)
 %}
 
 
+
 %
 % See also mvnpdf
 %
@@ -267,26 +338,29 @@ else
     % n = scalar, number of observations (input parameter of function DfM)
     % d = number of variables (input parameter of function DfM)
     DfM(X,Mu,X0,n,d);
+    
 end
 
 % Take Choleski of Sigma
 [Sigma,err] = chol(Sigma);
 if err ~= 0
-    warning('FSDA:logmvnpdfFS:BadMatrixSigma','Cholesky of Sigma is impossible');
-    warning('FSDA:logmvnpdfFS:BadMatrixSigma','Zero output is returned');
-    y=0;
+    if nargin<8 || (nargin ==8 && msg==1)
+        warning('FSDA:logmvnpdfFS:BadMatrixSigma','Cholesky of Sigma is impossible');
+        warning('FSDA:logmvnpdfFS:BadMatrixSigma','-Inf output is returned');
+    end
+    y=  -Inf;
 else
-
-% Define the following value: d*log(2*pi)/2
-Fd = 0.918938533204673*d;
-
-% Compute log(sqrt(diag(Sigma))), and define a constant value.
-Const= sum(log(diag(Sigma)))+Fd;
-
-y = -0.5*sum((X0*(Sigma\eyed)).^2,2)- Const;
-
-% Note that the instruction above is slightly faster than
-% y = -0.5*sum((X*inv(Sigma)).^2,2)- Const; %#ok<MINV>
+    
+    % Define the following value: d*log(2*pi)/2
+    Fd = 0.918938533204673*d;
+    
+    % Compute log(sqrt(diag(Sigma))), and define a constant value.
+    Const= sum(log(diag(Sigma)))+Fd;
+    
+    y = -0.5*sum((X0*(Sigma\eyed)).^2,2)- Const;
+    
+    % Note that the instruction above is slightly faster than
+    % y = -0.5*sum((X*inv(Sigma)).^2,2)- Const; %#ok<MINV>
 end
 end
 
