61564edeac1d32922665b115a57ba28fc40fd521
domenico <domenico@336e6153-a435-9040-aa94-c6aba0fb517b>
2016-03-07 18:19:38 +0000

    Changes introduced to encounter new positioning needs. To be tested extensively.
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@813 336e6153-a435-9040-aa94-c6aba0fb517b

--- utilities/position.m
+++ utilities/position.m
@@ -1,64 +1,115 @@
 function position(plmain)
 %position controls the position of the open figures
 %
+%<a href="matlab: docsearchFS('position')">Link to the help function</a>
+%
+% A number of relevant FSDA plots are positioned according to a predefined
+% layout.
+% Reminder: the position property format is [left, bottom, width, height]
+%
 % Required input arguments:
 %
 % Optional input arguments:
 %
-%   plmain, the handle of a 'main' figure to be positioned at the top-left
-%   side of the screen, which is supposed to be the position attracting
-%   first the attention of a user. In absence of the option, plmain is set
-%   to be the smaller handle, which usually corresponds to the first
-%   created figure. 
+%   plmain:     Figure handle. Scalar. The handle of a 'main' figure to be
+%               positioned at the top-left side of the screen, which is
+%               supposed to be the position attracting first the attention
+%               of a user.
+%                -  If plmain not given, or it is empty, it is set to be
+%                   the smaller handle, which normally is the handle of the
+%                   first created figure.
+%                -  If plmain is set to zero (0), then function cascade is
+%                   applied.
 %
-% A number of relevant FSDA plots are positioned according to a predefined
-% layout. 
 %
-% Reminder: the position property format is [left, bottom, width, height]
+%  Output:
+%
+% See also cascade
+%
+% References:
 %
 % Copyright 2008-2015.
 % Written by FSDA team
 %
+%<a href="matlab: docsearchFS('position')">Link to the help function</a>
 %
 % Last modified 06-Feb-2015
-%
+% Examples:
+
+%{
+    % Three plots, with a recognized tag. They are organized properly in
+    % the screen area.
+
+    Y1=load('geyser2.txt');
+    Y2=load('fishery.txt');
+    Y3=load('M5data.txt');
+    figure('Tag','pl_spm'); spmplot(Y1); hmain=gcf;
+    figure('Tag','pl_spm'); spmplot(Y2);
+    figure('Tag','pl_spm'); spmplot(Y3);
+    position(hmain);
+%}
+%{
+    % The three plots have now an unknown tag. In this case, we assume that 
+    % the plots are not relevant and are therefore put in a non-interesting
+    % screen area (top-right).
+    
+    close all
+    figure('Tag','aaaaa'); spmplot(Y1); hmain=gcf;
+    figure('Tag','bbbbb'); spmplot(Y2);
+    figure('Tag',''); spmplot(Y3);
+    position(hmain);
+%}
 
 
 %% Beginning of code
 
-% find all open figures and if there are no figures do nothing. This check
-% is necessary because the figure with handle plmain could have been closed
-% before the call of position.
+% find all open figures
 openfigs = findobj(allchild(0),'flat','Visible','on');
-if isempty(openfigs),
+
+% in case of 0 or 1 figures, do nothing
+if isempty(openfigs) || length(double(openfigs))==1
     return
-else
-    if nargin == 0 
-        plmain = min(openfigs);
-    end
-    %Ensure root units are pixels and get the size of the screen
-    set(0,'Units','pixels') ;
-    scnsize = get(0,'ScreenSize');
-    scrwidth = scnsize(3); scrheight = scnsize(4); halfscrheight = scrheight/2;
-    % The size and coordinates of the plot under brushing are not used,
-    % because the width/height ratio is fixed.
-    % plmainpos = get(plmain,'Position');plmainwidth = plmainpos(3);
-    % plmainheight = plmainpos(4);
-    whratio=560/420;
-    % Set an extra space equal to the border width between the main figure
-    % and other figures
-    edge = 2;
-    % Fraction of the upper screen which will be occupied by the main plot
-    fraction = 0.5; 
 end
 
-%% Reposition the main figure. 
+%  if the input argument 'plmain' is zero, there is no 'main'
+%  figure and simple cascade is used.
+if nargin == 1 && plmain == 0
+    cascade;
+    return
+end
+
+if nargin == 0
+    %  If 'plmain' is not indicated (i.e. no input arguments) the 'main'
+    %  figure positioned at the top-left side of the screen is set to be
+    %  the one with smaller handle, which usually corresponds to the first
+    %  created figure.
+    plmain = min(double(openfigs));
+end
+
+%% set the geometry of the space where figures will be positioned
+
+%Ensure root units are pixels and get the size of the screen
+set(0,'Units','pixels') ;
+scnsize = get(0,'ScreenSize');
+scrwidth = scnsize(3); scrheight = scnsize(4); halfscrheight = scrheight/2;
+% The size and coordinates of the plot under brushing are not used,
+% because the width/height ratio is fixed.
+% plmainpos = get(plmain,'Position');plmainwidth = plmainpos(3);
+% plmainheight = plmainpos(4);
+whratio=560/420;
+% Set an extra space equal to the border width between the main figure
+% and other figures
+edge = 2;
+% Fraction of the upper screen which will be occupied by the main plot
+fraction = 0.5;
+
+%% Reposition the main figure.
 % The main figure is usually a plot under brushing
 % posmain = [1 halfscrheight halfscrheight*whratio halfscrheight];
 posmain = [edge,...
-            scrheight * (1-fraction),...
-            halfscrheight*whratio - edge,...
-            scrheight * fraction];
+    scrheight * (1-fraction),...
+    halfscrheight*whratio - edge,...
+    scrheight * fraction];
 set(plmain,'WindowStyle','normal','OuterPosition',posmain);
 
 %% Reposition all other figures
@@ -74,7 +125,7 @@ demo=((findobj(openfigs,'flat','Tag','demo')));
 if isempty(demo)
     demo=[];
 else
-%   newdemopos = [halfscrheight*whratio halfscrheight halfscrheight*whratio halfscrheight-50];
+    %   newdemopos = [halfscrheight*whratio halfscrheight halfscrheight*whratio halfscrheight-50];
     newdemopos = [posmain(3) + edge,...
         posmain(2),...
         posmain(3),...
@@ -82,19 +133,19 @@ else
     set(demo,'Position',newdemopos);
 end
 
-% Check the *linkable* FS plots that are open and get their handles
-% residuals. In findobj, "'type','figure'" is replaced with
-% "openfigs,'flat'" to accelerate the search of the right handle.
+% Check the *linkable* FS plots that are open and get their handles. In
+% findobj, "'type','figure'" is replaced with "openfigs,'flat'" to
+% accelerate the search of the right handle.
 
 % REGRESSION PLOTS
 % yXplot
 plyX=((findobj(openfigs,'flat','Tag','pl_yX')));
 if isempty(plyX), plyX=[]; end;
 % resfwdplot
-plres=((findobj(openfigs,'flat','Tag','pl_resfwd'))); 
+plres=((findobj(openfigs,'flat','Tag','pl_resfwd')));
 if isempty(plres), plres=[]; end;
 % resfwdplot
-plresindex=((findobj(openfigs,'flat','Tag','pl_resindex'))); 
+plresindex=((findobj(openfigs,'flat','Tag','pl_resindex')));
 if isempty(plresindex), plresindex=[]; end;
 % levfwdplot
 pllev=((findobj(openfigs,'flat','Tag','pl_levfwd')));
@@ -106,7 +157,7 @@ if isempty(plmdr), plmdr=[]; end;
 plfan=((findobj(openfigs,'flat','Tag','pl_fan')));
 if isempty(plfan), plfan=[]; end;
 
-% MULTIVARIATE PLOTS  
+% MULTIVARIATE PLOTS
 % malfwdplot
 plmalfwd=((findobj(openfigs,'flat','Tag','pl_malfwd')));
 if isempty(plmalfwd), plmalfwd=[]; end;
@@ -122,24 +173,50 @@ if isempty(plspm), plspm=[]; end;
 
 %Set the new position of the FS plots linked to that under brushing
 lkplots = horzcat(plmdr,plres,plresindex,plyX,pllev,plfan,...
-            plmalindex,plmalfwd,plmmd,plspm);
+    plmalindex,plmalfwd,plmmd,plspm);
 lkplots(lkplots==plmain)=[];
+
 nlkplots = length(lkplots);
-width = scrwidth / (nlkplots + 1);
-left = zeros(nlkplots,1);
-for i=1:nlkplots
-    left(i) = floor(scrwidth-(i*width));
-end
-fb = 30 ; %position from bottom
-switch nlkplots;
-    case 0
-    case 1
-        poslkplots = cat(2,left,[fb width scrheight/2-fb]);
-        set(lkplots,'OuterPosition',poslkplots);
-    otherwise
-        poslkplots = cat(2,left,repmat([fb width scrheight/2-fb],nlkplots,1));
-        cposlkplots=num2cell(poslkplots,2);
-        set(lkplots,{'OuterPosition'},cposlkplots);
+if nlkplots > 0
+    % the width of the plots
+    switch nlkplots
+        case 1
+            lkplots_pos = get(lkplots,'OuterPosition');
+            width = lkplots_pos(3);
+        case {2,3,4}
+            lkplots_pos = cell2mat(get(lkplots,'OuterPosition'));
+            lkplots_width = min(min(lkplots_pos(:,3)),ceil(scrwidth / nlkplots));
+            width = lkplots_width;
+        otherwise
+            width = ceil(scrwidth / 4);
+    end
+    heigth = ceil(width/whratio);
+    
+    % the left position of the plots depends on their width
+    left = ((1:nlkplots)'-1)*width;
+    left = mod(left,scrwidth);
+    
+    % set the displacement from the bottom of the screen
+    fb = 30 ;
+    if nlkplots > 1
+        fb = fb*ones(nlkplots,1);
+        for i=1:nlkplots
+            nrows = ceil(i/4);
+            fb(i) = min(nrows*fb(i)+heigth*(nrows-1) , heigth*3);
+        end
+    end
+    
+    % set the new positions
+    switch nlkplots;
+        case 1
+            poslkplots = cat(2,left,[fb width scrheight/2-fb]);
+            set(lkplots,'OuterPosition',poslkplots);
+        otherwise
+            poslkplots = cat(2,left,fb,repmat(width,nlkplots,1),repmat(heigth,nlkplots,1));
+            cposlkplots=num2cell(poslkplots,2);
+            set(lkplots,{'OuterPosition'},cposlkplots);
+    end
+    
 end
 
 %Set the new position and size of any other open plot
