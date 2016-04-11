dc94cab78aee5111fcc6614c14c3ce98fcde2207
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2016-04-11 10:38:15 +0000

    Modification to cope with Summary of failed tests: 11-Apr-2016 07:11:49 - Execution of C:\.jenkins\workspace\fsda-test\EXAMPLES_test\tclustIC_ex01.m FAILED: Undefined variable "matlab" or class "matlab.lang.makeValidName".
       disp('MIXMIX (BIC)')
        if verLessThan ('matlab','8.2.0');
            VariableNames={'c_1'; 'c_2'; 'c_4'; 'c_8'; 'c_16'; 'c_32'; 'c_64'; 'c_128'};
            disp(VariableNames')
            disp(out.MIXMIX)
            disp('Rows refers to k (number of groups') 
        else
            RowNames=cellstr([repmat('k=',length(out.kk),1) num2str(out.kk')]);
            VariableNames=matlab.lang.makeValidName(cellstr([repmat('c=',length(out.cc),1) num2str(out.cc')]));
            disp(array2table(out.MIXMIX,'VariableNames',VariableNames','RowNames',RowNames))
        end
    
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@846 336e6153-a435-9040-aa94-c6aba0fb517b

--- clustering/tclustIC.m
+++ clustering/tclustIC.m
@@ -292,10 +292,17 @@ function out  = tclustIC(Y,varargin)
     Y=load('geyser2.txt');
     out=tclustIC(Y,'cleanpool',false,'plots',1,'alpha',0.1)
     % Write out.MIXMIX as MATLAB table
-    VariableNames=matlab.lang.makeValidName(cellstr([repmat('c=',length(out.cc),1) num2str(out.cc')]));
-    RowNames=cellstr([repmat('k=',length(out.kk),1) num2str(out.kk')]);
     disp('MIXMIX (BIC)')
-    disp(array2table(out.MIXMIX,'VariableNames',VariableNames','RowNames',RowNames))
+    if verLessThan ('matlab','8.2.0');
+        VariableNames={'c_1'; 'c_2'; 'c_4'; 'c_8'; 'c_16'; 'c_32'; 'c_64'; 'c_128'};
+        disp(VariableNames')
+        disp(out.MIXMIX)
+        disp('Rows refers to k (number of groups') 
+    else
+        RowNames=cellstr([repmat('k=',length(out.kk),1) num2str(out.kk')]);
+        VariableNames=matlab.lang.makeValidName(cellstr([repmat('c=',length(out.cc),1) num2str(out.cc')]));
+        disp(array2table(out.MIXMIX,'VariableNames',VariableNames','RowNames',RowNames))
+    end
     % Find smallest value inside the table and write the corresponding
     % values of k (number of groups) and c (restriction factor)
     [valmin,indk]=min(out.MIXMIX);
@@ -450,7 +457,7 @@ elseif strcmp(whichIC,'MIXCLA')
 elseif strcmp(whichIC,'CLACLA')
     typeIC=0;
 else
-    warning('Supplied string for whichIC is not supported.')
+    warning('FSDA:tclustICsol:WrongOpt','Supplied string for whichIC is not supported.')
     error('FSDA:tclustICsol:WrongIC','Specified information criterion is not supported: possible values are ''MIXMIX'' , ''MIXCLA'',  ''CLACLA'', ''ALL''')
 end
 
