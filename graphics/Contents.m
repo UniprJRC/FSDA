15b27e9aded0bc0f913532af89f807c17bb20db8
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2016-05-13 09:57:43 +0000

    changed FScategory of tclustICplot
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@855 336e6153-a435-9040-aa94-c6aba0fb517b

--- graphics/Contents.m
+++ graphics/Contents.m
@@ -20,5 +20,5 @@
 %   resfwdplot   - Plots the trajectories of the scaled (squared) residuals monitored                            - VIS-Reg - 2016 May 09
 %   resindexplot - Plots the residuals from a regression analysis versus index number or any other variable      - VIS-Reg - 2016 May 09
 %   spmplot      - Produces an interactive scatterplot matrix with boxplots or histograms on the main diagonal   - VIS-Mult- 2016 May 09
-%   tclustICplot - Plots information criterion as a function of c and k                                          - VIS-Mult- 2016 May 09
+%   tclustICplot - Plots information criterion as a function of c and k                                          - VIS-Clu - 2016 May 12
 %   yXplot       - Produces an interactive scatterplot of y against each variable of X in the input dataset      - VIS-Reg - 2016 May 09
