2c5b3520ffc61dd4d5dd78956f2a260736f41a3b
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2016-07-25 06:57:23 +0000

    suite to wot with xml files added (first commit of xml suite)
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1040 336e6153-a435-9040-aa94-c6aba0fb517b

---
+++ utilities/mHelpGUI.m
@@ -0,0 +1,12 @@
+function mHelpGUI(FileName)
+%mHelpGUI calls the GUI which enables to modfify the Help for FileName
+% create structure out from input XML file
+InputStructForHelpGUI=xmlreadFS(FileName); %#ok<NASGU>
+[FSDAroot]=fileparts(which('docsearchFS.m'));
+
+fsep=filesep;
+fullpathFileName=[FSDAroot fsep 'helpfiles' fsep 'XML' fsep 'InputStructForHelpGUI'];
+save(fullpathFileName,'InputStructForHelpGUI')
+HelpGUI
+
+end
