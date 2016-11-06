6fd29ff760dcccff75265c28948f2b8838c21865
domenico <domenico@336e6153-a435-9040-aa94-c6aba0fb517b>
2016-11-06 10:44:58 +0000

    tclustreg: example with nsamp added.
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1176 336e6153-a435-9040-aa94-c6aba0fb517b

--- clustering/tclustreg.m
+++ clustering/tclustreg.m
@@ -356,18 +356,24 @@ function [out, varargout] = tclustreg(y,X,k,restrfact,alpha1,alpha2,varargin)
 %{
     % Generate mixture of regression using MixSimReg, with an average
     % overlapping at centroids =0.01. 
+    rng(372,'twister');
     p=3;
     k=2;
     Q=MixSimreg(k,p,'BarOmega',0.001);
     n=200;
     [y,X,id]=simdatasetreg(n,Q.Pi,Q.Beta,Q.S,Q.Xdistrib);
 
+    % plot the dataset
+    yXplot(y,X);
+
     % Generate the elemental subsets used in tclustreg once and for all.
     nsamp  = 100;
     ncomb  = bc(n,p);
     method = [10*ones(n/2,1); ones(n/2,1)]; % weighted sampling using weights in "method"
     msg    = 0;
-    C      = subsets(nsamp, n, p, ncomb, msg, method);
+    for i=1:k
+        C(:,(i-1)*p+1:i*p) = subsets(nsamp, n, p, ncomb, msg, method);
+    end
 
     % tclustreg using samples in C
     out=tclustreg(y,X,k,50,0.01,0.01,'nsamp',C);
