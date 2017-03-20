95db753fef187e964d3152fe776a9633386df569
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2017-03-20 15:21:59 +0000

    included verlessthan because this function uses strjoin which is present only if matlab >= 2013a
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1259 336e6153-a435-9040-aa94-c6aba0fb517b

--- utilities/wraptextFS.m
+++ utilities/wraptextFS.m
@@ -140,49 +140,58 @@ function strFormatted = wraptextFS(str, varargin)
     comment.startcolumn=5;
     startcolumn=12;
     width=60;
-    Newstr=wraptextFS(str,'comment',comment,'startcolumn',startcolumn,'width',width);
+     if verLessThan('matlab','8.1') ==1
+        warning('This function uses routine strjoin and works just with Matlab >=2013a')
+    else
+       Newstr=wraptextFS(str,'comment',comment,'startcolumn',startcolumn,'width',width);
+    end
 %}
 
 %{ 
     % Example of use of option code.
-    out=xmlreadFS('tclust');
-    ii=2;
-    startcolumnEx=5;
-    endcolumn=60;
-    Ex=out.Ex;
-    comment=struct;
-    comment.commentsign='%';
-    comment.startcolumn=startcolumnEx;
-    endcolumnEx=60;
-    i=2; jj=3;
-    Exi=strtrim(Ex{i,jj});
-    Exi{3,1}='%                        This is an example with extra spaces on the left which are trimmed';
-    Exi{10,1}='for i=1:10';
-    Exi{11,1}='    disp(i)';  
-    % In this case the extra space on the left is wanted and it is not deleted
-    Exi{12,1}='end';
+    if verLessThan('matlab','8.1') ==1
+        warning('This function uses routine strjoin and works just with Matlab >=2013a')
+    else
+        out=xmlreadFS('tclust');
+        ii=2;
+        startcolumnEx=5;
+        endcolumn=60;
+        Ex=out.Ex;
+        comment=struct;
+        comment.commentsign='%';
+        comment.startcolumn=startcolumnEx;
+        endcolumnEx=60;
+        i=2; jj=3;
+        Exi=strtrim(Ex{i,jj});
+        Exi{3,1}='%                        This is an example with extra spaces on the left which are trimmed';
+        Exi{10,1}='for i=1:10';
+        Exi{11,1}='    disp(i)';  
+        % In this case the extra space on the left is wanted and it is not deleted
+        Exi{12,1}='end';
 
-    Eximod=Exi;
-    for ii=1:size(Exi,1)
-        % We must check whether it is comment or not
-        % If it is a comment if the first character is symbol %
-        Exii=Exi{ii,1};
-        if ~isempty(Exii)
-            if strcmp(Exii(1),'%')
-                % In this case strtrim is invoked inside wraptextFS (code is
-                % false)
-                descriFormatted=wraptextFS( Exii(2:end),'startcolumn',startcolumnEx,'endcolumn',endcolumn,'firstline',false,'comment',comment,'code',false);
-            else
-                descriFormatted=wraptextFS( Exii,'startcolumn',startcolumnEx,'endcolumn',endcolumnEx,'firstline',false,'comment',false,'code',true);
+        Eximod=Exi;
+        for ii=1:size(Exi,1)
+            % We must check whether it is comment or not
+            % If it is a comment if the first character is symbol %
+            Exii=Exi{ii,1};
+            if ~isempty(Exii)
+                if strcmp(Exii(1),'%')
+                    % In this case strtrim is invoked inside wraptextFS (code is
+                    % false)
+                    descriFormatted=wraptextFS( Exii(2:end),'startcolumn',startcolumnEx,'endcolumn',endcolumn,'firstline',false,'comment',comment,'code',false);
+                else
+                    descriFormatted=wraptextFS( Exii,'startcolumn',startcolumnEx,'endcolumn',endcolumnEx,'firstline',false,'comment',false,'code',true);
+                end
             end
+            Eximod{ii,1}=descriFormatted;
         end
-        Eximod{ii,1}=descriFormatted;
+
+        % Before formatting
+        disp(Exi)
+        % After formatting
+        disp([Eximod{:}])
     end
 
-    % Before formatting
-    disp(Exi)
-    % After formatting
-    disp([Eximod{:}])
 %}
 
 %% Input parameters checking
