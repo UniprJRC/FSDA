320a04cc0bd3338b9d1223243cc65fd707abf642
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2017-05-18 13:43:22 +0000

    Messaggio aggiunto con collegamento ipertestuale nel caso di missing help folder inside matla folder
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1353 336e6153-a435-9040-aa94-c6aba0fb517b

--- utilities/docrootFS.m
+++ utilities/docrootFS.m
@@ -10,16 +10,30 @@ if str2double(a.Version)>7.14
     if chkFSDAinsideMatlab ~=7
         FileName='addFSDA2path';
         FullPath=which(FileName);
-        FSDAhelproot=[FullPath(1:end-length(FileName)-3) filesep 'helpfiles' filesep 'FSDA'];
+        FSDAroot=FullPath(1:end-length(FileName)-3);
+        FSDAhelproot=[FSDAroot filesep '_tmp_helpfiles' filesep 'FSDA'];
         
         sprintf('----------------------------------------------------------')
-        fprintf('%s\n\n',['FSDA HTML help files must be copied inside path ' destinationpathFSDAdoc]);
-        msg1=['The FSDA help files have not been copied under the MATLAB help folder: ' docroot '.'];
-        msg2='  Please check the user right permissions and possibly copy manually the folder  ';
-        msg3='  in the MATLAB help folder';
-        msg=regexprep([msg1 msg2 FSDAhelproot msg3],'\\','/');
+        fprintf('%s\n\n',['FSDA HTML help files must be copied inside path "' destinationpathFSDAdoc '"']);
+        msg1=['The FSDA help files have not been copied under the MATLAB help folder: "' docroot '".'];
+        msg2='  Please check the user right permissions and possibly move manually the folder "';
+        msg3='"  in the MATLAB help folder. For additional details please see file: ';
+        msg4=[FSDAroot filesep 'InstallationNotes.pdf'];
+        
+        msg=regexprep([msg1 msg2 FSDAhelproot msg3 msg4],'\\','/');
         warning('FSDA:docrootFS:wrongSetUp',msg)
-        mydialog(msg)
+        h=mydialog(msg);
+        import com.mathworks.mlwidgets.html.HTMLRenderer;
+        % create component
+        r = HTMLRenderer;
+        % set the text to display
+        r.setHtmlText(['<html> <a href="' msg4 '">Installation Notes</a></html>']);
+        % make sure the component is opaque
+        r.setOpaque(true);
+        % add the component
+        javacomponent(r, [120 49 100 30], h);
+        %[left, bottom, width, height]
+        
         error('FSDA:docrootFS:wrongSetUp','Please read warning above and take appropriate action!')
     else
     end
@@ -40,16 +54,16 @@ end
 
 end
 
-function mydialog(msg)
-    d = dialog('Position',[300 300 350 150],'Name','FSDA warning message');
+function d=mydialog(msg)
+d = dialog('Position',[300 300 380 250],'Name','FSDA warning message');
 
-     uicontrol('Parent',d,...
-               'Style','text',...
-               'Position',[20 80 310 60],...
-               'String',msg);
+uicontrol('Parent',d,...
+    'Style','text',...
+    'Position',[20 80 330 155],...
+    'String',msg,'FontSize',11);
 
-     uicontrol('Parent',d,...
-               'Position',[135 20 70 25],...
-               'String','Close',...
-               'Callback','delete(gcf)');
+uicontrol('Parent',d,...
+    'Position',[135 20 70 25],...
+    'String','Close',...
+    'Callback','delete(gcf)');
 end
