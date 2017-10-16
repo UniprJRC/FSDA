c2e71054e6780bceab6b90c8c219b3a208202a5c
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2017-10-16 15:09:19 +0000

    this file had never been submitted before
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1457 336e6153-a435-9040-aa94-c6aba0fb517b

---
+++ utilities_help/Contents.m
@@ -0,0 +1,13 @@
+% UTILITIES_HELP
+%
+% File names, description, category and date last modified
+%
+%   Name                   - Description                                                                                             - Category- Date last modified
+%------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+%   htmlwriteFS            - Enables to create automatic HELP FILES from a specific MATLAB structure created with function mreadFS.m - UTIHELP- 2017 May 12
+%   makecontentsfileFS     - Extends Matlab function makecontentsfile                                                                - UTIHELP- 2017 May 26
+%   mreadFS                - Enables to create a structure with InputArgs/OptArgs/OutArgs ... from .m function files                 - UTIHELP- 2017 May 26
+%   publishFS              - Enables to create automatic HELP FILES from structured .m function files                                - UTIHELP- 2017 Jul 05
+%   publishFunctionAlpha   - Enables to create web page which contains the alphabetical list of functions                            - UTIHELP- 2016 Aug 01
+%   publishFunctionCate    - Enables to create web page which contains the categorical list of functions                             - UTIHELP- 2017 Sep 21
+%   xmlcreateFS            - Create an XML file passing through publishFS                                                            - UTIHELP- 2017 May 03
