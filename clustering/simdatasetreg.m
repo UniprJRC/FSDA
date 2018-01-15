f8b078203ec44a2359ba9073316a632dbacef41a
domenico <domenico@336e6153-a435-9040-aa94-c6aba0fb517b>
2018-01-15 15:58:38 +0000

    Added example on simdatasetreg, to demonstrate the use with user defined X data
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1602 336e6153-a435-9040-aa94-c6aba0fb517b

--- clustering/simdatasetreg.m
+++ clustering/simdatasetreg.m
@@ -464,6 +464,55 @@ function [y,X,id]=simdatasetreg(n, Pi, Beta, S, Xdistrib, varargin)
      suplabel('4 groups with outliers componentwise and from uniform in the interval [-2 3]','t')
 %}
 
+%{
+    %% Example with user defined explanatory variables values.
+
+    clear all
+    close all
+
+    rng(1234,'twister');
+
+    % mixture parameters
+    intercept = 0; % 1/0 = intercept yes/no
+    p=1+intercept;
+    k=2;
+    n=200;
+
+    % beta distributed as halfnormal
+    betadistrib=struct;
+    betadistrib.type='HalfNormal';
+    betadistrib.sigma=3;
+
+    % explanatory variables distribution chosen by the User from a beta 
+    XdistribB=struct;
+    XdistribB.intercept=intercept;
+    XdistribB.type='User';
+
+    X1=random('beta',1,10,n,1);            % data generation: user distribution is a beta
+    XdistribB.BarX = mean(X1);             % mean of the generated data
+
+    % overlap level baromega: chosen at random here, in a given range 
+    mino = 0.01; maxo = 0.1;
+    baromega = mino + (maxo-mino).*rand(1,1);
+
+    % estimated mixsim parameters
+    Q=MixSimreg(k,p,'BarOmega',baromega,'Xdistrib',XdistribB,'betadistrib',betadistrib);
+
+    % Simulate the data from the mixim parameters and the user values for X
+    if intercept
+        Q.Xdistrib.X = [ones(k*n,1) X1];
+    else
+        Q.Xdistrib.X = X1;
+    end
+
+    [y,X,id]=simdatasetreg(n,Q.Pi,Q.Beta,Q.S,Q.Xdistrib);
+    yXplot(y,X,'group',id,'tag','X_beta');
+    set(gcf,'Name','X Beta distributed');
+    title('User-defined distribution for X');
+
+
+%}
+
 
 
 %% Beginning of code
