19d4a46e151fa8d882700d677367b010b47324b4
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2018-04-14 08:26:25 +0000

    Added to forecastTS.m the examples of simulated data used in the paper
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1668 336e6153-a435-9040-aa94-c6aba0fb517b

--- regression/forecastTS.m
+++ regression/forecastTS.m
@@ -373,6 +373,42 @@ function [outFORE] = forecastTS(outEST,varargin)
     outFORE=forecastTS(out,'model',model,'nfore',nfore,'StartDate',StartDate,'conflev',conflev);
 %}
 
+%{
+    % Example with simulated data.
+    % Simulate data with linear trend, time varying seasonal component, and
+    % level shift
+    rng(1)
+    model=struct;
+    model.trend=1;
+    model.trendb=[1 1];
+    model.seasonal=103;
+    model.seasonalb=40*[0.5 -0.5 0.3 -0.3 0.1 -0.1 0.222];
+    model.lshift=40;
+    model.lshiftb=13000;
+    model.signal2noiseratio=20;
+    T=150;
+    FileNameOutput=[pwd filesep 'ysimout.txt'];
+    outSIM=simulateTS(T,'model',model,'FileNameOutput',FileNameOutput,...
+        'plots',true,'samescale',true);
+    y=outSIM.y;
+    % Data contamination
+    y(131:140)=y(131:140)-29000;
+
+    % Estimation
+    modelEST=struct;
+    modelEST.trend=1;
+    modelEST.seasonal=103;
+    modelEST.lshift=30;
+    outEST=LTSts(y,'model',modelEST,'dispresults',true,'plots',0);
+
+    % Forecasting
+    % nfore= number of forecasts;
+    nfore=36;
+    % Forecasts with a 99.9 per cent confidence level
+    OUTfore=forecastTS(outEST,'model',modelEST,'nfore',nfore,'conflev',0.999);
+%}
+
+
 %% Beginning of code
 if nargin<1
     error('FSDA:forecastTS:MissingInputs','Input structure is missing');
