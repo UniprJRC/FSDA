ddb58d50e21dba23446e99e0fa5e8b28e851c03c
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2018-04-23 11:55:10 +0000

    isnan control added in fsRmdr to cope with
    ummary of failed tests:
    23-Apr-2018 10:27:55 - Execution of C:\.jenkins\workspace\fsda-test\EXAMPLES_test\FSRmdr_ex09.m FAILED: Index in position 2 exceeds array bounds (must not exceed 1).
    
    
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1674 336e6153-a435-9040-aa94-c6aba0fb517b

--- regression/FSRmdr.m
+++ regression/FSRmdr.m
@@ -283,6 +283,7 @@ function [mdr,Un,BB,Bols,S2] = FSRmdr(y,X,bsb,varargin)
     [mdr,Un,BB,Bols,S2] = FSRmdr(y,X,out.bs,'nocheck',1);
 %}
 
+
 %{
     %% Monitoring of s2, and the evolution of beta coefficients for the Hawkins dataset
     load('hawkins.txt');
@@ -290,30 +291,34 @@ function [mdr,Un,BB,Bols,S2] = FSRmdr(y,X,bsb,varargin)
     X=hawkins(:,1:8);
     [out]=LXS(y,X,'nsamp',10000);
     [~,~,~,Bols,S2] = FSRmdr(y,X,out.bs);
-    %The forward plot of s2 shows that initially the estimate is virtually
-    %zero. The four line segments comprising the plot correspond to the four
-    %groups of observations
+    if isnan(S2)
+        disp('NoFullRank in initial subset, please rerun FSRmdr')
+    else
+        %The forward plot of s2 shows that initially the estimate is virtually
+        %zero. The four line segments comprising the plot correspond to the four
+        %groups of observations
 
-    % Plot of the monitoring of S2 and R2
-    figure;
-    subplot(1,2,1)
-    plot(S2(:,1),S2(:,2))
-    xlabel('Subset size m');
-    ylabel('S2');
-    subplot(1,2,2)
-    plot(S2(:,1),S2(:,3))
-    xlabel('Subset size m');
-    ylabel('R2');
-
-    %The forward plots of the estimates of the beta coefficients show that they are virtually constant until m = 86, after which they start fluctuating in different directions.
-
-    % Plots of the monitoring of "Estimates of beta coefficients"
-    figure;
-    for j=3:size(Bols,2);
-        subplot(2,4,j-2)
-        plot(Bols(:,1),Bols(:,j))
+        % Plot of the monitoring of S2 and R2
+        figure;
+        subplot(1,2,1)
+        plot(S2(:,1),S2(:,2))
         xlabel('Subset size m');
-        ylabel(['b' num2str(j-2)]);
+        ylabel('S2');
+        subplot(1,2,2)
+        plot(S2(:,1),S2(:,3))
+        xlabel('Subset size m');
+        ylabel('R2');
+
+        %The forward plots of the estimates of the beta coefficients show that they are virtually constant until m = 86, after which they start fluctuating in different directions.
+
+        % Plots of the monitoring of "Estimates of beta coefficients"
+        figure;
+        for j=3:size(Bols,2)
+            subplot(2,4,j-2)
+            plot(Bols(:,1),Bols(:,j))
+            xlabel('Subset size m');
+            ylabel(['b' num2str(j-2)]);
+        end
     end
 %}
 
@@ -328,13 +333,17 @@ function [mdr,Un,BB,Bols,S2] = FSRmdr(y,X,bsb,varargin)
     rng(100)
     [out]=LXS(y,X,'nsamp',10000);
     [mdr,Un,BB,Bols,S2] = FSRmdr(y,X,out.bs,'bsbsteps',[30 60]);
-    % BB has just two columns
-    % First column contains information about units forming subset at step m=30
-    % sum(~isnan(BB(:,1))) is 30
-    % Second column contains information about units forming subset at step m=60
-    % sum(~isnan(BB(:,2))) is 60
-    disp(sum(~isnan(BB(:,1))))
-    disp(sum(~isnan(BB(:,2))))
+    if isnan(S2)
+        disp('NoFullRank in initial subset, please rerun FSRmdr')
+    else
+        % BB has just two columns
+        % First column contains information about units forming subset at step m=30
+        % sum(~isnan(BB(:,1))) is 30
+        % Second column contains information about units forming subset at step m=60
+        % sum(~isnan(BB(:,2))) is 60
+        disp(sum(~isnan(BB(:,1))))
+        disp(sum(~isnan(BB(:,2))))
+    end
 %}
 
 
