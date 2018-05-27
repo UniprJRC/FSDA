a84eca2cbe57e545455d3610e6979e3ddb2dc658
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2018-05-27 20:23:29 +0000

    option axesellipse added as additional argument
    % axesellipse   : axes of the ellipse. Boolean. If axes is true (default)
    %                 dottted lines along the major axes of the ellipse are
    %                 drawn else just the ellipse contour appears.
    %                    Example - 'axesellipse', false
    %                    Data Types - Boolean
    
    
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1704 336e6153-a435-9040-aa94-c6aba0fb517b

--- utilities_stat/ellipse.m
+++ utilities_stat/ellipse.m
@@ -1,4 +1,4 @@
-function [Ell , he] = ellipse(mu, Sigma, conflev, Color)
+function [Ell , he] = ellipse(mu, Sigma, conflev, Color, axesellipse)
 %ellipse generates an ellipse given mu (location vector) and Sigma (scatter matrix)
 %
 %
@@ -13,35 +13,40 @@ function [Ell , he] = ellipse(mu, Sigma, conflev, Color)
 %
 % Required input arguments:
 %
-% mu    : Center of the ellipse. Vector. 
+% mu    : Center of the ellipse. Vector.
 %         Vector with two elements associated with the center of the
 %         ellipse
 % Sigma : 2 x 2 symmetric positive definite matrix. Matrix. Inverse of the
-%         matrix of the quadratic form which defines the equation of the 
-%         ellipse. Sigma is interpretable as the covariance matrix of the 
+%         matrix of the quadratic form which defines the equation of the
+%         ellipse. Sigma is interpretable as the covariance matrix of the
 %         original data points.
 %
 % Optional input arguments:
 %
-%              conflev : Confidence level. Scalar. 
-%                        Confidence level which controls the size of the ellipse.
-%                        If conflev is not specified the value 
-%                        chi2inv(0.95,2) is used.
-%                           Example - 'conflev', 0.99 
-%                           Data Types - single | double
+%       conflev : Confidence level. Scalar.
+%                 Confidence level which controls the size of the ellipse.
+%                 If conflev is not specified the value
+%                 chi2inv(0.95,2) is used.
+%                    Example - 'conflev', 0.99
+%                    Data Types - single | double
 %
-%              Color   : LineColor of the ellipse. String or 3 elements numeric vector.
-%                        Line color, specified as an RGB triplet, a color
-%                        string, or 'none'. If you specify the Color as
-%                        'none', then the line is invisible.
-%                        An RGB triplet is a three-element row vector whose
-%                        elements specify the intensities of the red,
-%                        green, and blue components of the color. The
-%                        intensities must be in the range [0,1], for
-%                        example, [0.4 0.6 0.7]. 
-%                           Example - 'Color', 'r'
-%                           Data Types - [0 0 1] (default) | RGB triplet | color string | 'none'
-%                           
+%       Color   : LineColor of the ellipse. String or 3 elements numeric vector.
+%                 Line color, specified as an RGB triplet, a color
+%                 string, or 'none'. If you specify the Color as
+%                 'none', then the line is invisible.
+%                 An RGB triplet is a three-element row vector whose
+%                 elements specify the intensities of the red,
+%                 green, and blue components of the color. The
+%                 intensities must be in the range [0,1], for
+%                 example, [0.4 0.6 0.7].
+%                    Example - 'Color', 'r'
+%                    Data Types - [0 0 1] (default) | RGB triplet | color string | 'none'
+% axesellipse   : axes of the ellipse. Boolean. If axes is true (default)
+%                 dottted lines along the major axes of the ellipse are
+%                 drawn else just the ellipse contour appears.
+%                    Example - 'axesellipse', false
+%                    Data Types - Boolean
+%
 %
 %  Output:
 %
@@ -55,6 +60,7 @@ function [Ell , he] = ellipse(mu, Sigma, conflev, Color)
 %                 modify properties of a specific chart line of the plot
 %                 containing the ellipse after it is created. For a list of
 %                 properties, see Chart Line Properties.
+%
 %
 % See also: ellipsoid
 %
@@ -83,7 +89,7 @@ function [Ell , he] = ellipse(mu, Sigma, conflev, Color)
 %}
 
 %{
-    %% Draw the ellipse using a blue color line. 
+    %% Draw the ellipse using a blue color line.
     close all
     rho=-2;
     A=[4 rho; rho 3 ];
@@ -94,16 +100,17 @@ function [Ell , he] = ellipse(mu, Sigma, conflev, Color)
 
 %{
     %% Draw an ellipse and fill it with yellow color.
-    close all 
+    close all
     rho=-2;
     A=[4 rho; rho 3 ];
     mu=[1.5 1];
     Ell=ellipse(mu,A);
-    patch(Ell(:,1),Ell(:,2),'y'); 
+    patch(Ell(:,1),Ell(:,2),'y');
 %}
 
 %{
-    %% Generate 1000 bivariate normal data and add the ellipse which
+    %% 99 per cent confidence ellipse.
+    % Generate 1000 bivariate normal data and add the ellipse which
     % contains approximately 990 of them.
     rng('default')
     rng(20)     % For reproducibility
@@ -112,7 +119,7 @@ function [Ell , he] = ellipse(mu, Sigma, conflev, Color)
     Sigma = [1,1.5;1.5,3];
     Y = mvnrnd(mu,Sigma,1000);
     figure
-    hold on; 
+    hold on;
     plot(Y(:,1),Y(:,2),'o');
     % add an ellipse to these points
     Ell=ellipse(mu,Sigma,0.99);
@@ -121,6 +128,31 @@ function [Ell , he] = ellipse(mu, Sigma, conflev, Color)
     disp('Number of points inside the ellipse')
     disp(sum(inpolygon(Y(:,1),Y(:,2),Ell(:,1),Ell(:,2))))
 %}
+
+%{
+    % 99 per cent confidence ellipse without showing the major axes.
+    % First close all previous plots
+    close all
+    % Generate 1000 bivariate normal data and add the ellipse which
+    % contains approximately 990 of them.
+    rng('default')
+    rng(20)     % For reproducibility
+    % Define mu and Sigma
+    mu = [2,3];
+    Sigma = [1,1.5;1.5,3];
+    Y = mvnrnd(mu,Sigma,1000);
+    figure
+    hold on;
+    plot(Y(:,1),Y(:,2),'o');
+    % add an ellipse to these points and do not show the major axes
+    AxesEllipse = false;
+    Ell=ellipse(mu,Sigma,0.99,[],AxesEllipse);
+    axis equal
+    % Count number of points inside the ellipse
+    disp('Number of points inside the ellipse')
+    disp(sum(inpolygon(Y(:,1),Y(:,2),Ell(:,1),Ell(:,2))))
+%}
+
 
 
 %% Beginning of code
@@ -161,17 +193,17 @@ if Lam(1,1)>Lam(2,2)
 end
 
 %disp(Gam)
- for j=1:2
-     if Gam(1,j)<0 && Gam(2,j)<0
+for j=1:2
+    if Gam(1,j)<0 && Gam(2,j)<0
         Gam(:,j)=-Gam(:,j);
-     else   
+    else
         if Gam(1,j)*Gam(2,j)<0
-           if Gam(1,j)*Sigma(1,2)>0 
-               Gam(:,j)=-Gam(:,j);
-           end
+            if Gam(1,j)*Sigma(1,2)>0
+                Gam(:,j)=-Gam(:,j);
+            end
         end
-     end
- end
+    end
+end
 % Note that the eigenvalues of matrix Sigma^-1
 % are simply 1/Lam(1,1) and 1/Lam(2,2)
 % The length of the semiaxis of the ellipse are simply
@@ -197,17 +229,20 @@ Ell=bsxfun(@plus,X, mu);
 % hold('on')
 he = plot(Ell(:,1),Ell(:,2),'Color',Color,'LineWidth',LineWidth);
 
-% Add line associated with major axis
-ax1=[-lenax1 0; lenax1 0];
-ax1ori=ax1*Gam;
-ax1ori=bsxfun(@plus,ax1ori, mu);
-line(ax1ori(:,1),ax1ori(:,2),'Color',Color,'LineWidth',LineWidth-1,'LineStyle','--');
-
-% Add line associated with minor axis
-ax2=[0 -lenax2;0  lenax2];
-ax2ori=ax2*Gam;
-ax2ori=bsxfun(@plus,ax2ori, mu);
-line(ax2ori(:,1),ax2ori(:,2),'Color',Color,'LineWidth',LineWidth-1,'LineStyle','--');
+if nargin<5 || (nargin ==5 &&   axesellipse == true)
+    
+    % Add line associated with major axis
+    ax1=[-lenax1 0; lenax1 0];
+    ax1ori=ax1*Gam;
+    ax1ori=bsxfun(@plus,ax1ori, mu);
+    line(ax1ori(:,1),ax1ori(:,2),'Color',Color,'LineWidth',LineWidth-1,'LineStyle','--');
+    
+    % Add line associated with minor axis
+    ax2=[0 -lenax2;0  lenax2];
+    ax2ori=ax2*Gam;
+    ax2ori=bsxfun(@plus,ax2ori, mu);
+    line(ax2ori(:,1),ax2ori(:,2),'Color',Color,'LineWidth',LineWidth-1,'LineStyle','--');
+end
 
 % axis equal
 
@@ -221,9 +256,9 @@ line(ax2ori(:,1),ax2ori(:,2),'Color',Color,'LineWidth',LineWidth-1,'LineStyle','
 %% Alternative code
 
 % deter=Sigma(1,1)*Sigma(2,2)-Sigma(1,2)^2;
-% 
+%
 % ylimit=sqrt(chi2inv(conflev,2)*Sigma(2,2));
-% 
+%
 % y=-ylimit:0.005*ylimit:ylimit;
 % sqtdi=sqrt(deter*(ylimit^2-y.^2))/Sigma(2,2);
 % sqtdi([1,end])=0;
