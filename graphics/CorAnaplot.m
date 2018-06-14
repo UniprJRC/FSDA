098fb48f18c65044e8689d0ca52b7ede9f071943
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2018-06-14 10:55:34 +0000

    Commit to cope with
    Summary of failed tests:
    11-Jun-2018 04:56:18 - Execution of C:\.jenkins\workspace\fsda-test\EXAMPLES_test\CorAnaplot_ex02.m FAILED: Undefined function or variable 'out'.
    11-Jun-2018 04:56:18 - Execution of C:\.jenkins\workspace\fsda-test\EXAMPLES_test\CorAnaplot_ex03.m FAILED: Undefined function or variable 'out'.
    
    
    
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1754 336e6153-a435-9040-aa94-c6aba0fb517b

--- graphics/CorAnaplot.m
+++ graphics/CorAnaplot.m
@@ -351,6 +351,7 @@ function CorAnaplot(out,varargin)
 
 %{
     %% CorAnaplot with all the default options.
+    % Prepare the data.
     N=[51	64	32	29	17	59	66	70;
         53	90	78	75	22	115	117	86;
         71	111	50	40	11	79	88	177;
@@ -396,16 +397,65 @@ function CorAnaplot(out,varargin)
         Sup.Lr=rowslab(15:end);
         Sup.c=N(1:14,6:8);
         Sup.Lc=colslab(6:8);
-        
-        out=CorAna(Nactive,'Sup',Sup,'plots',0,'dispresults',false);
-        % Rows and columns are showN in principal coordinates
-        CorAnaplot(out)
     end
+    % Compute correpondence analysis
+    out=CorAna(Nactive,'Sup',Sup,'plots',0,'dispresults',false);
+    % Show the correspodence analysis plot.
+    % Rows and columns are showN in principal coordinates
+    CorAnaplot(out)
 %}
 
 %{
     %% CorAnaplot with personalized symbols.
     close all
+    % Prepare the data.
+    N=[51	64	32	29	17	59	66	70;
+        53	90	78	75	22	115	117	86;
+        71	111	50	40	11	79	88	177;
+        1	7	5	5	4	9	8	5;
+        7	11	4	3	2	2	17	18;
+        7	13	12	11	11	18	19	17;
+        21	37	14	26	9	14	34	61;
+        12	35	19	6	7	21	30	28;
+        10	7	7	3	1	8	12	8;
+        4	7	7	6	2	7	6	13;
+        8	22	7	10	5	10	27	17;
+        25	45	38	38	13	48	59	52;
+        18	27	20	19	9	13	29	53;
+        35	61	29	14	12	30	63	58;
+        2	4	3	1	4	nan  nan	nan	  ;
+        2	8	2	5	2	nan  nan	nan;
+        1	5	4	6	3	nan  nan	nan;
+        3	3	1	3	4	nan  nan	nan];
+    % rowslab = cell containing row labels
+    rowslab={'money','future','unemployment','circumstances',...
+        'hard','economic','egoism','employment','finances',...
+        'war','housing','fear','health','work','comfort','disagreement',...
+        'world','to_live'};
+    % colslab = cell containing column labels
+    colslab={'unqualified','cep','bepc','high_school_diploma','university',...
+        'thirty','fifty','more_fifty'};
+    if verLessThan('matlab','8.2.0')==0
+        tableN=array2table(N,'VariableNames',colslab,'RowNames',rowslab);
+        % Extract just active rows
+        Nactive=tableN(1:14,1:5);
+        Nsupr=tableN(15:18,1:5);
+        Nsupc=tableN(1:14,6:8);
+        Sup=struct;
+        Sup.r=Nsupr;
+        Sup.c=Nsupc;
+        % Compute Correspondence analysis
+    else
+        Nactive=N(1:14,1:5);
+        Lr=rowslab(1:14);
+        Lc=colslab(1:5);
+        Sup=struct;
+        Sup.r=N(15:end,1:5);
+        Sup.Lr=rowslab(15:end);
+        Sup.c=N(1:14,6:8);
+        Sup.Lc=colslab(6:8);
+    end
+
     % Six-pointed star (hesagram) for supplementary rows
     SymbolRows='h';
     % Five-pointed star (pentagram) for supplementary rows
@@ -449,6 +499,53 @@ function CorAnaplot(out,varargin)
 %{
     % CorAnaplot with personalized displacement.
     close all
+    % Prepare the data.
+    N=[51	64	32	29	17	59	66	70;
+        53	90	78	75	22	115	117	86;
+        71	111	50	40	11	79	88	177;
+        1	7	5	5	4	9	8	5;
+        7	11	4	3	2	2	17	18;
+        7	13	12	11	11	18	19	17;
+        21	37	14	26	9	14	34	61;
+        12	35	19	6	7	21	30	28;
+        10	7	7	3	1	8	12	8;
+        4	7	7	6	2	7	6	13;
+        8	22	7	10	5	10	27	17;
+        25	45	38	38	13	48	59	52;
+        18	27	20	19	9	13	29	53;
+        35	61	29	14	12	30	63	58;
+        2	4	3	1	4	nan  nan	nan	  ;
+        2	8	2	5	2	nan  nan	nan;
+        1	5	4	6	3	nan  nan	nan;
+        3	3	1	3	4	nan  nan	nan];
+    % rowslab = cell containing row labels
+    rowslab={'money','future','unemployment','circumstances',...
+        'hard','economic','egoism','employment','finances',...
+        'war','housing','fear','health','work','comfort','disagreement',...
+        'world','to_live'};
+    % colslab = cell containing column labels
+    colslab={'unqualified','cep','bepc','high_school_diploma','university',...
+        'thirty','fifty','more_fifty'};
+    if verLessThan('matlab','8.2.0')==0
+        tableN=array2table(N,'VariableNames',colslab,'RowNames',rowslab);
+        % Extract just active rows
+        Nactive=tableN(1:14,1:5);
+        Nsupr=tableN(15:18,1:5);
+        Nsupc=tableN(1:14,6:8);
+        Sup=struct;
+        Sup.r=Nsupr;
+        Sup.c=Nsupc;
+        % Compute Correspondence analysis
+    else
+        Nactive=N(1:14,1:5);
+        Lr=rowslab(1:14);
+        Lc=colslab(1:5);
+        Sup=struct;
+        Sup.r=N(15:end,1:5);
+        Sup.Lr=rowslab(15:end);
+        Sup.c=N(1:14,6:8);
+        Sup.Lc=colslab(6:8);
+    end
     % No horizontal displacement for the labels.
     addx=0;
     CorAnaplot(out,'addx',addx)
