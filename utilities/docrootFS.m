a506e2cc70c8fe6573f201387e813ce759bb362b
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2018-09-13 17:01:54 +0000

    MOdifications to cope with the help system of 2018B
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@1830 336e6153-a435-9040-aa94-c6aba0fb517b

--- utilities/docrootFS.m
+++ utilities/docrootFS.m
@@ -3,7 +3,7 @@ function docr=docrootFS()
 
 a=ver('matlab');
 
-if str2double(a.Version)>7.14
+if str2double(a.Version)>7.14 && str2double(a.Version)<9.5
     docr=docroot;
     destinationpathFSDAdoc=[docr filesep 'FSDA'];
     chkFSDAinsideMatlab=exist(destinationpathFSDAdoc,'file');
@@ -36,7 +36,45 @@ if str2double(a.Version)>7.14
         
         error('FSDA:docrootFS:wrongSetUp','Please read warning above and take appropriate action!')
     else
+        
     end
+    
+elseif str2double(a.Version)>=9.5
+    docr='https://localhost:31515/static/help';
+    
+    docr2018b=docroot;
+    destinationpathFSDAdoc=[docr2018b filesep 'FSDA'];
+    chkFSDAinsideMatlab=exist(destinationpathFSDAdoc,'file');
+    if chkFSDAinsideMatlab ~=7
+        FileName='addFSDA2path';
+        FullPath=which(FileName);
+        FSDAroot=FullPath(1:end-length(FileName)-3);
+        FSDAhelproot=[FSDAroot filesep '_tmp_helpfiles' filesep 'FSDA'];
+        
+        sprintf('----------------------------------------------------------')
+        fprintf('%s\n\n',['FSDA HTML help files must be copied inside path "' destinationpathFSDAdoc '"']);
+        msg1=['The FSDA help files have not been copied under the MATLAB help folder: "' docroot '".'];
+        msg2='  Please check the user right permissions and possibly move manually the folder "';
+        msg3='"  in the MATLAB help folder. For additional details please see file: ';
+        msg4=[FSDAroot filesep 'InstallationNotes.pdf'];
+        
+        msg=regexprep([msg1 msg2 FSDAhelproot msg3 msg4],'\\','/');
+        warning('FSDA:docrootFS:wrongSetUp',msg)
+        h=mydialog(msg);
+        import com.mathworks.mlwidgets.html.HTMLRenderer;
+        % create component
+        r = HTMLRenderer;
+        % set the text to display
+        r.setHtmlText(['<html> <a href="' msg4 '">Installation Notes</a></html>']);
+        % make sure the component is opaque
+        r.setOpaque(true);
+        % add the component
+        javacomponent(r, [120 49 150 30], h);
+        %[left, bottom, width, height]
+        
+        error('FSDA:docrootFS:wrongSetUp','Please read warning above and take appropriate action!')
+    end
+    
 else
     fsep=filesep;
     
