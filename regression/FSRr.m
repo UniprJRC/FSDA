1f47f2ad7eff0b47e41cb59c3693e25e16a47007
domenico <domenico@336e6153-a435-9040-aa94-c6aba0fb517b>
2019-05-11 19:44:32 +0000

    New examples; doc warnings fixed.
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@2029 336e6153-a435-9040-aa94-c6aba0fb517b

--- regression/FSRr.m
+++ regression/FSRr.m
@@ -123,14 +123,32 @@ function [out , varargout] = FSRr(y, X, varargin)
 % Examples:
 
 %{
-        % Example of outlier detection in a case of almost perfect fit.
-        randn('state', 123456);
+        % Example of outlier detection in a case of almost perfect fit: all default options.
+        rng(12345);
         n=200; p=1;
         X = rand(n,p);
         y = X + 0.01*randn(n,1);
         % contaminated data points
         ycont = y;
-        ycont(1:5) = ycont(1:5)+0.07;
+        ycont(1:8) = ycont(1:8)+0.05;
+       
+        [out1 , xnew1 , ypred1, yci1] = ...
+                FSRr(ycont,X,'plotsPI',1,'plots',1);
+        
+        disp(['Outliers without R2 adjustment = ' num2str(out1.outliers)]);
+        disp(['Outliers with    R2 adjustment = ' num2str(out1.outliersr)]);
+        
+%}
+
+%{
+        % Example of outlier detection in a case of almost perfect fit.
+        rng(12345);
+        n=200; p=1;
+        X = rand(n,p);
+        y = X + 0.01*randn(n,1);
+        % contaminated data points
+        ycont = y;
+        ycont(1:8) = ycont(1:8)+0.05;
        
         [out1 , xnew1 , ypred1, yci1] = ...
                 FSRr(ycont,X,'alpha',0.01,...
@@ -139,7 +157,7 @@ function [out , varargout] = FSRr(y, X, varargin)
         h1 = allchild(gca); a1 = gca; f1 = gcf;
         
         [out2 , xnew2 , ypred2, yci2] = ...
-                FSRr(ycont,X,'alpha',0.01,'R2th',0.9,...
+                FSRr(ycont,X,'alpha',0.01,'R2th',0.95,...
                     'fullreweight',true ,'plotsPI',1,'plots',0);
 
         h2 = allchild(gca); a2 = gca; f2 = gcf;
@@ -153,6 +171,69 @@ function [out , varargout] = FSRr(y, X, varargin)
         disp(['Outliers without R2 adjustment = ' num2str(out1.outliersr)]);
         disp(['Outliers with    R2 adjustment = ' num2str(out2.outliersr)]);
         
+%}
+
+%{
+        %% Almost perfect fit issue in a real dataset from international trade.
+        %  Methods compared: LTS, FSR and FSRr.
+        close all;
+        clear all;
+        rng(12345);
+
+        load fishery
+        data = fishery.data;
+        X = data(:,1);
+        y = data(:,2);
+        n = length(y);
+
+        % Bonferronized confidence level
+        cl = 0.01/n;
+
+        % LTS with 60% observations for the LS fit
+        h = floor(n*0.6);
+        outLTS = LXS(y,X,'plots',0,'conflev',1-cl,'h',h);
+
+        plo.clr = 'bk';
+        plo.labeladd = 1;
+        yXplot(y,X,outLTS.weights,plo,'tag','LTSfit');
+        olsline(1)
+        price_LTS = X(logical(outLTS.weights))\y(logical(outLTS.weights));
+        title({'LTS fit using 60% of the data' , [num2str(price_LTS) ' euro/Kg']},'Fontsize',13);
+        xlabel('Quantity-Ton','Fontsize',14); ylabel('Value-1000euro','Fontsize',14);
+        h1 = allchild(gca); a1 = gca; f1 = gcf;
+
+        % Standard FSR, with Bonferronized band
+        outFS = FSR(y,X,'plots',0,'bonflev',cl,'h',h);
+        FSoutliers = ones(n,1); FSoutliers(outFS.outliers) = 0;
+        yXplot(y,X,'group',FSoutliers,'plo',plo,'tag','FSfit');
+        olsline(1)
+        price_FS = X(logical(FSoutliers))\y(logical(FSoutliers));
+        title({'Standard FS fit' , [num2str(price_FS) ' euro/Kg']},'Fontsize',13);
+        xlabel('Quantity-Ton','Fontsize',14); ylabel('Value-1000euro','Fontsize',14);
+        h2 = allchild(gca); a2 = gca; f2 = gcf;
+        
+        % FSRr, with scale rescaled so that R2 is forced to be 0.9 
+        outFSr = FSRr(y,X,'alpha',cl,'R2th',0.90,'fullreweight',true,'plotsPI',1,'plots',0);
+        title({'Prediction interval for FSRr fit' , 'with R2th=0.90.'},'Fontsize',13);
+        xlabel('Quantity-Ton','Fontsize',14); ylabel('Value-1000euro','Fontsize',14);
+        h3 = allchild(gca); a3 = gca; f3 = gcf;
+
+        FSroutliersr = ones(n,1); FSroutliersr(outFSr.outliersr) = 0;
+        yXplot(y,X,'group',FSroutliersr,'plo',plo,'tag','FSrfit');
+        olsline(1)
+        price_FSr = X(logical(FSroutliersr))\y(logical(FSroutliersr));
+        title({'FSRr fit, with R2th=0.90' , [num2str(price_FSr) ' euro/Kg']},'Fontsize',13);
+        xlabel('Quantity-Ton','Fontsize',14); ylabel('Value-1000euro','Fontsize',14);
+        h4 = allchild(gca); a4 = gca; f4 = gcf;
+
+        % move the figure above into a single one with two panels
+        figure; ax1 = subplot(2,2,1); ax2 = subplot(2,2,2);
+        ax3 = subplot(2,2,3); ax4 = subplot(2,2,4);
+        copyobj(h1,ax1); title(ax1,get(get(a1,'title'),'string'));
+        copyobj(h2,ax2); title(ax2,get(get(a2,'title'),'string'));
+        copyobj(h3,ax3); title(ax3,get(get(a3,'title'),'string'));
+        copyobj(h4,ax4); title(ax4,get(get(a4,'title'),'string'));
+        close(f1); close(f2); close(f3); close(f4);
 %}
 
 %% Beginning of code
@@ -229,18 +310,18 @@ beta = outFSR.beta;
 seq  = 1:n;
 
 % Options specific for this function
-alpha=options.alpha;
-fullreweight=options.fullreweight;
-R2th=options.R2th;
-plotsPI=options.plotsPI;
+alpha           = options.alpha;
+fullreweight    = options.fullreweight;
+R2th            = options.R2th;
+plotsPI         = options.plotsPI;
 
 % ListOut vector containing the outliers
 if ~isnan(outFSR.ListOut)
-    ListOut=outFSR.ListOut;
-    ListIn=setdiff(seq,ListOut);
+    ListOut = outFSR.ListOut;
+    ListIn  = setdiff(seq,ListOut);
 else
-    ListIn=seq;
-    ListOut='';
+    ListOut = '';
+    ListIn = seq;
 end
 nlistIn=length(ListIn);
 
@@ -367,10 +448,10 @@ if nargout > 0 || plotsPI==1
         figure('name','Prediction Interval');
         hold('on');
         plot(X(:,end),y,'o');
-        plot(xnew(:,end),ypred);
+        plot(xnew(:,end),ypred,'Color','k','LineWidth',1.5,'LineStyle','--');
         
-        plot(xnew(:,end),yci(:,1));
-        plot(xnew(:,end),yci(:,2));
+        plot(xnew(:,end),yci(:,1),'Color','r','LineWidth',1);
+        plot(xnew(:,end),yci(:,2),'Color','r','LineWidth',1);
         
         if R2th < 1
             if R2b > R2th
