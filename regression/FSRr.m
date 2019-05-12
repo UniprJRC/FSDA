2c4f287c6d699a0056b28514fba927909d76188e
domenico <domenico@336e6153-a435-9040-aa94-c6aba0fb517b>
2019-05-12 11:34:02 +0000

    Examples adapted in view of the movie
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@2034 336e6153-a435-9040-aa94-c6aba0fb517b

--- regression/FSRr.m
+++ regression/FSRr.m
@@ -173,7 +173,40 @@ function [out , varargout] = FSRr(y, X, varargin)
 
 %{
         %% Almost perfect fit issue in a real dataset from international trade.
-        %  Methods compared: LTS, FSR and FSRr.
+        close all; clear all;
+
+        load fishery;
+        X = fishery.data(:,1);
+        y = fishery.data(:,2);
+        n = length(y);
+
+        % Bonferronized confidence level
+        cl = 0.005/n;
+        
+        % FSRr, with scale rescaled so that R2 is forced to be 0.85 
+        outFSr = FSRr(y,X,'alpha',cl,'R2th',0.85,'fullreweight',true,'plotsPI',1,'plots',0);
+        h3 = allchild(gca); a3 = gca; f3 = gcf;
+
+        % Scatter of outliers detected with R2th=0.85 
+        FSroutliersr = ones(n,1); FSroutliersr(outFSr.outliersr) = 0;
+        plo.clr = 'rb'; plo.sym = 'xo'; plo.labeladd = 1;
+        yXplot(y,X,'group',FSroutliersr,'plo',plo,'tag','FSrfit');
+        olsline(1)
+        price_FSr = X(logical(FSroutliersr))\y(logical(FSroutliersr));
+        h4 = allchild(gca); a4 = gca; f4 = gcf;
+
+        % move the figures above into a single one with two panels
+        figure; ax3 = subplot(2,1,1); ax4 = subplot(2,1,2);
+        copyobj(h3,ax3); copyobj(h4,ax4); 
+        xlabel('Quantity-Ton','Fontsize',14); 
+        ylabel(ax3,'Value-1000euro','Fontsize',14);
+        ylabel(ax4,'Value-1000euro','Fontsize',14);
+        title(ax3,{'"fishery" dataset: FSRr fit with R2th=0.85','Prediction interval and outliers detected'},'Fontsize',20,'Interpreter','LaTex');
+        close(f3); close(f4);
+%}
+
+%{
+        %% Comparing LTS, FSR and FSRr on the international trade dataset.
         close all;
         clear all;
 
@@ -190,8 +223,7 @@ function [out , varargout] = FSRr(y, X, varargin)
         h = floor(n*0.6);
         outLTS = LXS(y,X,'plots',0,'conflev',1-cl,'h',h);
 
-        plo.clr = 'bk';
-        plo.labeladd = 1;
+        plo.clr = 'rb'; plo.sym = 'xo'; plo.labeladd = 1;
         yXplot(y,X,outLTS.weights,plo,'tag','LTSfit');
         olsline(1)
         price_LTS = X(logical(outLTS.weights))\y(logical(outLTS.weights));
