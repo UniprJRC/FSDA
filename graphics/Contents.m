59230d51d09de936dff38cfb30bc5d73fc3a42c0
marco <marco@336e6153-a435-9040-aa94-c6aba0fb517b>
2019-11-04 08:15:45 +0000

    startup.m deleted checkFSDAsetup.m added downloadGraphicalOutput.m added
    All html regenerated and also the contents
    
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@2156 336e6153-a435-9040-aa94-c6aba0fb517b
    
    Former-commit-id: 051062694e39f5483b387967a611f2e07019141c

--- graphics/Contents.m
+++ graphics/Contents.m
@@ -5,24 +5,24 @@
 %   Name         - Description                                                                                                                        - Category      - Date last modified
 %------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 %   aceplot      - Produces the aceplot to visualize the results of ace                                                                               - REG-Regression- 2019 May 20
-%   add2spm      - Adds objects (personalized clickable multilegends and text labels) to the scatter plot matrix                                      - VIS-Mult      - 2019 May 20
-%   add2yX       - Adds objects (personalized clickable multilegends and text labels) to the yXplot                                                   - VIS-Reg       - 2019 Jul 18
-%   boxplotb     - Computes a bivariate boxplot                                                                                                       - VIS-Mult      - 2019 May 20
-%   carbikeplot  - Produces the carbike plot to find best relevant clustering solutions                                                               - VIS-Clu       - 2019 May 20
-%   cdsplot      - Produces the candlestick plot for robust model selection in linear regression                                                      - VIS-Reg       - 2019 May 20
+%   add2spm      - Adds objects (personalized clickable multilegends and text labels) to the scatter plot matrix                                      - VIS-Mult      - 2019 Oct 17
+%   add2yX       - Adds objects (personalized clickable multilegends and text labels) to the yXplot                                                   - VIS-Reg       - 2019 Oct 17
+%   boxplotb     - Computes a bivariate boxplot                                                                                                       - VIS-Mult      - 2019 Oct 17
+%   carbikeplot  - Produces the carbike plot to find best relevant clustering solutions                                                               - VIS-Clu       - 2019 Oct 17
+%   cdsplot      - Produces the candlestick plot for robust model selection in linear regression                                                      - VIS-Reg       - 2019 Oct 17
 %   CorAnaplot   - Draws the Correspondence Analysis (CA) graphs with confidence ellipses                                                             - VIS-Mult      - 2019 May 20
-%   covplot      - Plots the trajectories of the elements of the covariance (correlation) matrix monitored                                            - VIS-Mult      - 2019 Oct 03
-%   fanplot      - Plots the fan plot for transformation in linear regression                                                                         - VIS-Reg       - 2019 May 20
-%   histFS       - Plots a histogram with the elements in each bin grouped according to a vector of labels                                            - VIS-Reg       - 2019 May 20
-%   levfwdplot   - Plots the trajectories of leverage along the search                                                                                - VIS-Reg       - 2019 May 20
-%   malfwdplot   - Plots the trajectories of scaled Mahalanobis distances along the search                                                            - VIS-Mult      - 2019 May 20
-%   malindexplot - Plots the Mahalanobis distances versus a selected variable                                                                         - VIS-Mult      - 2019 May 20
-%   mdrplot      - Plots the trajectory of minimum deletion residual (mdr)                                                                            - VIS-Reg       - 2019 May 20
-%   mmdplot      - Plots the trajectory of minimum Mahalanobis distance (mmd)                                                                         - VIS-Mult      - 2019 May 20
-%   mmdrsplot    - Plots the trajectories of minimum Mahalanobis distances from different starting points                                             - VIS-Mult      - 2019 May 20
-%   resfwdplot   - Plots the trajectories of the monitored scaled (squared) residuals                                                                 - VIS-Reg       - 2019 Jul 18
-%   resindexplot - Plots the residuals from a regression analysis versus index number or any other variable                                           - VIS-Reg       - 2019 May 20
-%   spmplot      - Produces an interactive scatterplot matrix with boxplots or histograms on the main diagonal and possibly robust bivariate contours - VIS-Mult      - 2019 Jun 21
-%   tclustICplot - Plots information criterion as a function of c and k                                                                               - VIS-Clu       - 2019 Oct 03
-%   wedgeplot    - Generates the double wedge plot of a time series                                                                                   - VIS-Reg       - 2019 May 20
-%   yXplot       - Produces an interactive scatterplot of y against each variable of X in the input dataset                                           - VIS-Reg       - 2019 Oct 03
+%   covplot      - Plots the trajectories of the elements of the covariance (correlation) matrix monitored                                            - VIS-Mult      - 2019 Oct 17
+%   fanplot      - Plots the fan plot for transformation in linear regression                                                                         - VIS-Reg       - 2019 Oct 17
+%   histFS       - Plots a histogram with the elements in each bin grouped according to a vector of labels                                            - VIS-Reg       - 2019 Oct 17
+%   levfwdplot   - Plots the trajectories of leverage along the search                                                                                - VIS-Reg       - 2019 Oct 17
+%   malfwdplot   - Plots the trajectories of scaled Mahalanobis distances along the search                                                            - VIS-Mult      - 2019 Oct 17
+%   malindexplot - Plots the Mahalanobis distances versus a selected variable                                                                         - VIS-Mult      - 2019 Oct 17
+%   mdrplot      - Plots the trajectory of minimum deletion residual (mdr)                                                                            - VIS-Reg       - 2019 Oct 17
+%   mmdplot      - Plots the trajectory of minimum Mahalanobis distance (mmd)                                                                         - VIS-Mult      - 2019 Oct 17
+%   mmdrsplot    - Plots the trajectories of minimum Mahalanobis distances from different starting points                                             - VIS-Mult      - 2019 Oct 17
+%   resfwdplot   - Plots the trajectories of the monitored scaled (squared) residuals                                                                 - VIS-Reg       - 2019 Oct 17
+%   resindexplot - Plots the residuals from a regression analysis versus index number or any other variable                                           - VIS-Reg       - 2019 Oct 17
+%   spmplot      - Produces an interactive scatterplot matrix with boxplots or histograms on the main diagonal and possibly robust bivariate contours - VIS-Mult      - 2019 Oct 17
+%   tclustICplot - Plots information criterion as a function of c and k                                                                               - VIS-Clu       - 2019 Oct 17
+%   wedgeplot    - Generates the double wedge plot of a time series                                                                                   - VIS-Reg       - 2019 Oct 17
+%   yXplot       - Produces an interactive scatterplot of y against each variable of X in the input dataset                                           - VIS-Reg       - 2019 Oct 17
