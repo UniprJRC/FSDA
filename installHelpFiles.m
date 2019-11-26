410bf5d900f8e35faa504e9b7d6b8069c099750d
gianluca <gianluca@336e6153-a435-9040-aa94-c6aba0fb517b>
2019-11-26 15:54:34 +0000

    Automatic copy of help files for Windows with admin privileges
    git-svn-id: https://160.78.46.112/svn/fsda.code.000/FSDA/trunk@2183 336e6153-a435-9040-aa94-c6aba0fb517b
    
    Former-commit-id: b7fb15a923a767f820b7e639655e009aa6b64653

---
+++ installHelpFiles.m
@@ -0,0 +1,99 @@
+% This file
+% 1) makes sure that the HTML files which are in subfolder
+%       (FSDA path)/helpfiles/FSDA
+% are copied inside
+%       (docroot)/help/FSDA
+% Note that to properly copy these file under windows, it is necessary to have
+% administrator privileges (or to run MATLAB with administrator privileges)
+%
+
+
+%% Beginning of code
+% Store current folder so that after the execution we go back to this
+% folder
+CurrentFolder=pwd;
+
+% First navigate to FSDA main folder
+FileName='addFSDA2path';
+FullPath=which(FileName);
+if isempty(FullPath)
+    error('FSDA:setup:WrongLocation','In order to properly run this file please navigate to the main folder of FSDA')
+else
+    %Navigate to the main folder of FSDA
+    FSDAroot=fileparts(FullPath);
+    cd(FSDAroot)
+end
+
+%% Copy all FSDA .html files inside docroot/FSDA
+fsep=filesep;
+
+try
+    source=['helpfiles' filesep 'FSDA'];
+    destination=[docroot filesep 'FSDA'];
+    disp('Copying all FSDA documentation files which are in folder')
+    disp([FSDAroot filesep source])
+    disp('into')
+    disp(destination)
+    disp('------------------------')
+    
+    
+    if ispc
+        % create a shell application
+        uac = actxserver('Shell.Application');
+        
+        
+        % Create temporary file file copy_FSDA_help_files.bat
+        stringToIncludeInBatFile=['robocopy /E "' FSDAroot filesep source '"  "' destination '"'];
+        
+        file1ID=fopen('copy_FSDA_help_files.bat','w');
+        fprintf(file1ID,'%s',stringToIncludeInBatFile);
+        fclose('all');
+        
+        % run copy_FSDA_help_files,bat file with admin privileges
+        uac.ShellExecute('copy_FSDA_help_files.bat', 'ELEV', '', 'runas', 1);
+        
+%         disp('copying files')
+%         disp('Click to stop')
+        pause(5);
+        % delete temporary file copy_FSDA_help_files.bat
+        delete copy_FSDA_help_files.bat
+        
+    else
+        
+        [status,msg]=copyfile(source,destination,'f');
+        if status ==1
+            disp('HTML FSDA documentation files correctly copied')
+        else
+            disp('Due to write permission problems HTML files in:')
+            disp([pwd filesep source])
+            disp('could not be copied inside folder')
+            disp(destination)
+            disp('To solve the problem, please run MATLAB as administrator')
+            disp('or manually copy the files, otherwise the HTML FSDA help files will not be visible')
+            warning('FSDA:startup:NotCopied','Impossible to copy FSDA HTML documentation files')
+        end
+    end
+catch
+    disp('Unknown error when trying to copy the HTML FSDA documentation files from')
+    disp([pwd filesep source])
+    disp('to:')
+    disp(destination)
+end
+
+% Create searchable databaset (not necessary)
+% subPointersFolder=[filesep 'helpfiles' filesep 'pointersHTML'];
+% folderwithSearchableDatabase=[FSDAroot subPointersFolder];
+%
+%
+% % Launch buildocsearchdb
+% % run builddocsearchdb in subfolder pointersHTML
+% try
+%     builddocsearchdb(folderwithSearchableDatabase)
+%     disp('FSDA searchable database correctly added')
+% catch
+%     disp('Unknown error when trying to run MATLAB routine builddocsearchdb')
+%     disp(['in folder:  '  folderwithSearchableDatabase])
+% end
+
+cd(CurrentFolder)
+clearvars
