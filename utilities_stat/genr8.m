3a89dcedc447ce09fd78e4f10fc943bbd8baf5fc
tortifr <tortifr@sitaf-lap101.IPSC.TLD>
2019-12-20 16:40:20 +0100

    just a typo
    Former-commit-id: a0726ac3cca19eb4f4cb382fbc36b6999f73053e

--- utilities_stat/genr8.m
+++ utilities_stat/genr8.m
@@ -5,7 +5,7 @@ function [out]= genr8(n, distrib, s1, s2)
 %
 %
 %    This function generates a sequence of pseudorandom numbers
-%    using the 32-bit rnadom number generator from figure 3 of
+%    using the 32-bit random number generator from figure 3 of
 %    the article by Pierre L'Ecuyer.
 %    The cycle length is claimed to be 2.30584E+18.
 %
