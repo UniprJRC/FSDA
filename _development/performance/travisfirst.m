df9e8f73221e150d7af5224e6803798a9b7ab2be
Marco Riani <mriani@unipr.it>
2019-12-27 19:26:58 +0100

    Test Travis with one example
    Former-commit-id: e0e7fe822bd1e1560ca265c94ab7e4bbe76fe861

---
+++ _development/performance/travisfirst.m
@@ -0,0 +1,10 @@
+    % LXS with default input and output.
+    % Compute LMS estimator without reweighting, add intercept to matrix X
+    % and do not produce plots.
+    n=200;
+    p=3;
+    randn('state', 123456);
+    X=randn(n,p);
+    y=randn(n,1);
+    y(1:5)=y(1:5)+6;
+    [out]=LXS(y,X);
