edfe1edfd31e0ebd5e8d8bf04a7259bf60a03140
Marco Riani <mriani@unipr.it>
2019-12-31 16:48:19 +0100

    Retest with  file1ID=fopen('tempfile.m','w');             disp(['File identifier: ' num2str(file1ID)])
    Former-commit-id: c72d26bdd0ed82749ac41aaa78fa63bf5469de7c

--- travisfirst.m
+++ travisfirst.m
@@ -1,92 +1,92 @@
-% %% Load necessary elements for performance test
-% % load OUT
-% run addFSDA2path
-% FileName='addFSDA2path';
-% FullPath=which(FileName);
-% %Navigate to the main folder of FSDA
-% FSDAroot=fileparts(FullPath);
-% cd(FSDAroot)
-% % Specify subfolders of main folders for which contents file has to be
-% % created
-% InclDir={'graphics' 'regression' 'multivariate' 'clustering' 'combinatorial' ...
-%     'examples' 'utilities' 'utilities_stat' 'utilities_help'};
-% ExclDir={'privateFS'  'datasets'};
-% % Create list of folders which must have the personalized contents file
-% list = findDir(FSDAroot,'InclDir',InclDir,'ExclDir',ExclDir);
-% % Crete personalized contents file for main folder of FSDA
-% % and required subfolders.
-% force=false;
-% [FilesIncluded,FilesExcluded]=makecontentsfileFS('dirpath',list,'FilterFileContent','%FScategory:','force',force,'printOutputCell','Contents.m');
-% disp('List of files which have been excluded (with path)')
-% disp(FilesExcluded(:,[1 9]))
-% [~,OUT]=publishFSallFiles(FilesIncluded, 'evalCode','false','write2file',false);
-% 
-% ij=1;
-% nfiles=length(OUT);
-% sz=[5000, 7];
-% TotSummary = table('Size',sz,'VariableTypes',{'cellstr' 'cellstr' 'cellstr' 'double' 'double' 'cellstr' 'cellstr'},...
-%     'VariableNames',{'FileName' 'Category', 'Identifier' 'MeanTime' 'MedianTime'  'Code' 'TestActivity'});
-% 
-% %% Performance part
-% % nfiles = number of files
-% 
-% for i=1:3
-%     clc
-%     disp(['Executing file n.' num2str(i) " of " num2str(nfiles)])
-%     disp(['File: ' FilesIncluded{i,1}]);
-%     Ex=OUT{i,1}.Ex;
-%     Extra=OUT{i,1}.ExtraEx;
-%     Excomb=[Ex;Extra];
-%     for iEx=1:size(Excomb,1)
-%         close all
-%         Exi=Excomb{iEx,3};
-%         if ~isempty(Exi) && isempty(strfind(Excomb{iEx,1},'Interactive example'))
-%             
-%             Exi=regexprep(Exi,'&lt;','<');
-%             Exi=regexprep(Exi,'&gt;','>');
-%             close all
-%             if iEx==1
-%                 Exif=[Exi,newline,'close all',newline 'save tempfileWS'];
-%             else
-%                 Exif=['load tempfileWS',newline,Exi,newline,'close all',newline, 'save tempfileWS'];
-%             end
-%             
+%% Load necessary elements for performance test
+% load OUT
+run addFSDA2path
+FileName='addFSDA2path';
+FullPath=which(FileName);
+%Navigate to the main folder of FSDA
+FSDAroot=fileparts(FullPath);
+cd(FSDAroot)
+% Specify subfolders of main folders for which contents file has to be
+% created
+InclDir={'graphics' 'regression' 'multivariate' 'clustering' 'combinatorial' ...
+    'examples' 'utilities' 'utilities_stat' 'utilities_help'};
+ExclDir={'privateFS'  'datasets'};
+% Create list of folders which must have the personalized contents file
+list = findDir(FSDAroot,'InclDir',InclDir,'ExclDir',ExclDir);
+% Crete personalized contents file for main folder of FSDA
+% and required subfolders.
+force=false;
+[FilesIncluded,FilesExcluded]=makecontentsfileFS('dirpath',list,'FilterFileContent','%FScategory:','force',force,'printOutputCell','Contents.m');
+disp('List of files which have been excluded (with path)')
+disp(FilesExcluded(:,[1 9]))
+[~,OUT]=publishFSallFiles(FilesIncluded, 'evalCode','false','write2file',false);
+
+ij=1;
+nfiles=length(OUT);
+sz=[5000, 7];
+TotSummary = table('Size',sz,'VariableTypes',{'cellstr' 'cellstr' 'cellstr' 'double' 'double' 'cellstr' 'cellstr'},...
+    'VariableNames',{'FileName' 'Category', 'Identifier' 'MeanTime' 'MedianTime'  'Code' 'TestActivity'});
+
+%% Performance part
+% nfiles = number of files
+
+for i=1:3
+    clc
+    disp(['Executing file n.' num2str(i) " of " num2str(nfiles)])
+    disp(['File: ' FilesIncluded{i,1}]);
+    Ex=OUT{i,1}.Ex;
+    Extra=OUT{i,1}.ExtraEx;
+    Excomb=[Ex;Extra];
+    for iEx=1:size(Excomb,1)
+        close all
+        Exi=Excomb{iEx,3};
+        if ~isempty(Exi) && isempty(strfind(Excomb{iEx,1},'Interactive example'))
+            
+            Exi=regexprep(Exi,'&lt;','<');
+            Exi=regexprep(Exi,'&gt;','>');
+            close all
+            if iEx==1
+                Exif=[Exi,newline,'close all',newline 'save tempfileWS'];
+            else
+                Exif=['load tempfileWS',newline,Exi,newline,'close all',newline, 'save tempfileWS'];
+            end
+            
             % Write Exi to a file
-            Exif='a b c d e';
+            % Exif='a b c d e';
             file1ID=fopen('tempfile.m','w');
             disp(['File identifier: ' num2str(file1ID)])
             fprintf(file1ID,'%s',Exif);
             fclose('all');
-%             try
-%                 
-%                 outp=runperf('tempfile.m');
-%                 MeanS=outp.sampleSummary.Mean;
-%                 FindNaN=isnan(MeanS);
-%                 MeanS=MeanS(~FindNaN);
-%                 TotSummary{ij,'MeanTime'}= MeanS;
-%                 
-%                 MedianS=outp.sampleSummary.Median;
-%                 MedianS=MedianS(~FindNaN);
-%                 TotSummary{ij,'MedianTime'}= MedianS;
-%                 
-%                 % TotSummary{ij,'MedianTime'}= outp.sampleSummary.Median;
-%                 TotSummary(ij,'Code')=Ex(1,3);
-%                 TotSummary(ij,'TestActivity')={outp.TestActivity};
-%                 TotSummary(ij,'FileName')=FilesIncluded(i,1);
-%                 TotSummary(ij,'Category')=FilesIncluded(i,8);
-%                 TotSummary(ij,'Identifier')={['Ex' num2str(iEx)]};
-%                 ij=ij+1;
-%             catch
-%                 disp(['Error on example ' num2str(iEx)])
-%                 disp(['Name of the file: '  FilesIncluded{i,1}])
-%                 warning('Stop here')
-%             end
-%             
-%         end
-%     end
-%     % disp(['Error on the following function: (fx .num:)' int2str(i)])
-%     % OUT{i,1}.titl;
-% end
-% 
-% TotSummary1=TotSummary(1:ij-1,:);
-% disp(TotSummary1)
+            try
+                
+                outp=runperf('tempfile.m');
+                MeanS=outp.sampleSummary.Mean;
+                FindNaN=isnan(MeanS);
+                MeanS=MeanS(~FindNaN);
+                TotSummary{ij,'MeanTime'}= MeanS;
+                
+                MedianS=outp.sampleSummary.Median;
+                MedianS=MedianS(~FindNaN);
+                TotSummary{ij,'MedianTime'}= MedianS;
+                
+                % TotSummary{ij,'MedianTime'}= outp.sampleSummary.Median;
+                TotSummary(ij,'Code')=Ex(1,3);
+                TotSummary(ij,'TestActivity')={outp.TestActivity};
+                TotSummary(ij,'FileName')=FilesIncluded(i,1);
+                TotSummary(ij,'Category')=FilesIncluded(i,8);
+                TotSummary(ij,'Identifier')={['Ex' num2str(iEx)]};
+                ij=ij+1;
+            catch
+                disp(['Error on example ' num2str(iEx)])
+                disp(['Name of the file: '  FilesIncluded{i,1}])
+                warning('Stop here')
+            end
+            
+        end
+    end
+    % disp(['Error on the following function: (fx .num:)' int2str(i)])
+    % OUT{i,1}.titl;
+end
+
+TotSummary1=TotSummary(1:ij-1,:);
+disp(TotSummary1)
