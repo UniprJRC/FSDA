81ad1ed2accbcfcc42b4438c1146fe57d300cf53
Marco Riani <mriani@unipr.it>
2020-01-08 17:27:58 +0100

    [skip ci] example of intercept as boolean and addition of "Beginning of code"
    Former-commit-id: 6689c7979e1460c6b11ea238afcc8975d53de905

--- regression/FSR.m
+++ regression/FSR.m
@@ -27,11 +27,13 @@ function [out]=FSR(y,X,varargin)
 %
 % Optional input arguments:
 %
-%  intercept :  Indicator for constant term. Scalar. If 1, a model with
-%               constant term will be fitted (default), else no constant
-%               term will be included.
-%               Example - 'intercept',1
-%               Data Types - double
+%    intercept :  Indicator for constant term. true (default) | false. 
+%                 Indicator for the constant term (intercept) in the fit,
+%                 specified as the comma-separated pair consisting of
+%                 'Intercept' and either true to include or false to remove
+%                 the constant term from the model.
+%                 Example - 'intercept',false
+%                 Data Types - boolean
 %           h   : The number of observations that have determined the least
 %                 trimmed squares estimator. Scalar. h is an integer
 %                 greater or equal than p but smaller then n. Generally if
@@ -462,8 +464,9 @@ function [out]=FSR(y,X,varargin)
     title('FS with bound on the leverage')
 %}
 
-%% Input parameters checking
+%% Beginning of code
 
+% Input parameters checking
 nnargin=nargin;
 vvarargin=varargin;
 [y,X,n,p] = chkinputR(y,X,nnargin,vvarargin);
