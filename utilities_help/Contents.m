f21d50a6167387b8bb0696ba63587d4d3cad8fd5
Marco Riani <mriani@unipr.it>
2020-02-21 09:56:20 +0100

    contents regenerated

--- utilities_help/Contents.m
+++ utilities_help/Contents.m
@@ -4,11 +4,11 @@
 %
 %   Name                         - Description                                                                                             - Category- Date last modified
 %------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-%   htmlwriteFS                  - Enables to create automatic HELP FILES from a specific MATLAB structure created with function mreadFS.m - UTIHELP- 2019 Oct 17
-%   makecontentsfileFS           - Extends Matlab function makecontentsfile                                                                - UTIHELP- 2019 Oct 17
-%   mreadFS                      - Enables to create a structure with InputArgs/OptArgs/OutArgs ... from .m function files                 - UTIHELP- 2019 Oct 17
-%   publishBibliography          - Enables to create web page which contains the references inside the input .m files                      - UTIHELP- 2019 Aug 31
-%   publishFS                    - Enables to create automatic HELP FILES from structured .m function files                                - UTIHELP- 2019 Nov 03
-%   publishFunctionAlpha         - Enables to create web page which contains the alphabetical list of functions                            - UTIHELP- 2019 Oct 17
-%   publishFunctionCate          - Enables to create web page which contains the categorical list of functions                             - UTIHELP- 2019 Oct 17
-%   xmlcreateFS                  - Creates an XML file passing through publishFS                                                           - UTIHELP- 2019 Oct 17
+%   htmlwriteFS                  - Enables to create automatic HELP FILES from a specific MATLAB structure created with function mreadFS.m - UTIHELP- 2020 Jan 29
+%   makecontentsfileFS           - Extends Matlab function makecontentsfile                                                                - UTIHELP- 2020 Feb 05
+%   mreadFS                      - Enables to create a structure with InputArgs/OptArgs/OutArgs ... from .m function files                 - UTIHELP- 2020 Jan 29
+%   publishBibliography          - Enables to create web page which contains the references inside the input .m files                      - UTIHELP- 2020 Feb 05
+%   publishFS                    - Enables to create automatic HELP FILES from structured .m function files                                - UTIHELP- 2020 Feb 20
+%   publishFunctionAlpha         - Enables to create web page which contains the alphabetical list of functions                            - UTIHELP- 2020 Feb 05
+%   publishFunctionCate          - Enables to create web page which contains the categorical list of functions                             - UTIHELP- 2020 Feb 05
+%   xmlcreateFS                  - Creates an XML file passing through publishFS                                                           - UTIHELP- 2020 Feb 05
