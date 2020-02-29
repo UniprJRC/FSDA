643193b7cd37a98330750e8052df504fd3b965ee
Marco Riani <mriani@unipr.it>
2020-02-29 10:38:46 +0100

    Commented part under the if
    if ~strcmp(tp,'none') && ~strcmp(tp,'x') && ~strcmp(tp,'micro')
    
    because never reached
    
    Former-commit-id: 56743f5c2847be1da9f0e83079838da21b27dc7a

--- utilities/ginputFS.m
+++ utilities/ginputFS.m
@@ -68,28 +68,28 @@ else
 end
 
 if ~strcmp(tp,'none') && ~strcmp(tp,'x') && ~strcmp(tp,'micro')
-    if nargout == 1
-        if nargin == 1
-            out1 = trmginput(arg1);
-        else
-            out1 = trmginput;
-        end
-    elseif nargout == 2 || nargout == 0
-        if nargin == 1
-            [out1,out2] = trmginput(arg1);
-        else
-            [out1,out2] = trmginput;
-        end
-        if  nargout == 0
-            out1 = [ out1 out2 ];
-        end
-    elseif nargout == 3
-        if nargin == 1
-            [out1,out2,out3] = trmginput(arg1);
-        else
-            [out1,out2,out3] = trmginput;
-        end
-    end
+%     if nargout == 1
+%         if nargin == 1
+%             out1 = trmginput(arg1);
+%         else
+%             out1 = trmginput;
+%         end
+%     elseif nargout == 2 || nargout == 0
+%         if nargin == 1
+%             [out1,out2] = trmginput(arg1);
+%         else
+%             [out1,out2] = trmginput;
+%         end
+%         if  nargout == 0
+%             out1 = [ out1 out2 ];
+%         end
+%     elseif nargout == 3
+%         if nargin == 1
+%             [out1,out2,out3] = trmginput(arg1);
+%         else
+%             [out1,out2,out3] = trmginput;
+%         end
+%     end
 else
     
     fig = gcf;
