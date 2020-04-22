6bbda4d0bce0ecc9d6113fb23eacd30b02b2291e
Marco Riani <mriani@unipr.it>
2020-04-22 14:42:49 +0200

    Regenerated all contents files

--- graphics/Contents.m
+++ graphics/Contents.m
@@ -2,27 +2,28 @@
 %
 % File names, description, category and date last modified
 %
-%   Name         - Description                                                                                                                        - Category- Date last modified
+%   Name         - Description                                                                                                                        - Category      - Date last modified
 %------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-%   aceplot      - Produces the aceplot to visualize the results of ace                                                                               - VIS-Reg - 2020 Jan 29
-%   add2spm      - Adds objects (personalized clickable multilegends and text labels) to the scatter plot matrix                                      - VIS-Mult- 2020 Jan 29
-%   add2yX       - Adds objects (personalized clickable multilegends and text labels) to the yXplot                                                   - VIS-Reg - 2020 Jan 29
-%   boxplotb     - Computes a bivariate boxplot                                                                                                       - VIS-Mult- 2020 Jan 29
-%   carbikeplot  - Produces the carbike plot to find best relevant clustering solutions                                                               - VIS-Clu - 2020 Jan 29
-%   cdsplot      - Produces the candlestick plot for robust model selection in linear regression                                                      - VIS-Reg - 2020 Jan 29
-%   CorAnaplot   - Draws the Correspondence Analysis (CA) graphs with confidence ellipses                                                             - VIS-Mult- 2020 Feb 18
-%   covplot      - Plots the trajectories of the elements of the covariance (correlation) matrix monitored                                            - VIS-Mult- 2020 Jan 29
-%   fanplot      - Plots the fan plot for transformation in linear regression                                                                         - VIS-Reg - 2020 Jan 29
-%   histFS       - Plots a histogram with the elements in each bin grouped according to a vector of labels                                            - VIS-Reg - 2020 Feb 18
-%   levfwdplot   - Plots the trajectories of leverage along the search                                                                                - VIS-Reg - 2020 Feb 18
-%   malfwdplot   - Plots the trajectories of scaled Mahalanobis distances along the search                                                            - VIS-Mult- 2020 Feb 18
-%   malindexplot - Plots the Mahalanobis distances versus a selected variable                                                                         - VIS-Mult- 2020 Jan 29
-%   mdrplot      - Plots the trajectory of minimum deletion residual (mdr)                                                                            - VIS-Reg - 2020 Feb 18
-%   mmdplot      - Plots the trajectory of minimum Mahalanobis distance (mmd)                                                                         - VIS-Mult- 2020 Feb 18
-%   mmdrsplot    - Plots the trajectories of minimum Mahalanobis distances from different starting points                                             - VIS-Mult- 2020 Feb 18
-%   resfwdplot   - Plots the trajectories of the monitored scaled (squared) residuals                                                                 - VIS-Reg - 2020 Feb 20
-%   resindexplot - Plots the residuals from a regression analysis versus index number or any other variable                                           - VIS-Reg - 2020 Feb 18
-%   spmplot      - Produces an interactive scatterplot matrix with boxplots or histograms on the main diagonal and possibly robust bivariate contours - VIS-Mult- 2020 Feb 18
-%   tclustICplot - Plots information criterion as a function of c and k                                                                               - VIS-Clu - 2020 Feb 18
-%   wedgeplot    - Generates the double wedge plot of a time series                                                                                   - VIS-Reg - 2020 Feb 18
-%   yXplot       - Produces an interactive scatterplot of y against each variable of X in the input dataset                                           - VIS-Reg - 2020 Feb 18
+%   aceplot      - Produces the aceplot to visualize the results of ace                                                                               - VIS-Reg       - 2020 Jan 29
+%   add2spm      - Adds objects (personalized clickable multilegends and text labels) to the scatter plot matrix                                      - VIS-Mult      - 2020 Jan 29
+%   add2yX       - Adds objects (personalized clickable multilegends and text labels) to the yXplot                                                   - VIS-Reg       - 2020 Apr 10
+%   boxplotb     - Computes a bivariate boxplot                                                                                                       - VIS-Mult      - 2020 Jan 29
+%   carbikeplot  - Produces the carbike plot to find best relevant clustering solutions                                                               - VIS-Clu       - 2020 Apr 21
+%   cdsplot      - Produces the candlestick plot for robust model selection in linear regression                                                      - VIS-Reg       - 2020 Jan 29
+%   CorAnaplot   - Draws the Correspondence Analysis (CA) graphs with confidence ellipses                                                             - VIS-Mult      - 2020 Feb 18
+%   covplot      - Plots the trajectories of the elements of the covariance (correlation) matrix monitored                                            - VIS-Mult      - 2020 Jan 29
+%   fanplot      - Plots the fan plot for transformation in linear regression                                                                         - VIS-Reg       - 2020 Apr 22
+%   histFS       - Plots a histogram with the elements in each bin grouped according to a vector of labels                                            - VIS-Reg       - 2020 Feb 18
+%   levfwdplot   - Plots the trajectories of leverage along the search                                                                                - VIS-Reg       - 2020 Apr 21
+%   malfwdplot   - Plots the trajectories of scaled Mahalanobis distances along the search                                                            - VIS-Mult      - 2020 Feb 18
+%   malindexplot - Plots the Mahalanobis distances versus a selected variable                                                                         - VIS-Mult      - 2020 Jan 29
+%   mdrplot      - Plots the trajectory of minimum deletion residual (mdr)                                                                            - VIS-Reg       - 2020 Feb 18
+%   mdrrsplot    - Plots the trajectory of minimum deletion residual from random starts                                                               - CLUS-RobClaREG- 2020 Apr 22
+%   mmdplot      - Plots the trajectory of minimum Mahalanobis distance (mmd)                                                                         - VIS-Mult      - 2020 Apr 21
+%   mmdrsplot    - Plots the trajectories of minimum Mahalanobis distances from different starting points                                             - VIS-Mult      - 2020 Apr 21
+%   resfwdplot   - Plots the trajectories of the monitored scaled (squared) residuals                                                                 - VIS-Reg       - 2020 Apr 21
+%   resindexplot - Plots the residuals from a regression analysis versus index number or any other variable                                           - VIS-Reg       - 2020 Feb 18
+%   spmplot      - Produces an interactive scatterplot matrix with boxplots or histograms on the main diagonal and possibly robust bivariate contours - VIS-Mult      - 2020 Apr 21
+%   tclustICplot - Plots information criterion as a function of c and k                                                                               - VIS-Clu       - 2020 Apr 21
+%   wedgeplot    - Generates the double wedge plot of a time series                                                                                   - VIS-Reg       - 2020 Apr 21
+%   yXplot       - Produces an interactive scatterplot of y against each variable of X in the input dataset                                           - VIS-Reg       - 2020 Apr 21
--- multivariate/Contents.m
+++ multivariate/Contents.m
@@ -7,7 +7,7 @@
 %   barnardtest     - Barnard's unconditional test                                                             - MULT-Categorical    - 2020 Jan 29
 %   boxtest         - Performs Box test of equality of covariance matrices                                     - MULT-Multivariate   - 2020 Jan 29
 %   CorAna          - Performs correspondence analysis                                                         - MULT-Categorical    - 2020 Jan 29
-%   corrNominal     - Measures strength of association between two unordered (nominal) categorical variables   - MULT-Categorical    - 2020 Jan 29
+%   corrNominal     - Measures strength of association between two unordered (nominal) categorical variables   - MULT-Categorical    - 2020 Apr 21
 %   corrOrdinal     - Measures strength of association between two ordered categorical variables               - MULT-Categorical    - 2020 Jan 29
 %   CressieRead     - Computes the power divergence family                                                     - MULT-Categorical    - 2020 Jan 29
 %   FSM             - Gives an automatic outlier detection procedure in multivariate analysis                  - MULT-Multivariate   - 2020 Jan 29
@@ -17,17 +17,17 @@
 %   FSMenvmmd       - Computes the theoretical envelopes of Minimum MD outside subset during the search        - MULT-Multivariate   - 2020 Jan 29
 %   FSMfan          - Computes confirmatory lrt of a suggested transformation                                  - MULT-Transformations- 2020 Jan 29
 %   FSMinvmmd       - Converts values of minimum Mahalanobis distance into confidence levels                   - MULT-Multivariate   - 2020 Jan 29
-%   FSMmmd          - Monitors minMD                                                                           - MULT-Multivariate   - 2020 Jan 29
+%   FSMmmd          - Monitors minMD                                                                           - MULT-Multivariate   - 2020 Apr 21
 %   FSMmmdeasy      - Is exactly equal to FSMmmd but it is much less efficient                                 - MULT-Multivariate   - 2020 Jan 29
 %   FSMtra          - Computes MLE of transformation parameters                                                - MULT-Transformations- 2020 Jan 29
 %   mcd             - Computes Minimum Covariance Determinant                                                  - MULT-Multivariate   - 2020 Feb 20
 %   MMmult          - Computes MM estimators in multivariate analysis with auxiliary S-scale                   - MULT-Multivariate   - 2020 Feb 20
-%   MMmultcore      - Computes multivariate MM estimators for a selected fixed scale                           - MULT-Multivariate   - 2020 Jan 29
-%   MMmulteda       - Computes MM estimators in multivariate analysis for a series of values of eff            - MULT-Multivariate   - 2020 Jan 29
+%   MMmultcore      - Computes multivariate MM estimators for a selected fixed scale                           - MULT-Multivariate   - 2020 Apr 21
+%   MMmulteda       - Computes MM estimators in multivariate analysis for a series of values of eff            - MULT-Multivariate   - 2020 Apr 22
 %   mve             - Computes Minimum volume ellipsoid                                                        - MULT-Multivariate   - 2020 Feb 20
 %   mveeda          - Monitors Minimum volume ellipsoid for a series of values of bdp                          - MULT-Multivariate   - 2020 Feb 20
-%   rcontFS         - Generates a random two-way table with given marginal totals                              - MULT-Categorical    - 2020 Jan 29
-%   SDest           - Computes Stahel-Donoho robust estimator of dispersion-location                           - MULT-Multivariate   - 2020 Feb 20
+%   rcontFS         - Generates a random two-way table with given marginal totals                              - MULT-Categorical    - 2020 Feb 29
+%   SDest           - Computes Stahel-Donoho robust estimator of dispersion-location                           - MULT-Multivariate   - 2020 Apr 21
 %   Smult           - Computes S estimators in multivariate analysis                                           - MULT-Multivariate   - 2020 Feb 20
 %   Smulteda        - Computes S estimators in multivariate analysis for a series of values of bdp             - MULT-Multivariate   - 2020 Jan 29
 %   SparseTableTest - Computes independence test for large and sparse contingency tables                       - MULT-Categorical    - 2020 Jan 29
