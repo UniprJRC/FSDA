d5b6ffda20b170674eb6982ab07d13c03eb037e1
perrodo <perrodo@macdp152.homenet.telecomitalia.it>
2020-04-23 17:39:18 +0200

    Virtual commit

--- clustering/Contents.m
+++ clustering/Contents.m
@@ -2,32 +2,33 @@
 %
 % File names, description, category and date last modified
 %
-%   Name           - Description                                                                                                     - Category       - Date last modified
+%   Name                 - Description                                                                                                     - Category       - Date last modified
 %------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-%   ClusterRelabel - Enables to control the labels of the clusters which contain predefined units                                    - UTISTAT        - 2020 Jan 29
-%   dempk          - Performs a merging of components found by tkmeans                                                               - CLUS-RobClaMULT- 2020 Feb 20
-%   FSMmmdrs       - Performs random start monitoring of minimum Mahalanobis distance                                                - CLUS-RobClaMULT- 2020 Jan 29
-%   FSRmdrrs       - Performs random start monitoring of minimum deletion residual                                                   - CLUS-RobClaREG - 2020 Jan 29
-%   genSigmaGPCM   - Generates covariance matrix for the 14 Gaussian Parsimonious Clustering Models                                  - CLUS-RobClaMULT- 2020 Jan 24
-%   GowerIndex     - Computes matrix of similarity indexes using Gower metric                                                        - CLUS-RobClaMULT- 2020 Jan 29
-%   lga            - Performs linear grouping analysis                                                                               - CLUS-RobClaREG - 2020 Jan 29
-%   MixSim         - Generates k clusters in v dimensions with given overlap                                                         - CLUS-MixSim    - 2020 Feb 18
-%   MixSimreg      - Generates k regression hyperplanes in p dimensions with given overlap                                           - CLUS-MixSim    - 2020 Feb 20
-%   overlap        - Computes the exact overlap given the parameters of the mixture                                                  - CLUS-MixSim    - 2020 Feb 18
-%   overlapmap     - Produces an interactive overlap map                                                                             - CLUS-RobClaMULT- 2020 Feb 20
-%   restrdeter     - Computes determinant restriction                                                                                - CLUS-RobClaMULT- 2020 Feb 18
-%   restrdeterGPCM - Applies determinat restrictions for the 14 GPCM                                                                 - CLUS-RobClaMULT- 2020 Jan 23
-%   restreigen     - Computes eigenvalues restriction (without Dykstra algorithm)                                                    - CLUS-RobClaMULT- 2020 Feb 18
-%   restreigeneasy - Restreigen computes eigenvalues restriction (without Dykstra algorithm)                                         - CLUS-RobClaMULT- 2020 Jan 29
-%   restrshapeGPCM - Produces the restricted shape matrix for the 14 GPCM                                                            - CLUS-RobClaMULT- 2020 Feb 20
-%   restrSigmaGPCM - Computes constrained covariance matrices for the 14 GPCM specifications                                         - CLUS-RobClaMULT- 2020 Feb 20
-%   rlga           - Performs robust linear grouping analysis                                                                        - CLUS-RobClaREG - 2020 Jan 29
-%   simdataset     - Simulates and-or contaminates a dataset given the parameters of a finite mixture model with Gaussian components - CLUS-MixSim    - 2020 Feb 18
-%   simdatasetreg  - Simulates a regression dataset given the parameters of a mixture regression model                               - CLUS-MixSim    - 2020 Feb 18
-%   tclust         - Computes trimmed clustering with scatter restrictions                                                           - CLUS-RobClaMULT- 2020 Feb 20
-%   tclusteda      - Computes tclust for a series of values of the trimming factor                                                   - CLUS-RobClaMULT- 2020 Feb 18
-%   tclustIC       - Computes tclust for different number of groups k and restriction factors c                                      - CLUS-RobClaMULT- 2020 Jan 29
-%   tclustICsol    - Extracts a set of best relevant solutions                                                                       - CLUS-RobClaMULT- 2020 Feb 20
-%   tclustreg      - Performs robust linear grouping analysis                                                                        - CLUS-RobClaREG - 2020 Feb 18
-%   tclustregIC    - Computes tclustreg for different number of groups k and restriction factors c                                   - CLUS-RobClaREG - 2020 Feb 20
-%   tkmeans        - Computes trimmed k-means                                                                                        - CLUS-RobClaMULT- 2020 Feb 20
+%   ClusterRelabel       - Enables to control the labels of the clusters which contain predefined units                                    - UTISTAT        - 2020 Apr 21
+%   dempk                - Performs a merging of components found by tkmeans                                                               - CLUS-RobClaMULT- 2020 Feb 20
+%   FSMmmdrs             - Performs random start monitoring of minimum Mahalanobis distance                                                - CLUS-RobClaMULT- 2020 Apr 21
+%   FSRmdrrs             - Performs random start monitoring of minimum deletion residual                                                   - CLUS-RobClaREG - 2020 Apr 21
+%   genSigmaGPCM         - Generates covariance matrix for the 14 Gaussian Parsimonious Clustering Models                                  - CLUS-RobClaMULT- 2020 Jan 24
+%   GowerIndex           - Computes matrix of similarity indexes using Gower metric                                                        - CLUS-RobClaMULT- 2020 Jan 29
+%   lga                  - Performs linear grouping analysis                                                                               - CLUS-RobClaREG - 2020 Apr 22
+%   MixSim               - Generates k clusters in v dimensions with given overlap                                                         - CLUS-MixSim    - 2020 Feb 18
+%   MixSimreg            - Generates k regression hyperplanes in p dimensions with given overlap                                           - CLUS-MixSim    - 2020 Apr 21
+%   overlap              - Computes the exact overlap given the parameters of the mixture                                                  - CLUS-MixSim    - 2020 Feb 18
+%   overlapmap           - Produces an interactive overlap map                                                                             - CLUS-RobClaMULT- 2020 Feb 20
+%   restrdeter           - Computes determinant restriction                                                                                - CLUS-RobClaMULT- 2020 Feb 18
+%   restrdeterGPCM       - Applies determinat restrictions for the 14 GPCM                                                                 - CLUS-RobClaMULT- 2020 Mar 02
+%   restreigen           - Computes eigenvalues restriction (without Dykstra algorithm)                                                    - CLUS-RobClaMULT- 2020 Feb 18
+%   restreigeneasy       - Restreigen computes eigenvalues restriction (without Dykstra algorithm)                                         - CLUS-RobClaMULT- 2020 Jan 29
+%   restrshapeGPCM       - Produces the restricted shape matrix for the 14 GPCM                                                            - CLUS-RobClaMULT- 2020 Feb 20
+%   restrSigmaGPCM       - Computes constrained covariance matrices for the 14 GPCM specifications                                         - CLUS-RobClaMULT- 2020 Mar 02
+%   rlga                 - Performs robust linear grouping analysis                                                                        - CLUS-RobClaREG - 2020 Apr 21
+%   simdataset           - Simulates and-or contaminates a dataset given the parameters of a finite mixture model with Gaussian components - CLUS-MixSim    - 2020 Feb 18
+%   simdatasetreg        - Simulates a regression dataset given the parameters of a mixture regression model                               - CLUS-MixSim    - 2020 Feb 18
+%   tclust               - Computes trimmed clustering with scatter restrictions                                                           - CLUS-RobClaMULT- 2020 Apr 21
+%   tclusteda            - Computes tclust for a series of values of the trimming factor                                                   - CLUS-RobClaMULT- 2020 Apr 21
+%   tclustIC             - Computes tclust for different number of groups k and restriction factors c                                      - CLUS-RobClaMULT- 2020 Apr 21
+%   tclustICsol          - Extracts a set of best relevant solutions                                                                       - CLUS-RobClaMULT- 2020 Apr 22
+%   tclustreg            - Performs robust linear grouping analysis                                                                        - CLUS-RobClaREG - 2020 Apr 22
+%   tclustregeda         - Performs robust linear grouping analysis for a series of values of the trimming factor                          - CLUS-RobClaREG - 2020 Apr 22
+%   tclustregIC          - Computes tclustreg for different number of groups k and restriction factors c                                   - CLUS-RobClaREG - 2020 Apr 21
+%   tkmeans              - Computes trimmed k-means                                                                                        - CLUS-RobClaMULT- 2020 Apr 21
