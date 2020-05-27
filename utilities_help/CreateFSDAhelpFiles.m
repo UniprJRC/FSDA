7494509f2349dc0c48f5906ea831ac0b31897134
Aldo Corbellini <aldo.corbellini@unipr.it>
2020-05-27 09:22:21 +0200

    regenerated contents.m files for FSDA 2020b prerelease

--- utilities_help/CreateFSDAhelpFiles.m
+++ utilities_help/CreateFSDAhelpFiles.m
@@ -70,7 +70,7 @@ ExclDir={'privateFS'  'datasets'};
 list = findDir(FSDAroot,'InclDir',InclDir,'ExclDir',ExclDir);
 % Crete personalized contents file for main folder of FSDA
 % and required subfolders.
-force=false;
+force=true;
 [FilesIncluded,FilesExcluded]=makecontentsfileFS('dirpath',list,'FilterFileContent','%FScategory:','force',force,'printOutputCell','Contents.m');
 disp('List of files which have been excluded (with path)')
 disp(FilesExcluded(:,[1 9]))
