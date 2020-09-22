617fc0b924993aae80550f7c08636c187dcdeff4
Marco Riani <mriani@unipr.it>
2020-09-22 15:40:39 +0200

    stable commit of funnelplot

--- graphics/funnelplot.m
+++ graphics/funnelplot.m
@@ -1,19 +1,23 @@
 function h  = funnelplot(x, varargin)
-%carbikeplot produces the carbike plot to find best relevant clustering solutions
+%funnelplot a funnel plot
 %
 %<a href="matlab: docsearchFS('funnelplot')">Link to the help function</a>
 %
 % Funnel charts show values across multiple stages in a process. For
 % example, you could use a funnel chart to show the number of sales
 % prospects at each stage in a sales pipeline. Typically, the values
-% decrease gradually, allowing the bars to resemble a funnel.
+% decrease gradually, allowing the bars to resemble a funne.
+% This type of chart can also be useful in identifying potential problem
+% areas in an organization’s sales processes. A funnel chart is similar to
+% a stacked percent bar chart. For more details see
+% https://en.wikipedia.org/wiki/Funnel_chart
 %
 %  Required input arguments:
 %
 %           x : input data. Vector or matrix.
-%                If y is a vector, funnelplot plots one funnel plot. If y
+%                If x is a vector, funnelplot plots one funnel plot. If x
 %                is a matrix, funnelplot plots one funnel plot for each
-%                column of y.
+%                column of x.
 %          Data Types - double
 %
 %  Optional input arguments:
@@ -64,6 +68,16 @@ function h  = funnelplot(x, varargin)
 %}
 
 
+%{
+    %% funnelplot when x is a matrix.
+    x=100*abs(randn(10,4));
+    x=sort(x,1,'descend');
+    labels={'A', 'B', 'C', 'D', 'E', ...
+        'F', 'G' 'H', 'I' 'J'};
+    funnelplot(x,'Labels',labels)
+%}
+
+
 
 %% Beginning of code
 
@@ -71,33 +85,42 @@ if ~isnumeric(x)
     error('FSDA:carbikeplot:WrongInput','First input argument must be numeric.');
 end
 
-n=length(x);
+x(x<0)=0;
+if isvector(x)
+    n=length(x);
+    p=1;
+    % Make sure that x is a column vector
+    x=x(:);
+else
+    [n,p]=size(x);
+end
+
 Labels=cellstr(num2str((1:n)'));
 
-    options=struct('Labels',{Labels});
+options=struct('Labels',{Labels});
+
+UserOptions=varargin(1:2:length(varargin));
+if ~isempty(UserOptions)
     
-    UserOptions=varargin(1:2:length(varargin));
-    if ~isempty(UserOptions)
-        
-        
-        % Check if number of supplied options is valid
-        if length(varargin) ~= 2*length(UserOptions)
-            error('FSDA:funnelplot:WrongInputOpt','Number of supplied options is invalid. Probably values for some parameters are missing.');
-        end
-        
-        % Check if all the specified optional arguments were present
-        % in structure options
-        % Remark: the nocheck option has already been dealt by routine
-        % chkinputR
-        inpchk=isfield(options,UserOptions);
-        WrongOptions=UserOptions(inpchk==0);
-        if ~isempty(WrongOptions)
-            disp(strcat('Non existent user option found->', char(WrongOptions{:})))
-            error('FSDA:funnelplot:NonExistInputOpt','In total %d non-existent user options found.', length(WrongOptions));
-        end
+    
+    % Check if number of supplied options is valid
+    if length(varargin) ~= 2*length(UserOptions)
+        error('FSDA:funnelplot:WrongInputOpt','Number of supplied options is invalid. Probably values for some parameters are missing.');
     end
     
- if nargin>1   
+    % Check if all the specified optional arguments were present
+    % in structure options
+    % Remark: the nocheck option has already been dealt by routine
+    % chkinputR
+    inpchk=isfield(options,UserOptions);
+    WrongOptions=UserOptions(inpchk==0);
+    if ~isempty(WrongOptions)
+        disp(strcat('Non existent user option found->', char(WrongOptions{:})))
+        error('FSDA:funnelplot:NonExistInputOpt','In total %d non-existent user options found.', length(WrongOptions));
+    end
+end
+
+if nargin>1
     % Write in structure 'options' the options chosen by the user
     for i=1:2:length(varargin)
         options.(varargin{i})=varargin{i+1};
@@ -111,24 +134,58 @@ end
 Labels=flipud(Labels(:));
 
 % Create figure
-figure1 = figure;
+ figure;
+  set(gca,'YTick','','XTick','');
 
-% Create axes
-axes1 = axes('Parent',figure1);
+
+switch p
+    case 1
+        nr=1;
+        nc=1;
+    case 2
+        nr=2;
+        nc=1;
+    case {3,4}
+        nr=2;
+        nc=2;
+    case {5,6}
+        nr=3;
+        nc=2;
+    case {7,8,9}
+        nr=3;
+        nc=3;
+        
+    case {10,11,12}
+        nr=3;
+        nc=4;
+    otherwise
+        nr=4;
+        nc=4;
+end
+   % set(gca,'YTick','','XTick','','XTickLabel','');
 
 kk=1.05;
- axis([-max(x)*kk  max(x)*kk 1.5 n+0.5]);
-grid off;
-set(axes1,'YTick',1.5:(n+0.5),'XTick','');
-ylim([0.5 n+1.3])
-for i=1:n
-    rectangle('position',[-x(i) n+1-i 2*x(i)  1],'facecolor','c','Curvature',[0.02 0.02]);
-    text(0, n+1.5-i ,num2str(x(i)),'HorizontalAlignment','center','FontSize',15,'VerticalAlignment','middle');
+for j=1:p
+    subplot(nr,nc,j);
+    % Create axes
+    % axes1 = axes('Parent',figure1);
+    Xj=x(:,j);
+    axis([-max(Xj)*kk  max(Xj)*kk 1.5 n+0.5]);
+    grid off;
+    set(gca,'YTick',1.5:(n+0.5),'XTick','');
+    ylim([0.5 n+1.3])
+    for i=1:n
+        if Xj(i)>0
+            rectangle('position',[-Xj(i) n+1-i 2*Xj(i)  1],'facecolor','c','Curvature',[0.02 0.02]);
+            text(0, n+1.5-i ,num2str(Xj(i),'%.1f'),'HorizontalAlignment','center','FontSize',15,'VerticalAlignment','middle');
+        end
+    end
+    
+    set(gca,'YTickLabel',Labels);
+    
+    box('on');
 end
 
-set(axes1,'YTickLabel',Labels);
-
-box('on');
 h=gcf;
 end
 %FScategory:VIS-Clu
