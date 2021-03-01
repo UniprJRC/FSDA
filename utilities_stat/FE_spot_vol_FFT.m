e0c7dbf841bb945e14623e46799e5c8cc6166e24
SimonaSanfeliciUNIPR <simona.sanfelici@unipr.it>
2021-03-01 18:59:00 +0100

    Fixed some minor errors

--- utilities_stat/FE_spot_vol_FFT.m
+++ utilities_stat/FE_spot_vol_FFT.m
@@ -81,7 +81,7 @@ function [spotvar,tau] = FE_spot_vol_FFT(x,t, varargin)
 % Examples:
 
 %{
-    %% Example of call of FE_spot_vol with just two input arguments.
+    %% Example of call of FE_spot_vol_FFT with just two input arguments.
     % Generates price and instantaneous variance from the Constant Elasticity of Variance model [S. Beckers, The Journal of Finance, Vol. 35, No. 3, 1980] and estimates the instantaneous variance via the Fourier method.
     n=1000;  T=1; t=0:T/n:T; S0=100;
     [S,sigma]=CEVmodel(t,S0); % data generation
@@ -92,7 +92,7 @@ function [spotvar,tau] = FE_spot_vol_FFT(x,t, varargin)
 %}
 
 %{
-    %% FE_spot_vol called with optional input argument N and M.
+    %% FE_spot_vol_FFT called with optional input argument N and M.
     % Generates price and instantaneous variance from the Constant Elasticity of Variance model [S. Beckers, The Journal of Finance, Vol. 35, No. 3, 1980] and estimates the instantaneous variance via the Fourier method.
     n=21600;  T=1; t=0:T/n:T; S0=100;
     [S,sigma]=CEVmodel(t,S0); % data generation
@@ -104,7 +104,7 @@ function [spotvar,tau] = FE_spot_vol_FFT(x,t, varargin)
 %}
 
 %{
-    %% Example of call of FE_spot_vol providing both estimation values and times.
+    %% Example of call of FE_spot_vol_FFT providing both estimation values and times.
     % Generates price and instantaneous variance from the Constant Elasticity of Variance model [S. Beckers, The Journal of Finance, Vol. 35, No. 3, 1980] and estimates the instantaneous variance via the Fourier method.
     n=21600;  T=1; t=0:T/n:T; S0=100;
     [S,sigma]=CEVmodel(t,S0); % data generation
@@ -167,16 +167,15 @@ idx=M+N+1:-1:2; ff=fft_v(idx);
 fft_def=[conj(ff) fft_v(1:M+N+1)];
 fft_def=fft_def./T; % Fourier coeff. of log-returns
 
-idxk=-N:1:N; nshift=M+N+1;
-k=-M:1:M;
-coeff=zeros(1,length(k));
-for kk=k
+idxk=-N:1:N; nshift=M+N+1; coeff=zeros(1,2*M+1);
+for kk=-M:M
     idxx=idxk+nshift+kk;
     Capp=fft_def(idxx);
     coeff(M+kk+1)=Capp*fft_def(nshift-N:nshift+N)';
 end
 C=coeff.*(T/(2*N+1)); % Fourier coeff. of variance
 
+k=(-M:1:M);
 f=C.*(1-abs(k)/M);
 Fsum=(2*M+1)*ifft(f);
 Fsum=exp(-1i*2*pi*M*(k+M)/(2*M+1)).*Fsum;
