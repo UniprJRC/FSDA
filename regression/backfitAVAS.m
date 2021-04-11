5e83bc41138f4afd2323e951ed69693bf9197f09
Marco Riani <mriani@unipr.it>
2021-04-11 18:12:04 +0200

    avas robustified. Added options rob and  PredictorOrderR2. HTML page regenerated

--- regression/backfitAVAS.m
+++ regression/backfitAVAS.m
@@ -1,4 +1,4 @@
-function [newtX, rsq]=backfitAVAS(ty,tX,X,w,M,l,rsq,maxit,sw,p,delrsq)
+function [newtX, rsq]=backfitAVAS(ty,tX,X,w,M,l,rsq,maxit,sw,p,delrsq,bsb,outliers,PredictorOrderR2)
 %backfitAVAS contains the backfitting algorithm (inner loop) inside avas function
 %
 % This funtion is not intended to be called directly
@@ -6,6 +6,14 @@ function [newtX, rsq]=backfitAVAS(ty,tX,X,w,M,l,rsq,maxit,sw,p,delrsq)
 % Copyright 2008-2021.
 % Written by FSDA team
 %$LastChangedDate::                      $: Date of the last commit
+n=length(ty);
+if nargin<13
+    bsb=1:n;
+    outliers=[];
+    PredictorOrderR2=false;
+elseif nargin<14
+    PredictorOrderR2=false;
+end
 
 % ($nit$ = counter for number of iterations in the inner loop).
 nit=0;
@@ -26,26 +34,77 @@ while lfinishInnerLoop==1 % Beginning of Inner Loop
     % Loop through all the explanatory variables
     % and transform them
     %   do 420 i=1,p (in Fortran code)
-    for  j=1:p
+    
+    if PredictorOrderR2 ==true && p>1
+        varin=false(p,1);
+        varout=true(p,1);
+        ChooseBestR2=true;
+        sqweights = w(bsb).^(1/2);
+        tywbsb = ty(bsb) .* sqweights;
+        seqp=1:p;
+    else
+        ChooseBestR2=false;
+    end
+    % avas is dependent on the order of the predictors in X.
+    
+    for  ij=1:p
+        
+        if ChooseBestR2 == true
+            tXbsbVarout=tX(bsb,varout);
+            % Indexes (interger numbers in 1, ...p) containing the
+            % variables not included yet in the transformation 
+            tXcandIndexes=seqp(varout);
+            R2cand=zeros(length(tXcandIndexes),1);
+            % Matrix tX referred to the explanatory variables already
+            % included
+            tXbsbVarin=tX(bsb,varin);
+            for jj=1:size(tXbsbVarout,2)
+                tXjj=[tXbsbVarin tXbsbVarout(:,jj)];
+                tXjjw=tXjj(bsb,:).*sqweights;
+                % estimate of beta from weighted regression (RWLS)
+                bjj=tXjjw\tywbsb;
+                DEVres=sum(((ty(bsb)-tXjj(bsb,:)*bjj).^2).*w);
+                % Note that ty is standardized
+                R2jj=1-DEVres/sw;
+                R2cand(jj)=R2jj;
+            end
+            [~,indmax]=max(R2cand);
+            j=tXcandIndexes(indmax);
+            % Variable j is selected as the one producing the best R2 given
+            % those already in
+            varin(j)=true;
+            varout(j)=false;
+        else
+            j=ij;
+        end
         
         % sorted indexes of the j-th column of X
         ordXj=M(:,j);
+        if ~isempty(outliers)
+            ordXj=intersect(ordXj,bsb,'stable');
+        end
         
-        %   Find $Z_1= Z_5 + tX_i$ (note that the observations of all the matrices involved are ordered using the ith column of matrix $M$).
-        %   Therefore to be precise   $Z_1= Z_5(M(:,i)) + tX_i(M(:,i))$.
-        %   $Z_1$ is the $n \times 1$ vector
-        %   which contains the residuals from transformed $y$ and transformed $X$ excluding
-        %   variable $X_i$ with values ordered according to $X_i$.
+        %   Find eplustXjOrdXj (eplustXjOrdXj is Z_1 is the
+        %   original fortran code)
+        %   (note that the observations of all the matrices involved are
+        %   ordered using the j-th column of matrix $M$).
+        %   Therefore to be precise   $eplustXjOrdXj= e(M(:,j)) + tX_i(M(:,j))$.
+        %   $eplustXjOrdXj$ is the $n \times 1$ vector
+        %   which contains the residuals from transformed $y$ and transformed $X$, $tX$ excluding
+        %   variable $X_j$ with values ordered according to $X_j$.
         %   \[
-        %   Z_1=  ty(M(:,i))- \sum_{j \ne i } tX_j(M(:,i)), \qquad j=1, 2, \ldots, p
+        %   eplustXjOrdXj=  ty(M(:,j))- \sum_{i \ne j } tX_j(M(:,j)), \qquad i=1, 2, \ldots, p
         %   \]
-        %    The first element of $Z_1$ is associated with the lowest value of $X_i$, the second element is associated with the second lowet value of $X_i$ ...,
+        %    The first element of $eplustXjOrdXj$ is associated with the
+        %    lowest value of $X_j$, the second element is associated with
+        %    the second lowet value of $X_j$ ...,
         eplustXjOrdXj=e(ordXj)+tX(ordXj,j);
+        
         
         % xjord = original expl. variable X(:,j) sorted
         Xjord=X(ordXj,j);
         
-        % z4 = corresponding weights
+        % wOrdXj  corresponding weights (z4 in the fortran code)
         wOrdXj=w(ordXj);
         
         %  smo=smothr(abs(l(i)),z(:,2),z(:,1),z(:,4));
@@ -64,16 +123,21 @@ while lfinishInnerLoop==1 % Beginning of Inner Loop
         % Convert residual sum of squares into R2
         % Remember that y has been standardized
         sv=1-sv/sw;
-    
-        %%%%%%%% backfitAVAS senza questo if 
-%         if sv <= rsq
-%         else
-            % rsq = the multiple R-squared value for the transformed values
-            rsq=sv;
-            
-            tX(ordXj,j)=tXOrdXj;
-            e(ordXj)=eplustXjOrdXj-tXOrdXj;
-%         end
+        
+        %%%%%%%% backfitAVAS does not have this if
+        %         if sv <= rsq
+        %         else
+        % rsq = the multiple R-squared value for the transformed values
+        rsq=sv;
+        
+        tX(ordXj,j)=tXOrdXj;
+        e(ordXj)=eplustXjOrdXj-tXOrdXj;
+        %         end
+        
+        % Now compute fitted values for transformed variable Xj in correspondence of the outlierss
+        [XjordUni,XjOrdInd]=unique(Xjord);
+        tXjoutliers=interp1(XjordUni,tXOrdXj(XjOrdInd),X(outliers,j),'linear','extrap');
+        tX(outliers,j)=tXjoutliers;
         
     end       % End of the loop for the explanatory variables
     
@@ -86,6 +150,7 @@ while lfinishInnerLoop==1 % Beginning of Inner Loop
     end
 end % End of the Inner Loop
 
-% Store new tranformed values for the explanatory variables
+% Store new tranformed values for the explanatory variables (also included
+% are the estimated transformed values for the rows declared as outliers).
 newtX=tX;
 end
