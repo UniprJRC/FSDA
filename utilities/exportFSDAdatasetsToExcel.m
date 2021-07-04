3f77125732b366a390ef5f696f0a5db66ecf7464
Marco Riani <mriani@unipr.it>
2021-07-04 19:47:27 +0200

    Added new function exportFSDAdatasetsToExcel.m
    Former-commit-id: a3883b12db69e2fe21af451a3a5c585b19e35fbd

---
+++ utilities/exportFSDAdatasetsToExcel.m
@@ -0,0 +1,44 @@
+% This routine exports all dataset file of FSDA into Excel format
+% (FSDAroot)/datasetExcel is created with subfolders
+% clustering 
+% multivariate
+% multivariate_regression
+% regression
+% Every subfoler contains all FSDA datasets in Excel (.xlsx) format
+
+%% Find FSDA root
+FileName='addFSDA2path';
+FullPath=which(FileName);
+%Navigate to the main folder of FSDA
+FSDAroot=fileparts(FullPath);
+cd(FSDAroot)
+
+% create subfolder datasetsExcel
+mkdir('datasetsExcel')
+cd('datasetsExcel\')
+datasetExcelFolder=pwd;
+cd('..\')
+
+%% Export datasets inside subfolder clustering
+
+cd(datasetExcelFolder)
+folders={'clustering' 'multivariate' 'regression' 'multivariate_regression'};
+for ii=1:length(folders)
+    folderi=folders{ii};
+
+    mkdir(folderi)
+    pathFSDAfolder=[FSDAroot '\datasets\' folderi '\'];
+    pathFSDAfolderExcel=[datasetExcelFolder '\' folderi '\'];
+    d=dir([pathFSDAfolder '*.mat']);
+    n=length(d);
+    if n==0
+        error('FSDA:exportFSDAdatasetstoExcel','Files not found')
+    end
+    for i=1:n
+        FileName=d(i).name;
+        [~,FileNameNoext]=fileparts(FileName);
+        load(FileName)
+        writetable(eval(['' FileNameNoext '']),[pathFSDAfolderExcel FileNameNoext '.xlsx'],'WriteRowNames',true)
+    end
+
+end
