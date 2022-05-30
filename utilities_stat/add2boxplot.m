c789ef550fcd9a55b4c2719b16fb7fd1695c27bb
MarcoRianiUNIPR <mriani@unipr.it>
2022-05-30 14:12:05 +0100

    All HTML files regenerated

--- utilities_stat/add2boxplot.m
+++ utilities_stat/add2boxplot.m
@@ -1,5 +1,5 @@
 function add2boxplot(Y,g)
-%add2boxplot added labels to the boxplot figure
+%add2boxplot add labels to the boxplot figure
 %
 %<a href="matlab: docsearchFS('add2boxplot')">Link to the help function</a>
 %
@@ -216,3 +216,5 @@ if ~isempty(outliersYcoo)
 
 end
 end
+
+%FScategory:VIS-Mult
