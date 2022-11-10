f4cfed4dc7d75ec9aab01a59ca70d38d68876715
calcapa <patrizia.calcaterra@ext.ec.europa.eu>
2022-11-10 18:35:30 +0100

    Copyright changed

--- regression/FSR.m
+++ regression/FSR.m
@@ -352,7 +352,7 @@ function [out]=FSR(y,X,varargin)
 % number of multivariate outliers, "Journal of the Royal Statistical
 % Society Series B", Vol. 71, pp. 201-221.
 %
-% Copyright 2008-2021.
+% Copyright 2008-2023.
 % Written by FSDA team
 %
 %
