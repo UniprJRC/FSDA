e262bc7f9f845c1859e6608d6b3457870d80f607
MarcoRianiUNIPR <mriani@unipr.it>
2022-11-26 10:07:30 +0100

    examples in LTStsLSmult corrected and HTML regenerated

--- regression/LTStsLSmult.m
+++ regression/LTStsLSmult.m
@@ -103,7 +103,7 @@ function out = LTStsLSmult(y,varargin)
 % Rousseeuw and Mia Hubert
 %
 %
-%<a href="matlab: docsearchFS('LTSts')">Link to the help function</a>
+%<a href="matlab: docsearchFS('LTStsLSmult')">Link to the help function</a>
 %
 %$LastChangedDate:: 2019-12-15 21:09:21 #$: Date of the last commit
 
@@ -125,20 +125,17 @@ function out = LTStsLSmult(y,varargin)
     % Monitoring of Many Time Series with Application to Fraud Detection,
     % "Econometrics and Statistics".
 
-    load('P_12119085_TQV_full.txt'); % KE-GB
-    load('P_17049075_TQV_full.txt'); % UA-LT
+    load('P12119085full.txt'); % KE-GB
+    load('P17049075full.txt'); % UA-LT
     
-    dates1 = P_12119085_TQV_full(:,1);
-    yin1   = P_12119085_TQV_full(:,2);
-    
-    dates2 = P_17049075_TQV_full(:,1);
-    yin2   = P_17049075_TQV_full(:,2);
+    yin1   = P12119085full(:,2);
+    yin2   = P17049075full(:,2);
     
     out1  = LTStsLSmult(yin1,'msg',true,'plots',1);
-    title('P_12119085_KE_GB','interpreter','none','Fontsize',20);
+    title('P12119085_KE_GB','interpreter','none','Fontsize',20);
     pause(1);
     out2  = LTStsLSmult(yin2,'msg',true,'plots',1);
-    title('P_17049075_UA_LT','interpreter','none','Fontsize',20);
+    title('P17049075_UA_LT','interpreter','none','Fontsize',20);
 %}
 
 
@@ -146,9 +143,8 @@ function out = LTStsLSmult(y,varargin)
 % Multiple level shift and variable selection. Example 1.
 % Detection of multiple Level Shifts followed by variable selection on the
 % dataset of example before
-  load('P_17049075_TQV_full.txt'); % UA-LT
-  dates2 = P_17049075_TQV_full(:,1);
-  yin2   = P_17049075_TQV_full(:,2);
+  load('P17049075full.txt'); % UA-LT
+  yin2   = P17049075full(:,2);
   out = LTStsLSmult(yin2,'maxLS',4,'alphaLTS',0.01,...
     'alphaLS',0.01,'thresLS',0.01,'plots',1,'msg',1);
   outX = out.outX;
@@ -168,18 +164,17 @@ model.X = outX(:,3:end);
 % Multiple level shift and variable selection. Example 2.
 % Detection of multiple Level Shifts followed by variable selection on the
 % dataset of example before
-  load('P_12119085_TQV_full.txt'); % UA-LT
-  dates2 = P_12119085_TQV_full(:,1);
-  yin2   = P_12119085_TQV_full(:,2);
+  load('P12119085full.txt'); % UA-LT
+  yin2   = P12119085full(:,2);
   out = LTStsLSmult(yin2,'maxLS',4,'alphaLTS',0.01,...
     'alphaLS',0.01,'thresLS',0.01,'plots',1,'msg',1);
   outX = out.outX;
-model.trend = 2;
-model.lshift = 0;
-model.seasonal=303;
-model.X = outX(:,3:end);
-
-[out_model_1, out_reduced_1] = LTStsVarSel(yin2,'model',model,'plots',1);
+    model.trend = 2;
+    model.lshift = 0;
+    model.seasonal=303;
+    model.X = outX(:,3:end);
+    
+    [out_model_1, out_reduced_1] = LTStsVarSel(yin2,'model',model,'plots',1);
 
 [out_LTSts]=LTSts(yin2,'model',out_model_1,...
     'plots',1,'msg',0,'dispresults',1,'SmallSampleCor',1,'conflev',1-0.01/length(yin2));
@@ -190,6 +185,7 @@ model.X = outX(:,3:end);
 
 options=struct('msg',false,'plots',0,'maxLS',5,'alphaLTS',0.05,'bdpLTS',0.1,...
     'alphaLS',0.01,'thresLS',20,'startLS',3,'sampleLS',[]);
+
 for i=1:2:length(varargin)
     options.(varargin{i})=varargin{i+1};
 end
@@ -257,6 +253,7 @@ while LSsignif==true &&  ij<=maxLS
         out_LTSts_tmp=outTent; %#ok<NASGU>
     else
         LSsignif=false;
+        outTent.posLS=[];
         wedgeplot(outTent,'extradata',[y outTent.yhat]); 
     end
 end
@@ -269,7 +266,8 @@ model.lshift = 0;
 outLTSts = LTSts(y,'model',model,'conflev',1-alphaLTS/T,'plots',plots,'msg',0,'bdp',bdpLTS,'yxsave',true);
 yhat = outLTSts.yhat;
 outliers = outLTSts.outliers;
-goodobs = 1:T;  goodobs(outliers)=[];
+goodobs = 1:T;  
+goodobs(outliers)=[];
 res = (y-yhat);
 resgood = res(goodobs);
 % numerator of estimate of the error variance
