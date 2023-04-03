681167c439b902a14480c67d20c338deaa07f4a9
Bensingh Pancras <bpancras@mathworks.com>
2023-04-03 15:09:57 +0530

    minor update

--- utilities_help/createFSDAToolboxActions.m
+++ utilities_help/createFSDAToolboxActions.m
@@ -1,10 +1,10 @@
-% This file is meant to run on GitHub Actions, the runner is always a Linux
+% This file is meant to run on a Linux node in GitHub Actions
 prjFileName=".."+filesep+"/fsdaToolboxPackaging.prj";
 toolboxOption=matlab.addons.toolbox.ToolboxOptions(prjFileName);
 % toolboxOption.Identifier="fsdaToolbox.UParma";
 
-% Need to edit the line below for every release
-toolboxOption.ToolboxVersion="0.0.17";
+% Need to edit the line below before every release
+toolboxOption.ToolboxVersion="0.0.22";
 toolboxOption.OutputFile=".."+filesep+"release"+filesep+"fsda.mltbx";
 
 %Create toolbox MLTBX
