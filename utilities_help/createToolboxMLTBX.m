bb2694a9880d38e42db64dbc50dd90815f97a3d1
Bensingh Pancras <bpancras@mathworks.com>
2023-04-26 13:06:33 +0530

    replaced verNumber with toolboxVersion

--- utilities_help/createToolboxMLTBX.m
+++ utilities_help/createToolboxMLTBX.m
@@ -1,17 +1,17 @@
-function createToolboxMLTBX(verNumber)
+function createToolboxMLTBX(toolboxVersion)
 % This file is meant to run on a Linux node in GitHub Actions
 % Execute the code from project root
 % The code also assumes that there is a fsdaToolboxPackaging.prj at the
 % project root.
 % Input:
-%   verNumber- string in major.minor.bugfix format
+%   toolboxVersion- string in major.minor.bugfix format
 % Copyright 2023 The MathWorks, Inc.
 
 prjFileName = "fsdaToolboxPackaging.prj";
 toolboxOption = matlab.addons.toolbox.ToolboxOptions(prjFileName);
 
 % Update the version number
-toolboxOption.ToolboxVersion = verNumber;
+toolboxOption.ToolboxVersion = toolboxVersion;
 
 % Name for MLTBX file
 toolboxOption.OutputFile = fullfile("release", "fsda.mltbx");
