328274b7d0d78371345fb0196cac2790a2bedb91
MarcoRianiUNIPR <mriani@unipr.it>
2023-05-29 10:41:13 +0200

    Added possibility of checking document location in different versions of MATLAB
    Former-commit-id: bd811f89d5cbbb3e107c0079e52977f4bb174eb1

--- utilities/webFS.m
+++ utilities/webFS.m
@@ -1,34 +1,34 @@
 function webFS(varargin)
-% In case of MATLAB 2021A or greater if the user has chosen as
-% Documentation Location "web on Mathworks" and the user is searching for a
-% FSDA function temporarily change the Domain in order to prevent to search
-% in Mathworks web site.
+% We check whether the user has chosen as
+% Documentation Location "web" or "locally", depending on that we show the
+% FSDA documentation on rosa.unipr.it/FSDA or locally
 
 %% Beginning of code
 
-if verLessThanFS([9 10])
+s=settings;
+
+if verLessThanFS([9 10]) % < R2021a
     web(varargin{:});
-else
-    s=settings;
+elseif verLessThanFS([9 14]) % From R2021a to R2022b
+    try
+        PersonalvalueDOC=s.matlab.help.DocCenterLocation21a.PersonalValue;
+    catch
+        PersonalvalueDOC='WEB';
+    end
+
+else % From 2023a
     try
         PersonalvalueDOC=s.matlab.help.DocLocation.PersonalValue;
     catch
         PersonalvalueDOC='WEB';
     end
-
-     if strcmp(PersonalvalueDOC,'WEB')
-         disp('In order to be able to see locally the FSDA documentation locally')
-         disp('It is necessary in Home|Preferences to set ''Documentation Location''')
-         disp('to ''Installed Locally or restart MATLAB''')
-         [~,nameFile,ext]=fileparts(varargin);
-         varargin{:}=['http://rosa.unipr.it/FSDA/' nameFile ext];
-%         warning('FSDA:webFS:Wrongloc','Please change Documentation Location in order to view HTML FSDA documentation')
-%         %     % Change in a temporary way the user option to INSTALLED
-%         s.matlab.help.DocCenterLocation21a.PersonalValue='INSTALLED';
-%         %     web(varargin{:});
-%         %     % Restore previous option
-%         %     s.matlab.help.DocCenterLocation21a.PersonalValue=PersonalvalueDOC;
-%         %  com.mathworks.mlservices.MLHelpServices.setDocCenterDomain('');
-     end
-    web(varargin{:});
+end
+if strcmp(PersonalvalueDOC,'WEB')
+    disp('In order to be able to see the FSDA documentation locally,')
+    disp('it is necessary in ''Home|Preferences'' to set ''Documentation Location''')
+    disp('to ''Installed Locally''')
+    [~,nameFile,ext]=fileparts(varargin);
+    varargin{:}=['http://rosa.unipr.it/FSDA/' nameFile ext];
+end
+web(varargin{:});
 end
