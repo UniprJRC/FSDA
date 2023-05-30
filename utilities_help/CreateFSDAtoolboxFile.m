e18788fe00ee5c4a929d2fcd20c0da751edc1f97
MarcoRianiUNIPR <mriani@unipr.it>
2023-05-30 16:57:06 +0200

    GettingStarted.mlx updated
    Former-commit-id: 8886fef38264247a030b7c43faf7dfa1b398f112

--- utilities_help/CreateFSDAtoolboxFile.m
+++ utilities_help/CreateFSDAtoolboxFile.m
@@ -3,10 +3,10 @@
 %% Beginning of code
 
 % specify the version number, please use the format 'major.minor.revision'
-newVersion = '8.7.0.7';
+newVersion = '8.7.1.0';
 
 % Add the sentence which describes the new feature of the release
-commentRelease='Added new function txmerge';
+commentRelease='Added new suite of functions for robust regression using Andrews link';
 
 % Specify folder where to create the toolbox project
 FSDAProjFolder='D:\tmp';
