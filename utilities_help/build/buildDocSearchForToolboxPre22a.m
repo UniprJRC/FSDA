bce4f852b0bb458b958ab7b642b388ed5db6d036
Jos Martin <jlmartin@mathworks.com>
2023-06-07 16:19:19 +0100

    Use internal functions to build doc search on 21b

--- utilities_help/build/buildDocSearchForToolboxPre22a.m
+++ utilities_help/build/buildDocSearchForToolboxPre22a.m
@@ -5,5 +5,5 @@ function buildDocSearchForToolboxPre22a
 % builddocsearchdb(fullfile(FSDAroot, "helpfiles", "pointersHTML"))
 com.mathworks.mlwidgets.help.HelpPrefs.addProduct(FSDAroot); %#ok<JAPIMATHWORKS>
 com.mathworks.mlwidgets.help.customdoc.CustomToolboxIndexer.index(fullfile(pwd, "helpfiles", "pointersHTML")); %#ok<JAPIMATHWORKS>
-ls helpfiles\pointersHTML\helpsearch-v3\
+ls helpfiles/pointersHTML/helpsearch-v3/
 end
