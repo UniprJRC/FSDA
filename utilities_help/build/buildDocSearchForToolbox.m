b5d94046f450e47f8968e475fd3fc4549bd7b992
Jos Martin <jlmartin@mathworks.com>
2023-06-07 14:19:24 +0100

    Build doc on windows 21b with pause

--- utilities_help/build/buildDocSearchForToolbox.m
+++ utilities_help/build/buildDocSearchForToolbox.m
@@ -1,6 +1,12 @@
 function buildDocSearchForToolbox
 % Select the root folder
 [FSDAroot, cleanup] = changeDirToRootWithCleanup; %#ok<ASGLU>
+
+rehash
+drawnow
+pause(3)
+which info.xml
+
 % build the doc search database
 builddocsearchdb(fullfile(FSDAroot, "helpfiles", "pointersHTML"))
 end
