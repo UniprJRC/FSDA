da9feba081ef6757046d7ee2b0b65ec4cd1e0b11
Jos Martin <jlmartin@mathworks.com>
2023-06-08 18:03:56 +0100

    Move UUID to config prj file

--- utilities_help/build/buildDocSearchForToolbox.m
+++ utilities_help/build/buildDocSearchForToolbox.m
@@ -1,12 +1,6 @@
 function buildDocSearchForToolbox
 % Select the root folder
 [FSDAroot, cleanup] = changeDirToRootWithCleanup; %#ok<ASGLU>
-
-rehash
-drawnow
-pause(3)
-which info.xml
-
 % build the doc search database
 builddocsearchdb(fullfile(FSDAroot, "helpfiles", "pointersHTML"))
 end
