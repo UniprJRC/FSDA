7ac406af87d7e88cb79c5765fd41905ee2213497
Jos Martin <jlmartin@mathworks.com>
2023-06-09 11:03:14 +0100

    Code comments from bpancras incorporated

--- utilities_help/build/buildDocSearchForToolboxPre22a.m
+++ utilities_help/build/buildDocSearchForToolboxPre22a.m
@@ -1,9 +1,22 @@
 function buildDocSearchForToolboxPre22a
-% Select the root folder
+% Change to the root folder so that we can then pick up all the correct
+% files.
 [FSDAroot, cleanup] = changeDirToRootWithCleanup; %#ok<ASGLU>
-% build the doc search database
+% build the doc search database - Normally we would just call the following
+% line of code - however prior to R2022b this doesn't work in nodisplay
+% MATLAB so we need to look at its internals and do the bits needed.
+
 % builddocsearchdb(fullfile(FSDAroot, "helpfiles", "pointersHTML"))
+
+% Firstly we need to ensure that the help preference system knows about the
+% info.xml file that is in the FSDA root - this is what registers the
+% existence of the toolbox with help search
 com.mathworks.mlwidgets.help.HelpPrefs.addProduct(FSDAroot); %#ok<JAPIMATHWORKS>
+% Then we need to index the help using the customer toolbox indexer - this
+% is what you can see in builddocsearchdb
 com.mathworks.mlwidgets.help.customdoc.CustomToolboxIndexer.index(fullfile(pwd, "helpfiles", "pointersHTML")); %#ok<JAPIMATHWORKS>
-ls helpfiles/pointersHTML/helpsearch-v3/
+% Finally - lets show that this worked
+if exist(fullfile(FSDAroot, 'helpfiles', 'pointersHTML', 'helpsearch-v3'), 'dir')
+    disp('docsearch index built in helpsearch-v3 folder')
+end
 end
