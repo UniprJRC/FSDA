6a8419c792bc55a0ed7389aac33444be6bebdc6d
Jos Martin <jlmartin@mathworks.com>
2023-06-09 11:03:14 +0100

    Code comments from bpancras incorporated
    Former-commit-id: 7ac406af87d7e88cb79c5765fd41905ee2213497

--- buildfile.m
+++ buildfile.m
@@ -2,23 +2,30 @@ function plan = buildfile
 
 plan = buildplan(localfunctions);
 
+% Build doc before packaging toolbox as it is needed in the toolbox
 plan("toolbox").Dependencies = "doc";
 
-% Make the "createMLTBX" task the default task in the plan
+% Make the "toolbox" task the default task in the plan
 plan.DefaultTasks = "toolbox";
 end
 
 function docTask(~)
+% This task builds the doc search DB for the current version of MATLAB - the
+% expected output will be in the folder ./helpfiles/pointersHTML
 cleanup = iCdWithRevert(fullfile(iGetRootFolder, "utilities_help", "build")); %#ok<NASGU>
 buildDocSearchForToolbox
 end
 
 function toolboxTask(~)
+% This task packages the toolbox MLTBX file - the expected output will be
+% in the ./bin/ folder (defined in the createMLTBX file)
 cleanup = iCdWithRevert(fullfile(iGetRootFolder, "utilities_help", "build")); %#ok<NASGU>
 createMLTBX
 end
 
 function root = iGetRootFolder()
+% buildfile.m is always in the root of the toolbox - so use this definition
+% to get the root of the toolbox from the location of buildfile.m
 persistent THE_ROOT
 if isempty(THE_ROOT)
     thisFile = mfilename("fullpath");
@@ -28,6 +35,7 @@ root = THE_ROOT;
 end
 
 function cleanup = iCdWithRevert(folder)
+% Change to folder (probably build folder) and revert afterwards to pwd
 cdir = pwd;
 cleanup = onCleanup(@() cd(cdir));
 cd(folder)
--- utilities_help/build/createMLTBX.m
+++ utilities_help/build/createMLTBX.m
@@ -1,10 +1,10 @@
 function createMLTBX 
-%% File which automatically creates toolbox file FSDA.mltbx for fileexchange
+%% Create toolbox file for FSDA, both locally and in a CI action
 
-
-%% Beginning of code
-
-% specify the version number, please use the format 'major.minor.revision'
+% Specify the version number, please use the format 'major.minor.revision'
+% During a github build this will be taken from github.ref_name and stored
+% in GITHUB_ENV - and for local sandbox builds that don't specify this we
+% simply default to a large 10.0.0 release
 newVersion = getenv('GITHUB_RELEASE_TAG');
 
 if isempty(newVersion)
@@ -12,7 +12,9 @@ if isempty(newVersion)
     newVersion = "10.0.0";
 end
 
-% Specify folder where to create the toolbox project
+% Builds need to start from the root to cleanly define all the relative
+% paths, but revert back to where ever we started from when the "cleanup"
+% object goes out of scope
 [FSDAroot, cleanup] = changeDirToRootWithCleanup; %#ok<ASGLU>
 
 % Get filesep
@@ -27,10 +29,17 @@ publish('Contents.m');
 
 %% Create toolbox project file
 
-% Note that when matlab.addons.toolbox.ToolboxOptions the files attached to
-% the project are automatically added inside options.ToolboxFiles 
+% Create our desired toolbox options from the default toolbox package
+% configuration project fil in the root. This specifies as little as
+% possible ... but it does define the following:
+%
+% 1. Our File Exchange UUID (currently 20669fbc-61ca-4050-bc87-575422f4c0b8)
+% 2. By default ALL files should be packaged in the toolbox - we will
+%   exclude those that are not needed below using some helpers.
 options = matlab.addons.toolbox.ToolboxOptions(fullfile(FSDAroot, "defaultToolboxPackageConf.prj"));
 
+% Firstly there are a set of folders in this repository that we do not want
+% in the packaged toolbox - remove those using a helper function
 options = removeFoldersFromToolboxPackage(options, [ ...
     "_automation_tools"
     "_development"
@@ -46,6 +55,8 @@ options = removeFoldersFromToolboxPackage(options, [ ...
     "Univ"
     ]);
 
+% Secondly there are a set of files in this repository that we do not want
+% in the packaged toolbox - remove those using a helper function
 options = removeFilesFromToolboxPackage(options, [...
     "examples" + fsep + "examples_categorical.mlx"
     "examples" + fsep + "examples_multivariate.mlx"
@@ -68,7 +79,9 @@ options = removeFilesFromToolboxPackage(options, [...
     ]);
 
 
-% add FSDA paths
+% Define the paths that we want to add to an installed MATLAB path
+% NOTE - we need the root as well as some sub-folders so include the empty
+% string at the beginning
 pathsToAdd = [ ...
     ""
     "multivariate"
@@ -87,36 +100,30 @@ pathsToAdd = [ ...
     "FSDAdemos"    
     ];
 
-% NOTE scalar expansion here with vector of pathsToAdd
+% AND NOTE scalar expansion here with vector of pathsToAdd
 options.ToolboxMatlabPath = FSDAroot + fsep + pathsToAdd;
 
-% add toolbox name
+% Define our TOOLBOX name, version and other metadata
 options.ToolboxName = "FSDA";
 
-% add toolbox version
 options.ToolboxVersion = newVersion;
 
-% Detailed description of the toolbox.
 options.Description="Flexible Statistics and Data Analysis (FSDA) extends MATLAB for " + ...
     "a robust analysis of data sets affected by different sources of heterogeneity. " + ...
     "It is open source software licensed under the European Union Public Licence (EUPL). " + ...
     "FSDA is a joint project by the University of Parma and the Joint Research Centre " + ...
     "of the European Commission."; 
 
-% add summary description of the toolbox
 options.Summary="Flexible Statistics Data Analysis Toolbox";
 
-% add toolbox author.
 options.AuthorName= "Marco Riani";
 
-% add email address address
 options.AuthorEmail = "FSDA@unipr.it";
 
-% added company
 options.AuthorCompany = "University of Parma (UNIPR) and Joint Research Centre of the " + ...
     "European Commission(JRC).";
  
-% add architecture support (option name changed in the release)
+% Define the final architectures that we will correctly work on
 options.SupportedPlatforms.Win64 = true;
 options.SupportedPlatforms.Maci64 = true;
 options.SupportedPlatforms.Glnxa64 = true;
@@ -134,29 +141,50 @@ options.ToolboxGettingStartedGuide = fullfile(FSDAroot, 'doc', 'GettingStarted.m
 
 % add gallery files
 options.AppGalleryFiles=[];
-% options.OutputFile='testFSDA';
-mkdir(FSDAroot, 'bin')
 
+mkdir(FSDAroot, 'bin')
 options.OutputFile = fullfile(FSDAroot, "bin", "FSDA");
 
+% Display the options during build in case the github action fails and we
+% need some debugging output.
 disp(options)
 
+% Finally - package the toolbox
 disp('Package toolbox and create file FSDA.mltbx')
 matlab.addons.toolbox.packageToolbox(options);
-
 
 end
 
 function options = removeFoldersFromToolboxPackage(options, foldersToRemove)
+% This helper function removes all files in a a set of folders (defined
+% relative to the root of the package) from the ToolboxFiles list. This is
+% done using string comparison startsWith to find all the currently listed
+% files in ToolboxFiles that start with the desired folders.
 root = options.ToolboxFolder;
+% NOTE - likely scalar expansion of root to accomodate multiple
+% foldersToRemove - DO NOT replace with fullfile which does not support
+% expansion.
+% Also note addition of filesep on the end to ensure that ONLY files in
+% folders are removed. Without this a file that began with the name
+% folderToRemove and continued with more chars would ALSO be removed.
 foldersToRemove = root + filesep + foldersToRemove + filesep;
+% NOTE - startsWith takes multiple patterns for second option - we are in
+% that case
 itemsToRemove = startsWith(options.ToolboxFiles, foldersToRemove);
 options.ToolboxFiles(itemsToRemove) = [];
 end
 
 function options = removeFilesFromToolboxPackage(options, filesToRemove)
+% This helper function removes all files listed from the ToolboxFiles list.
+% This is done using string comparison matches to find all the currently
+% listed files in ToolboxFiles that match the desired list.
 root = options.ToolboxFolder;
+% NOTE likely scalar expansion of root to accomodate multiple
+% filesToRemove - DO NOT replace with fullfile which does not support
+% expansion
 filesToRemove = root + filesep + filesToRemove;
+% NOTE - matches takes multiple patterns for second option - we are in
+% that case
 itemsToRemove = matches(options.ToolboxFiles, filesToRemove);
 options.ToolboxFiles(itemsToRemove) = [];
 end
