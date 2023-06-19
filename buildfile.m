ea631a6adf097645aa20e143afafe2eaa5d49880
Aldo Corbellini <aldo.corbellini@unipr.it>
2023-06-19 11:35:26 +0200

    added task releaseToGithub that tags a new release and build the toolbox MLTBX file -on GitHub- using createMLTBX.m and GiHub Actions.

--- buildfile.m
+++ buildfile.m
@@ -23,6 +23,34 @@ cleanup = iCdWithRevert(fullfile(iGetRootFolder, "utilities_help", "build")); %#
 createMLTBX
 end
 
+function releaseToGithubTask(~, opts)
+% This task tags a new release and build the toolbox MLTBX file -on GitHub-
+% using createMLTBX.m and GiHub Actions 
+arguments
+    ~
+    opts.Version(1,1) string = ""
+    opts.Comment(1,1) string = ""
+end
+
+if opts.Version == "" || opts.Comment == ""
+    error('FSDA:ReleaseToGithub:IncorrectInputs',['You must specify ' ...
+        '2 strings, Version and Comment, as input to the releaseToGithub built task'])
+end
+
+[OK, msg] = system("git tag -a " + opts.Version + " -m " + opts.Comment);
+disp(msg);
+if OK ~= 0
+    error("FSDA:ReleaseToGithub:GitTagFailed", "git tag failed");
+end
+[OK, msg] = system("git push origin " + opts.Version);
+disp(msg)
+if OK ~= 0
+    error("FSDA:ReleaseToGithub:GitPushFailed", "git push failed");
+end
+end
+
+
+
 function root = iGetRootFolder()
 % buildfile.m is always in the root of the toolbox - so use this definition
 % to get the root of the toolbox from the location of buildfile.m
