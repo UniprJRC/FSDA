130c612c0fb27ac27b061a4bc3c4dea74744e542
MarcoRianiUNIPR <mriani@unipr.it>
2024-01-14 12:34:29 +0100

    Removed last example about R conection in inversegaminv.m

--- toolbox/utilities_stat/inversegaminv.m
+++ toolbox/utilities_stat/inversegaminv.m
@@ -138,33 +138,6 @@ function x = inversegaminv(p,a,b,nocheck)
     disp(max(max(abs(Ychk))));
 %}
 
-%{
-    % Compare results with R (library actuar).
-    % The example below assumes that the Connection with R has already been setup
-    % For more information on how to connect R and Matlab see file
-    % Connect_Matlab_with_R_HELP
-    % in folder
-    % disp(which('Connect_Matlab_with_R_HELP'))
-    chkMatlab_With_R_connection=exist('openR','file');
-    if chkMatlab_With_R_connection==0
-        disp('Connection with R has not been setup yet')
-        examp=which('Connect_Matlab_with_R_HELP.m');
-        examp1=strrep(examp,'\','\\');
-        stri=['See file <a href="matlab: opentoline(' examp1 ',27)">Connect_Matlab_with_R_HELP.m</a>  for more information'];
-        disp(stri);
-    else
-        openR
-        evalR('library(actuar)');
-        evalR('x=seq(0,0.9999,0.0001)');
-        evalR('a=2');
-        evalR('b=3');
-        yfromR=evalR('qinvgamma(x,a,scale=b)');
-        x=(0:0.0001:0.9999);
-        yfromMatlab=inversegaminv(x,2,3);
-        disp(max(abs(yfromR-yfromMatlab)));
-        closeR;
-    end
-%}
 
 %% Beginning of code
 
@@ -172,7 +145,7 @@ if nargin<4
     nocheck=0;
 end
 
-if nocheck==1
+if nt example about R coocheck==1
     % This code is much faster but there are no checks
     x = b./gammaincinv(p,a,'upper');
 else
