095777655538391573b652706d8d8e4ac7e54ba3
MarcoRianiUNIPR <mriani@unipr.it>
2025-03-22 18:56:11 +0100

    Added robust tab to pcaFS

---
+++ toolbox/+aux/computePCA.m
@@ -0,0 +1,191 @@
+function  [Ztable,Rtable,explained,explainedT,V,VT,loadings,loadingsT,communwithcum,communwithcumT,score,scoreT,orthDist,scoreDist]=computePCA(Y,bsb,rownames,varnames,standardize,NumComponents,dispresults,plots)
+% Compute all PCA quantities (this function is not intended to be called
+% directly)
+[n,v]=size(Y);
+Ybsb=Y(bsb,:);
+nbsb=size(Ybsb,1);
+
+center=mean(Ybsb);
+if standardize==true
+    dispersion=std(Ybsb);
+    % Create matrix of standardized data
+    Z=(Y-center)./dispersion;
+else
+    % Create matrix of deviations from the means
+    Z=Y-center;
+end
+
+% [~,S,loadings]=svd(Z./sqrt(n-1),0);
+% Z=(Y-mean(Y))*loadings;
+
+Ztable=array2table(Z,'RowNames',rownames,'VariableNames',varnames);
+
+% Correlation (Covariance) matrix in table format
+Zbsb=Z(bsb,:);
+R=corr(Zbsb);
+Rtable=array2table(R,'VariableNames',varnames,'RowNames',varnames);
+
+sigmas=sqrt(diag(R));
+
+% svd on matrix Z.
+[~,Gamma,V]=svd(Zbsb,'econ');
+Gamma=Gamma/sqrt(nbsb-1);
+
+% \Gamma*\Gamma = matrice degli autovalori della matrice di correlazione
+La=Gamma.^2;
+la=diag(La);
+
+%% Explained variance
+sumla=sum(la);
+explained=[la 100*(la)/sumla 100*cumsum(la)/sumla];
+namerows=cellstr([repmat('PC',v,1) num2str((1:v)')]);
+namecols={'Eigenvalues' 'Explained_Variance' 'Explained_Variance_cum'};
+explainedT=array2table(explained,'RowNames',namerows,'VariableNames',namecols);
+if isempty(NumComponents)
+    NumComponents=find(explained(:,3)>100*0.95^v,1);
+    if NumComponents==1 && v>1
+        disp('The first PC already explains more than 0.95^v variability')
+        disp('In what follows we still extract the first 2 PCs')
+        NumComponents=2;
+    end
+end
+
+% labels of the PCs
+pcnames=cellstr(num2str((1:NumComponents)','PC%d'));
+
+V=V(:,1:NumComponents);
+La=La(1:NumComponents,1:NumComponents);
+VT=array2table(V,'RowNames',varnames','VariableNames',pcnames);
+
+
+%% Loadings
+loadings=V*sqrt(La)./sigmas;
+loadingsT=array2table(loadings,'RowNames',varnames','VariableNames',pcnames);
+
+
+%% Principal component scores
+score=Z*V;
+scoreT=array2table(score,'RowNames',rownames,'VariableNames',pcnames);
+
+
+%% Communalities
+commun=loadings.^2;
+labelscum=cellstr([repmat([pcnames{1} '-'],NumComponents-1,1) char(pcnames{2:end})]);
+communcum=cumsum(loadings.^2,2);
+communwithcum=[commun communcum(:,2:end)];
+if isempty(labelscum{1})
+    varNames=pcnames;
+else
+    varNames=[pcnames; labelscum];
+end
+
+if verLessThanFS('9.7')
+    varNames=matlab.lang.makeValidName(varNames);
+end
+communwithcumT=array2table(communwithcum,'RowNames',varnames,...
+    'VariableNames',varNames);
+
+%% Orthogonal distance to PCA subspace based on k PC
+Res=Z-score*V';
+orthDist=sqrt(sum(Res.^2,2));
+
+
+%% Score distance in PCA subspace of dimension k
+larow=diag(La)';
+scoreDist=sqrt(sum(score.^2./larow,2));
+
+% Find the 5 units with the largest value of the combination between
+% orthogonal and score distance
+DD=[orthDist,scoreDist];
+mDD=zeros(1,2);
+distM=mahalFS(DD,mDD,cov(DD));
+[~,indsor]=sort(distM,1,"descend");
+selu=indsor(1:5);
+
+
+if dispresults == true
+    format bank
+    if standardize == true
+        disp('Initial correlation matrix')
+    else
+        disp('Initial covariance matrix')
+    end
+    disp(Rtable)
+
+    disp('Explained variance by PCs')
+    disp(explainedT)
+
+    disp('Loadings = correlations between variables and PCs')
+    disp(loadingsT)
+
+    disp('Communalities')
+    disp(communwithcumT)
+    format short
+
+    disp('Units with the 5 largest values of (combined) score and orthogonal distance')
+    disp(selu')
+end
+
+if plots==1
+
+    %% Explained variance through Pareto plot
+    % Delete figure if it already exists
+
+    delete(findobj(0, 'type', 'figure','tag','pl_eigen'));
+    figure('Name','Explained variance','Tag','pl_eigen')
+    [h,axesPareto]=pareto(explained(:,1),namerows);
+    % h(1) refers to the bars h(2) to the line
+    h(1).FaceColor='g';
+    linelabels = string(round(100*h(2).YData/sumla,2));
+    text(axesPareto(2),h(2).XData,h(2).YData,linelabels,...
+        'Interpreter','none');
+    xlabel('Principal components')
+    ylabel('Explained variance (%)')
+
+    %% Plot loadings
+    xlabels=categorical(varnames,varnames);
+    delete(findobj(0, 'type', 'figure','tag','pl_loadings'));
+    figure('Name','Loadings','Tag','pl_loadings')
+
+    for i=1:NumComponents
+        subplot(NumComponents,1,i)
+        b=bar(xlabels, loadings(:,i),'g');
+        title(['Correlations with PC' num2str(i)])
+        xtips=b(1).XData;
+        ytips=b(1).YData;
+        % The alternative instructions below only work from MATLAB
+        % 2019b
+        %   xtips = b.XEndPoints;
+        %   ytips = b.YEndPoints;
+        barlabels = string(round(loadings(:,i),2));
+        text(xtips,ytips,barlabels,'HorizontalAlignment','center',...
+            'VerticalAlignment','bottom')
+        title(['Correlations  with PC' num2str(i)])
+    end
+
+    %% Plot of orthogonal distance (Y) versus score distance (X)
+    delete(findobj(0, 'type', 'figure','tag','pl_OutlierMap'));
+    figure('Name','OutlierMap','tag','pl_OutlierMap')
+    group1=repelem("Normal units",n,1);
+    group1(bsb==0)="Outliers";
+    scatterboxplot(scoreDist,orthDist,'group',group1);
+    xlabel('Score distance')
+    ylabel('Orth. dist. from PCA subspace')
+    text(1.01,0,['Good' newline 'leverage' newline 'points'],'Units','normalized')
+    text(-0.05,0,['Normal' newline 'units'],'Units','normalized','HorizontalAlignment','right')
+    text(0.05,-0.05,['Normal' newline 'units'],'Units','normalized','HorizontalAlignment','left')
+    text(-0.05,1.05,'Orthogonal outliers','Units','normalized','HorizontalAlignment','left')
+    text(0.95,1.05,'Bad leverage points','Units','normalized','HorizontalAlignment','right')
+    text(1.01,0.95,['Bad' newline 'leverage' newline 'points'],'Units','normalized','HorizontalAlignment','left')
+    text(scoreDist(selu),orthDist(selu),rownames(selu),'HorizontalAlignment','left','VerticalAlignment','bottom');
+    % Good leverage points: points which lie close to the PCA space but far
+    % from the regular observations.
+    % Orthogonal outliers points: points which have a large orthogonal distance
+    % to the PCA space but cannot be seen when we look only at their
+    % projection on the PCA subspace.
+    % Bad leverage points: points which have a large orthogonal distance
+    % and whose projection on the PCA subspace is remote from the typical
+    % projections.
+
+end
+end
--- toolbox/multivariate/pcaFS.m
+++ toolbox/multivariate/pcaFS.m
@@ -58,16 +58,37 @@ function out=pcaFS(Y,varargin)
 %
 %  Optional input arguments:
 %
-%      bsb       : units forming subset on which to perform PCA. vector.
-%                  Vector containing the list of the untis to use to
-%                  compute the svd. The other units are projected in the
-%                  space of the first two PC. bsb can be either a numeric
-%                  vector of length m (m<=n) containin the list of the
+%       robust   : robust principal components. boolean or struct. If
+%               robust is a scalar boolean equal to true (default), FS is
+%               applied and using a Bonferronized confidence level units
+%               are declared as outliers. If robust is a struct it is
+%               possible to choose between FS or MCD and the structure may
+%               contain the following fields:
+%           robust.bsb = units forming subset on which to
+%                  compute the svd. bsb can be either a
+%                  numeric vector of length m (m<=n) containing the list of the
 %                  units (e.g. 1:50) or a logical vector of length n
 %                  containing the true for the units which have to be used
 %                  in the calculation of svd. For example bsb=true(n,1),
 %                  bsb(13)=false; excludes from the svd unit number 13.
-%                  Note that if bsb is supplied bdp must be empty.
+%                  The other units are projected into this robust space.
+%                  Note that is robust.bsb is supplied all the other
+%                  options are ignored.
+%           robust.conflev = scalar in the interval (0 1) which contains
+%                   the confidence level to declare the units as outliers.
+%                   if robust.conflev is not specified or if it is missing
+%                   a 0.99 Bonferronized confidence level is used.
+%           robust.class = class can be either 'FS' (default) or 'MCD' or 'MM'
+%                   in order to choose between Forward search, or minimum
+%                   covariance determinant or MM estimators.
+%           robust.bdp= break down point to use in the 'FS' or 'MCD' estimator or in the
+%                   preliminary S estimator which is used to compute the MM.
+%                   The default is bdp=0.4; It measures the fraction of outliers the algorithm should
+%               resist. In this case any value greater than 0 but smaller
+%               or equal than 0.5 will do fine.
+%           robust.eff = nominal efficiency to use for the MM estimator
+%                       (note that  this field is used just if robust.class
+%                       is 'MM')
 %                 Example - 'bsb',[2 10:90 93]
 %                 Data Types - double or logical
 %
@@ -231,14 +252,13 @@ standardize=true;
 biplot=1;
 dispresults=true;
 NumComponents=[];
-bdp='';
-bsb='';
+robust=false;
 
 if nargin>1
     options=struct('plots',plots, ...
         'standardize',standardize,'biplot', biplot,...
         'dispresults',dispresults,'NumComponents',NumComponents,...
-        'bdp',bdp,'bsb',bsb);
+        'robust',robust);
 
     [varargin{:}] = convertStringsToChars(varargin{:});
     UserOptions=varargin(1:2:length(varargin));
@@ -273,8 +293,7 @@ if nargin>1
     biplot=options.biplot;
     dispresults=options.dispresults;
     NumComponents=options.NumComponents;
-    bdp=options.bdp;
-    bsb=options.bsb;
+    robust=options.robust;
 end
 
 if istable(Y)
@@ -289,127 +308,267 @@ else
     rownames=cellstr(num2str((1:n)','%d'));
 end
 
-if ~isempty(bdp) && ~isempty(bsb)
-    error('FSDA:pcaFS:WrongInputOpt','just one between bsb and bdp has to be supplied');
-end
+bsb=true(n,1);
 
-if ~isempty(bdp)
-    outMCD=mcd(Y,'bdp',bdp,'conflev',1-0.01/n);
-    % bsb=outMCD.outliers
-    bsb=true(n,1);
-    bsb(outMCD.outliers)=false;
-    robust=true;
-elseif ~isempty(bsb)
-    if ~islogical(bsb)
-        bsbini=false(n,1);
-        bsbini(bsb)=true;
-        bsb=bsbini;
-    end
-    robust=true;
-else
-    bsb=true(n,1);
-    robust=false;
-end
-Ybsb=Y(bsb,:);
-nbsb=size(Ybsb,1);
+if isstruct(robust) || (islogical(robust) && robust ==true)
 
-center=mean(Ybsb);
-if standardize==true
-    dispersion=std(Ybsb);
-    % Create matrix of standardized data
-    Z=(Y-center)./dispersion;
-else
-    % Create matrix of deviations from the means
-    Z=Y-center;
-end
+    if isstruct(robust)
+        if isfield(robust,'bsb') % User has chosen a prespecified subset of units to use
+            bsbini(bsb)=true;
+            bsb=bsbini;
 
-% [~,S,loadings]=svd(Z./sqrt(n-1),0);
-% Z=(Y-mean(Y))*loadings;
-
-Ztable=array2table(Z,'RowNames',rownames,'VariableNames',varnames);
+        elseif  isfield(robust,'class')
+            class=robust.class;
+            if strcmp(class,'MCD')
+                % User has chosen MCD
+                if isfield(robust,'conflev')
+                    conflev=robust.conflev;
+                else
+                    conflev=1-0.01/n;
+                end
+                if isfield(robust,'bdp')
+                    bdp=robust.bdp;
+                else
+                    bdp=0.5;
+                end
+                outMCD=mcd(Y,'bdp',bdp,'conflev',conflev,'plots',0);
+                bsb(outMCD.outliers)=false;
 
 
-% Correlation (Covariance) matrix in table format
-Zbsb=Z(bsb,:);
-R=corr(Zbsb);
-Rtable=array2table(R,'VariableNames',varnames,'RowNames',varnames);
+            elseif strcmp(class,'MM') % user has chosen MM estimators
+                if isfield(robust,'eff') % User has specified eff of MM estimators
+                    eff=robust.eff;
+                else
+                    eff=0.95;
+                end
 
-sigmas=sqrt(diag(R));
+                if isfield(robust,'conflev')
+                    conflev=robust.conflev;
+                else
+                    conflev=1-0.01/n;
+                end
 
-% svd on matrix Z.
-[~,Gamma,V]=svd(Zbsb,'econ');
-Gamma=Gamma/sqrt(nbsb-1);
+                outMM=MMmult(Y,'eff',eff,'conflev',conflev);
+                bsb(outMM.outliers)=false;
+            elseif strcmp(class,'FS') % user has chosen FS
+                if isfield(robust,'conflev')
+                    conflev=robust.conflev;
+                else
+                    conflev=0.99;
+                end
+                outFS=FSM(Y,'bonflev',conflev,'plots',0,'msg',0);
+                bsb(outFS.outliers)=false;
+            else
+                error('FSDA:pcaFS:wrongCLASS','Admissible classes are "FS", "MCD" or "MM"')
+            end
+        else
+            error('FSDA:pcaFS:missingCLASS','Specify a class among "FS", "MCD" or "MM"')
+        end
 
-% \Gamma*\Gamma = matrice degli autovalori della matrice di correlazione
-La=Gamma.^2;
-la=diag(La);
+    else % in this case robust is a scalar boolean
 
-%% Explained variance
-sumla=sum(la);
-explained=[la 100*(la)/sumla 100*cumsum(la)/sumla];
-namerows=cellstr([repmat('PC',v,1) num2str((1:v)')]);
-namecols={'Eigenvalues' 'Explained_Variance' 'Explained_Variance_cum'};
-explainedT=array2table(explained,'RowNames',namerows,'VariableNames',namecols);
-if isempty(NumComponents)
-    NumComponents=find(explained(:,3)>100*0.95^v,1);
-    if NumComponents==1 && v>1
-        disp('The first PC already explains more than 0.95^v variability')
-        disp('In what follows we still extract the first 2 PCs')
-        NumComponents=2;
+        if robust == true
+            outFS=FSM(Y,'bonflev',0.99,'msg',0,'plots',0);
+            bsb(outFS.outliers)=false;
+        else
+            bsb=true(n,1);
+        end
     end
 end
 
-% labels of the PCs
-pcnames=cellstr(num2str((1:NumComponents)','PC%d'));
+%{
+    Ybsb=Y(bsb,:);
+    nbsb=size(Ybsb,1);
 
-V=V(:,1:NumComponents);
-La=La(1:NumComponents,1:NumComponents);
-VT=array2table(V,'RowNames',varnames','VariableNames',pcnames);
+    center=mean(Ybsb);
+    if standardize==true
+        dispersion=std(Ybsb);
+        % Create matrix of standardized data
+        Z=(Y-center)./dispersion;
+    else
+        % Create matrix of deviations from the means
+        Z=Y-center;
+    end
+
+    % [~,S,loadings]=svd(Z./sqrt(n-1),0);
+    % Z=(Y-mean(Y))*loadings;
+
+    Ztable=array2table(Z,'RowNames',rownames,'VariableNames',varnames);
+
+    % Correlation (Covariance) matrix in table format
+    Zbsb=Z(bsb,:);
+    R=corr(Zbsb);
+    Rtable=array2table(R,'VariableNames',varnames,'RowNames',varnames);
+
+    sigmas=sqrt(diag(R));
+
+    % svd on matrix Z.
+    [~,Gamma,V]=svd(Zbsb,'econ');
+    Gamma=Gamma/sqrt(nbsb-1);
+
+    % \Gamma*\Gamma = matrice degli autovalori della matrice di correlazione
+    La=Gamma.^2;
+    la=diag(La);
+
+    %% Explained variance
+    sumla=sum(la);
+    explained=[la 100*(la)/sumla 100*cumsum(la)/sumla];
+    namerows=cellstr([repmat('PC',v,1) num2str((1:v)')]);
+    namecols={'Eigenvalues' 'Explained_Variance' 'Explained_Variance_cum'};
+    explainedT=array2table(explained,'RowNames',namerows,'VariableNames',namecols);
+    if isempty(NumComponents)
+        NumComponents=find(explained(:,3)>100*0.95^v,1);
+        if NumComponents==1 && v>1
+            disp('The first PC already explains more than 0.95^v variability')
+            disp('In what follows we still extract the first 2 PCs')
+            NumComponents=2;
+        end
+    end
+
+    % labels of the PCs
+    pcnames=cellstr(num2str((1:NumComponents)','PC%d'));
+
+    V=V(:,1:NumComponents);
+    La=La(1:NumComponents,1:NumComponents);
+    VT=array2table(V,'RowNames',varnames','VariableNames',pcnames);
 
 
-%% Loadings
-loadings=V*sqrt(La)./sigmas;
-loadingsT=array2table(loadings,'RowNames',varnames','VariableNames',pcnames);
+    %% Loadings
+    loadings=V*sqrt(La)./sigmas;
+    loadingsT=array2table(loadings,'RowNames',varnames','VariableNames',pcnames);
 
 
-%% Principal component scores
-score=Z*V;
-scoreT=array2table(score,'RowNames',rownames,'VariableNames',pcnames);
+    %% Principal component scores
+    score=Z*V;
+    scoreT=array2table(score,'RowNames',rownames,'VariableNames',pcnames);
 
 
-%% Communalities
-commun=loadings.^2;
-labelscum=cellstr([repmat([pcnames{1} '-'],NumComponents-1,1) char(pcnames{2:end})]);
-communcum=cumsum(loadings.^2,2);
-communwithcum=[commun communcum(:,2:end)];
-if isempty(labelscum{1})
-    varNames=pcnames;
-else
-    varNames=[pcnames; labelscum];
-end
+    %% Communalities
+    commun=loadings.^2;
+    labelscum=cellstr([repmat([pcnames{1} '-'],NumComponents-1,1) char(pcnames{2:end})]);
+    communcum=cumsum(loadings.^2,2);
+    communwithcum=[commun communcum(:,2:end)];
+    if isempty(labelscum{1})
+        varNames=pcnames;
+    else
+        varNames=[pcnames; labelscum];
+    end
 
-if verLessThanFS('9.7')
-    varNames=matlab.lang.makeValidName(varNames);
-end
-communwithcumT=array2table(communwithcum,'RowNames',varnames,...
-    'VariableNames',varNames);
+    if verLessThanFS('9.7')
+        varNames=matlab.lang.makeValidName(varNames);
+    end
+    communwithcumT=array2table(communwithcum,'RowNames',varnames,...
+        'VariableNames',varNames);
 
-%% Orthogonal distance to PCA subspace based on k PC
-Res=Z-score*V';
-orthDist=sqrt(sum(Res.^2,2));
+    %% Orthogonal distance to PCA subspace based on k PC
+    Res=Z-score*V';
+    orthDist=sqrt(sum(Res.^2,2));
 
 
-%% Score distance in PCA subspace of dimension k
-larow=diag(La)';
-scoreDist=sqrt(sum(score.^2./larow,2));
+    %% Score distance in PCA subspace of dimension k
+    larow=diag(La)';
+    scoreDist=sqrt(sum(score.^2./larow,2));
 
-% Find the 5 units with the largest value of the combination between
-% orthogonal and score distance
-DD=[orthDist,scoreDist];
-mDD=zeros(1,2);
-distM=mahalFS(DD,mDD,cov(DD));
-[~,indsor]=sort(distM,1,"descend");
-selu=indsor(1:5);
+    % Find the 5 units with the largest value of the combination between
+    % orthogonal and score distance
+    DD=[orthDist,scoreDist];
+    mDD=zeros(1,2);
+    distM=mahalFS(DD,mDD,cov(DD));
+    [~,indsor]=sort(distM,1,"descend");
+    selu=indsor(1:5);
+%}
+
+
+
+%{
+
+    if dispresults == true
+        format bank
+        if standardize == true
+            disp('Initial correlation matrix')
+        else
+            disp('Initial covariance matrix')
+        end
+        disp(Rtable)
+
+        disp('Explained variance by PCs')
+        disp(explainedT)
+
+        disp('Loadings = correlations between variables and PCs')
+        disp(loadingsT)
+
+        disp('Communalities')
+        disp(communwithcumT)
+        format short
+
+        disp('Units with the 5 largest values of (combined) score and orthogonal distance')
+        disp(selu')
+    end
+
+    if plots==1
+
+        %% Explained variance through Pareto plot
+        figure('Name','Explained variance')
+        [h,axesPareto]=pareto(explained(:,1),namerows);
+        % h(1) refers to the bars h(2) to the line
+        h(1).FaceColor='g';
+        linelabels = string(round(100*h(2).YData/sumla,2));
+        text(axesPareto(2),h(2).XData,h(2).YData,linelabels,...
+            'Interpreter','none');
+        xlabel('Principal components')
+        ylabel('Explained variance (%)')
+
+        %% Plot loadings
+        xlabels=categorical(varnames,varnames);
+        figure('Name','Loadings')
+
+        for i=1:NumComponents
+            subplot(NumComponents,1,i)
+            b=bar(xlabels, loadings(:,i),'g');
+            title(['Correlations with PC' num2str(i)])
+            xtips=b(1).XData;
+            ytips=b(1).YData;
+            % The alternative instructions below only work from MATLAB
+            % 2019b
+            %   xtips = b.XEndPoints;
+            %   ytips = b.YEndPoints;
+            barlabels = string(round(loadings(:,i),2));
+            text(xtips,ytips,barlabels,'HorizontalAlignment','center',...
+                'VerticalAlignment','bottom')
+            title(['Correlations  with PC' num2str(i)])
+        end
+
+        %% Plot of orthogonal distance (Y) versus score distance (X)
+        figure('Name','OutlierMap')
+        group1=repelem("Normal units",n,1);
+        group1(bsb==0)="Outliers";
+        scatterboxplot(scoreDist,orthDist,'group',group1);
+        xlabel('Score distance')
+        ylabel('Orth. dist. from PCA subspace')
+        text(1.01,0,['Good' newline 'leverage' newline 'points'],'Units','normalized')
+        text(-0.05,0,['Normal' newline 'units'],'Units','normalized','HorizontalAlignment','right')
+        text(0.05,-0.05,['Normal' newline 'units'],'Units','normalized','HorizontalAlignment','left')
+        text(-0.05,1.05,'Orthogonal outliers','Units','normalized','HorizontalAlignment','left')
+        text(0.95,1.05,'Bad leverage points','Units','normalized','HorizontalAlignment','right')
+        text(1.01,0.95,['Bad' newline 'leverage' newline 'points'],'Units','normalized','HorizontalAlignment','left')
+        text(scoreDist(selu),orthDist(selu),rownames(selu),'HorizontalAlignment','left','VerticalAlignment','bottom');
+        % Good leverage points: points which lie close to the PCA space but far
+        % from the regular observations.
+        % Orthogonal outliers points: points which have a large orthogonal distance
+        % to the PCA space but cannot be seen when we look only at their
+        % projection on the PCA subspace.
+        % Bad leverage points: points which have a large orthogonal distance
+        % and whose projection on the PCA subspace is remote from the typical
+        % projections.
+
+    end
+%}
+
+
+% The part above which has been commented is done inside the function below
+% In this way the function can be called without pcaFS
+[Ztable,Rtable,explained,explainedT,V,VT,loadings,loadingsT,communwithcum,communwithcumT, ...
+    score,scoreT,orthDist,scoreDist]=aux.computePCA(Y,bsb,rownames,varnames,standardize,NumComponents,dispresults,plots);
 
 out=struct;
 out.Rtable=Rtable;
@@ -427,90 +586,8 @@ out.orthDist=orthDist;
 out.scoreDist=scoreDist;
 out.class='pcaFS';
 
-if dispresults == true
-    format bank
-    if standardize == true
-        disp('Initial correlation matrix')
-    else
-        disp('Initial covariance matrix')
-    end
-    disp(Rtable)
-
-    disp('Explained variance by PCs')
-    disp(explainedT)
-
-    disp('Loadings = correlations between variables and PCs')
-    disp(loadingsT)
-
-    disp('Communalities')
-    disp(communwithcumT)
-    format short
-
-    disp('Units with the 5 largest values of (combined) score and orthogonal distance')
-    disp(selu')
-end
-
-if plots==1
-
-    %% Explained variance through Pareto plot
-    figure('Name','Explained variance')
-    [h,axesPareto]=pareto(explained(:,1),namerows);
-    % h(1) refers to the bars h(2) to the line
-    h(1).FaceColor='g';
-    linelabels = string(round(100*h(2).YData/sumla,2));
-    text(axesPareto(2),h(2).XData,h(2).YData,linelabels,...
-        'Interpreter','none');
-    xlabel('Principal components')
-    ylabel('Explained variance (%)')
-
-    %% Plot loadings
-    xlabels=categorical(varnames,varnames);
-    figure('Name','Loadings')
-
-    for i=1:NumComponents
-        subplot(NumComponents,1,i)
-        b=bar(xlabels, loadings(:,i),'g');
-        title(['Correlations with PC' num2str(i)])
-        xtips=b(1).XData;
-        ytips=b(1).YData;
-        % The alternative instructions below only work from MATLAB
-        % 2019b
-        %   xtips = b.XEndPoints;
-        %   ytips = b.YEndPoints;
-        barlabels = string(round(loadings(:,i),2));
-        text(xtips,ytips,barlabels,'HorizontalAlignment','center',...
-            'VerticalAlignment','bottom')
-        title(['Correlations  with PC' num2str(i)])
-    end
-
-    %% Plot of orthogonal distance (Y) versus score distance (X)
-    figure('Name','OutlierMap')
-    scatterboxplot(scoreDist,orthDist);
-    xlabel('Score distance')
-    ylabel('Orth. dist. from PCA subspace')
-    text(1.01,0,['Good' newline 'leverage' newline 'points'],'Units','normalized')
-    text(-0.05,0,['Normal' newline 'units'],'Units','normalized','HorizontalAlignment','right')
-    text(0.05,-0.05,['Normal' newline 'units'],'Units','normalized','HorizontalAlignment','left')
-    text(-0.05,1.05,'Orthogonal outliers','Units','normalized','HorizontalAlignment','left')
-    text(0.95,1.05,'Bad leverage points','Units','normalized','HorizontalAlignment','right')
-    text(1.01,0.95,['Bad' newline 'leverage' newline 'points'],'Units','normalized','HorizontalAlignment','left')
-    text(scoreDist(selu),orthDist(selu),rownames(selu),'HorizontalAlignment','left','VerticalAlignment','bottom');
-    % Good leverage points: points which lie close to the PCA space but far
-    % from the regular observations.
-    % Orthogonal outliers points: points which have a large orthogonal distance
-    % to the PCA space but cannot be seen when we look only at their
-    % projection on the PCA subspace.
-    % Bad leverage points: points which have a large orthogonal distance
-    % and whose projection on the PCA subspace is remote from the typical
-    % projections.
-
-end
 if biplot==1
-    if robust==true
-        biplotAPP(Ztable,'standardize',standardize,'bsb',bsb)
-    else
-        biplotAPP(Ztable,'standardize',standardize)
-    end
+    biplotAPP(Ztable,'standardize',standardize,'bsb',bsb)
 end
 
 end
