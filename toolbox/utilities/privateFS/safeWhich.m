b0124e23fda5725bd7bdf456dd2b49ccadb31162
MarcoRianiUNIPR <mriani@unipr.it>
2026-06-25 01:24:09 +0200

    Added file safeWhich.m

---
+++ toolbox/utilities/privateFS/safeWhich.m
@@ -0,0 +1,11 @@
+function f = safeWhich(dpfkpghqgvzsxx_pcxwvgpalieekcpmss)
+%safeWhich   Call WHICH in a clean workspace to avoid matching variables.
+
+% Copyright 2014 The MathWorks, Inc.
+
+% Any additional variables defined in this function's workspace when
+% calling WHICH may overshadow a file with that name on the path, so
+% isolate this into a subfunction with its own workspace and use a crazy
+% variable name.
+
+f = which(dpfkpghqgvzsxx_pcxwvgpalieekcpmss);
